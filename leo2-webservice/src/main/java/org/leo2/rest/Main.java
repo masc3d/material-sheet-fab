48c41cd7277e53bcec6e76dbf737c4624ffcfffc
masc <masc@disappear.de>
2014-07-30 12:05:26 +0200

    leo2-webservice: added standalone run configuration using grizzy http container

---
+++ leo2-webservice/src/main/java/org/leo2/rest/Main.java
@@ -0,0 +1,23 @@
+package org.leo2.rest;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Created by masc on 30.07.14.
+ */
+public class Main {
+    private static final URI BASE_URI = URI.create("http://localhost:8080/leo2/");
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(BASE_URI, new WebserviceResourceConfig());
+
+        System.out.println("Enter to stop webservice");
+        System.in.read();
+
+        server.shutdownNow();
+    }
+}
