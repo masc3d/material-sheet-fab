166fe2b908edd474c64924a6e3b05982c72a22ed
masc <masc@disappear.de>
2014-09-13 00:18:57 +0200

    Moved leo2 application to leo2-ui module

--- leo2-ui/src/main/java/org/leo2/Main.java
+++ leo2-ui/src/main/java/org/leo2/Main.java
@@ -6,21 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
-import org.glassfish.jersey.client.proxy.WebResourceFactory;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
-import org.leo2.rest.v1.*;
+import java.net.URL;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("fx/main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fx/Main.fxml"));
         primaryStage.setTitle("leo2");
-        primaryStage.setScene(new Scene(root, 900, 600));
+        primaryStage.setScene(new Scene(root, 1600, 800));
         primaryStage.show();
     }
 
