e7dcc4b939d9c6fdb42cf68fb45b0042969fe208
masc <masc@disappear.de>
2014-12-05 15:43:34 +0100

    OPTIMIZED imports and code format

--- src/main/java/sx/util/EventDispatcher.java
+++ src/main/java/sx/util/EventDispatcher.java
@@ -6,34 +6,38 @@ import java.util.List;
 
 /**
  * Multicast event dispatcher abstract and factory.
- *
+ * <p/>
  * This class is supposed to be used by observable classes, which can in turn expose instance(s) via EventDelegate interface for consumer to add listeners.
  * Created by masc on 11.08.14.
  */
 public abstract class EventDispatcher<T extends EventListener> implements EventDelegate<T> {
     /**
      * Generic runnable interface for event emission
+     *
      * @param <T>
      */
     public interface Runnable<T> {
         public void run(T listener);
     }
 
-    public EventDispatcher(){
+    public EventDispatcher() {
     }
 
     /**
      * Add listener
+     *
      * @param listener
      */
     public abstract void add(T listener);
     /**
      * Remove listener
+     *
      * @param listener
      */
     public abstract void remove(T listener);
     /**
      * Remove weak references from listeners, used for cleaning out garbage collected/invalid rfs
+     *
      * @param listeners
      */
     protected abstract void remove(List<WeakReference<T>> listeners);
@@ -41,8 +45,10 @@ public abstract class EventDispatcher<T extends EventListener> implements EventD
      * Get copy of listener references
      */
     protected abstract List<WeakReference<T>> getListeners();
+
     /**
      * Emit event to listeners
+     *
      * @param r Runnable supposed to call listener interface method
      */
     public void emit(Runnable<T> r) {
@@ -64,8 +70,10 @@ public abstract class EventDispatcher<T extends EventListener> implements EventD
         if (invalidRefs != null)
             this.remove(invalidRefs);
     }
+
     /**
      * Factory method to create a regular (thread-unsafe) event dispatcher
+     *
      * @param <V> Type of event to dispatch
      * @return Event dispatcher
      */
@@ -75,8 +83,11 @@ public abstract class EventDispatcher<T extends EventListener> implements EventD
 
     /**
      * Factory method to create a thread-safe event dispatcher
+     *
      * @param <V> Type of event to dispatch
      * @return Event dispatcher
      */
-    public static <V extends EventListener> EventDispatcher<V> createThreadSafe() { return new ThreadSafeEventDispatcher<V>(); }
+    public static <V extends EventListener> EventDispatcher<V> createThreadSafe() {
+        return new ThreadSafeEventDispatcher<V>();
+    }
 }
