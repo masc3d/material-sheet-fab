5258a7a77f299488e5a43cb6b9c70833e4c8e575
masc <masc@disappear.de>
2015-04-21 22:40:40 +0200

    ADDED exception logging for jersey

---
+++ leo2-central/src/main/java/org/deku/leo2/central/rest/ExceptionMapper.java
@@ -0,0 +1,25 @@
+package org.deku.leo2.central.rest;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.ext.Provider;
+import javax.xml.ws.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by masc on 21.04.15.
+ */
+@Provider
+public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception> {
+    Logger mLogger = Logger.getLogger(ExceptionMapper.class.getName());
+
+    @Override
+    public javax.ws.rs.core.Response toResponse(Exception e) {
+        mLogger.log(Level.SEVERE, e.getMessage(), e);
+        if (e instanceof WebApplicationException) {
+            return ((WebApplicationException) e).getResponse();
+        } else {
+            return null;
+        }
+    }
+}
--- leo2-central/src/main/java/org/deku/leo2/central/rest/WebserviceResourceConfig.java
+++ leo2-central/src/main/java/org/deku/leo2/central/rest/WebserviceResourceConfig.java
@@ -1,8 +1,10 @@
 package org.deku.leo2.central.rest;
 
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * Created by masc on 23.07.14.
@@ -10,6 +12,8 @@ import java.util.Set;
 public class WebserviceResourceConfig extends ResourceConfig {
     public WebserviceResourceConfig()
     {
+        register(new LoggingFilter(Logger.getGlobal(), true));
+
         packages("org.deku.leo2.central.rest");
 
         Set<Class<?>> classes = this.getClasses();
