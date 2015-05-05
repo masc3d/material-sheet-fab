947b097115359cad978ddc15844c91415a6f9a9b
Jürgen Töpper <juergen.toepper@dr-t.de>
2015-05-05 15:15:31 +0200

    Cleanup + dns leo2ws in WS Clientfactory

--- leo2-central/src/main/java/org/deku/leo2/central/Main.java
+++ leo2-central/src/main/java/org/deku/leo2/central/Main.java
@@ -11,7 +11,7 @@ import java.net.URI;
  * Created by masc on 30.07.14.
  */
 public class Main {
-    private static final URI BASE_URI = URI.create("http://localhost:8080/leo2/");
+    private static final URI BASE_URI = URI.create("http://leo2ws:8080/leo2/");
 
     public static void main(String[] args) throws IOException {
         Persistence.instance().initialize();
