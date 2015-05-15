cecafeb3085f452ed2ec0ca5d15c61b3103089c7
masc <masc@disappear.de>
2015-05-15 12:37:15 +0200

    ADDED leo2-central: exception mapper implementation

--- leo2-central/src/main/java/org/deku/leo2/central/rest/ExceptionMapper.java
+++ leo2-central/src/main/java/org/deku/leo2/central/rest/ExceptionMapper.java
@@ -1,6 +1,10 @@
 package org.deku.leo2.central.rest;
 
+import sx.util.Cast;
+
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,13 +14,38 @@ import java.util.logging.Logger;
  */
 @Provider
 public class ExceptionMapper implements javax.ws.rs.ext.ExceptionMapper<Exception> {
+    static class ExceptionResponse {
+        private WebApplicationException mException;
+
+        public ExceptionResponse(WebApplicationException e) {
+            mException = e;
+        }
+
+        public Integer getStatus() {
+            return mException.getResponse().getStatus();
+        }
+
+        public String getMessage() {
+            return mException.getMessage();
+        }
+
+        public String getLocalizedMessage() {
+            return mException.getLocalizedMessage();
+        }
+    }
+
     Logger mLogger = Logger.getLogger(ExceptionMapper.class.getName());
 
     @Override
     public javax.ws.rs.core.Response toResponse(Exception e) {
         mLogger.log(Level.SEVERE, e.getMessage(), e);
-        if (e instanceof WebApplicationException) {
-            return ((WebApplicationException) e).getResponse();
+        WebApplicationException we = Cast.as(WebApplicationException.class, e);
+        if (we != null) {
+            return Response
+                    .status(we.getResponse().getStatus())
+                    .entity(new ExceptionResponse(we))
+                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .build();
         } else {
             return null;
         }
