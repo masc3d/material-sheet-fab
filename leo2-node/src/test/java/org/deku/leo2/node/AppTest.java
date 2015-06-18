4ca252c06892a994ecdc323b1771e0934590bf34
masc <masc@disappear.de>
2015-06-18 20:58:49 +0200

    UPDATED test case superclass hierarchy

---
+++ leo2-node/src/test/java/org/deku/leo2/node/AppTest.java
@@ -0,0 +1,17 @@
+package org.deku.leo2.node;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by masc on 18.06.15.
+ */
+public class AppTest {
+    public AppTest() {
+        App.instance().initialize(App.LogConfigurationType.NONE);
+
+        Logger lRoot = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        lRoot.setLevel(Level.INFO);
+    }
+}
--- leo2-node/src/test/java/org/deku/leo2/node/DataTest.java
+++ leo2-node/src/test/java/org/deku/leo2/node/DataTest.java
@@ -1,10 +1,7 @@
 package org.deku.leo2.node;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.ConfigFileApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
@@ -20,14 +17,7 @@ import javax.servlet.ServletContext;
 @ContextConfiguration(classes = {
         org.deku.leo2.node.data.PersistenceContext.class,
 }, initializers = ConfigFileApplicationContextInitializer.class)
-public class DataTest {
-    public DataTest() {
-        App.instance().initialize();
-
-        Logger lRoot = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        lRoot.setLevel(Level.INFO);
-    }
-
+public class DataTest extends AppTest {
     // masc20150604. due to workaround of resteasy/spring @Context bug, have to deliver a dummy ServletContext here
     // this implies that services injecting context cannot be unit tested using this base class.
     @Bean
