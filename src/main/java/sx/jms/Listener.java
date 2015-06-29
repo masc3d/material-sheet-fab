ae7bdefe54a2a428a07b9f69a7074de64d7b6fad
masc <masc@disappear.de>
2015-06-29 12:50:40 +0200

    ADDED Listener: delegate support ADDED default message converter

--- src/main/java/sx/jms/Listener.java
+++ src/main/java/sx/jms/Listener.java
@@ -19,6 +19,15 @@ public abstract class Listener implements Disposable, ExceptionListener, Handler
     private HashMap<Class, Handler> mHandlerDelegates = new HashMap<Class, Handler>();
     private Converter mConverter = null;
 
+    /**
+     * Message handling exception
+     */
+    private class HandlingException extends RuntimeException {
+        public HandlingException(String message) {
+            super(message);
+        }
+    }
+
     public Listener(ConnectionFactory connectionFactory) {
         mLog = LogFactory.getLog(this.getClass());
         mConnectionFactory = connectionFactory;
@@ -44,6 +53,23 @@ public abstract class Listener implements Disposable, ExceptionListener, Handler
         mConverter = converter;
     }
 
+    @Override
+    public void onMessage(Message message, Session session) throws JMSException {
+        Object messageObject = mConverter.fromMessage(message);
+
+        Handler handler = null;
+        handler = mHandlerDelegates.getOrDefault(messageObject.getClass(), null);
+        if (handler == null) {
+            handler = mHandlerDelegates.getOrDefault(Message.class, null);
+        }
+
+        if (handler == null) {
+            throw new HandlingException(String.format("No delegate for object type [%s] and no generic delegate for [%s]",
+                    messageObject.getClass(),
+                    Message.class));
+        }
+    }
+
     /**
      * Add handler delegate for handling messages of specific (object) type
      * Delegate handlers requires a converter to be set.
@@ -52,16 +78,14 @@ public abstract class Listener implements Disposable, ExceptionListener, Handler
      * @param <T>
      */
     public <T> void addDelegate(Class<T> c, Handler<T> delegate) {
-        synchronized (mHandlerDelegates) {
-            mHandlerDelegates.put(c, delegate);
-        }
+        mHandlerDelegates.put(c, delegate);
     }
 
     @Override
     public void dispose() {
         try {
             this.stop();
-        } catch(Exception e) {
+        } catch (Exception e) {
             mLog.error(e.getMessage(), e);
         }
     }
