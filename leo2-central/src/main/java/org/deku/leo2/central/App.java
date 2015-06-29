20df0238d706ccc9aedbf789737324debda1a419
masc <masc@disappear.de>
2015-06-29 12:49:52 +0200

    MOVED leo2-central: log listener configuration to specific class

--- leo2-central/src/main/java/org/deku/leo2/central/App.java
+++ leo2-central/src/main/java/org/deku/leo2/central/App.java
@@ -12,28 +12,13 @@ import org.deku.leo2.messaging.log.LogListener;
 public class App extends org.deku.leo2.node.App {
     private Log mLog = LogFactory.getLog(App.class);
 
-    LogListener mLogListener;
-
-    private ActiveMQBroker.Listener mBrokerListener = new ActiveMQBroker.Listener() {
-        @Override
-        public void onStart() {
-            mLog.info("Detected broker start, attaching listeners");
-            mLogListener = new LogListener(ActiveMQContext.instance());
-            mLogListener.start();
-        }
-    };
-
     public static App instance() {
         return (App)org.deku.leo2.node.App.instance();
     }
-
 
     @Override
     public void initialize() {
         // No JMS logging for leo2-central
         super.initialize(LogConfigurationType.NONE, EntitySyncConfigurationType.NONE);
-
-        // Register to broker start
-        ActiveMQBroker.instance().getListenerEventDispatcher().add(mBrokerListener);
     }
 }
---
+++ leo2-central/src/main/java/org/deku/leo2/central/log/LogListenerConfiguration.java
@@ -0,0 +1,45 @@
+package org.deku.leo2.central.log;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.deku.leo2.messaging.activemq.ActiveMQBroker;
+import org.deku.leo2.messaging.activemq.ActiveMQContext;
+import org.deku.leo2.messaging.log.LogListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Created by masc on 29.06.15.
+ */
+@Configuration
+@Lazy(false)
+public class LogListenerConfiguration {
+    Log mLog = LogFactory.getLog(this.getClass());
+
+    /** Log listener instance */
+    LogListener mLogListener;
+
+    private ActiveMQBroker.Listener mBrokerListener = new ActiveMQBroker.Listener() {
+        @Override
+        public void onStart() {
+            mLog.info("Detected broker start, attaching listeners");
+            mLogListener = new LogListener(ActiveMQContext.instance());
+            mLogListener.start();
+        }
+    };
+
+    @PostConstruct
+    public void onInitialize() {
+        // Register to broker start
+        ActiveMQBroker.instance().getListenerEventDispatcher().add(mBrokerListener);
+    }
+
+    @PreDestroy
+    public void onDestroy() {
+        if (mLogListener != null)
+            mLogListener.dispose();
+    }
+}
