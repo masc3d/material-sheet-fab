06595e096cf29d919e1b979453bbf06de894509d
masc <masc@disappear.de>
2015-07-19 16:34:53 +0200

    ADDED skeleton for leo2-boot, packager

---
+++ leo2-boot/src/main/java/org/deku/leo2/boot/Main.java
@@ -0,0 +1,10 @@
+package org.deku.leo2.boot;
+
+/**
+ * Created by n3 on 18-Jul-15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
