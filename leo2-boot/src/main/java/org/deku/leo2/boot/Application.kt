420de9377a1f2e353d1f3fbe516dfc698b570f55
masc <masc@disappear.de>
2015-07-31 01:25:27 +0200

    ADDED: leo2-boot stub

---
+++ leo2-boot/src/main/java/org/deku/leo2/boot/Application.kt
@@ -0,0 +1,23 @@
+package org.deku.leo2.boot
+
+import javafx.fxml.FXMLLoader
+import javafx.scene.Parent
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+/**
+ * Created by n3 on 29-Jul-15.
+ */
+class Application : javafx.application.Application() {
+    throws(Exception::class)
+
+    override fun start(primaryStage: Stage) {
+        val root = FXMLLoader.load<Parent>(this.javaClass.getResource("fx/Main.fxml"))
+        primaryStage.setTitle("Hello World")
+        primaryStage.setScene(Scene(root, 300.0, 275.0))
+        primaryStage.show()
+    }
+
+    companion object {
+    }
+}
---
+++ leo2-boot/src/main/java/org/deku/leo2/boot/fx/MainController.kt
@@ -0,0 +1,7 @@
+package org.deku.leo2.boot.fx
+
+/**
+ * Created by n3 on 29-Jul-15.
+ */
+class MainController {
+}
