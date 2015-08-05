b1ab0ccfa1b485d63bddc7b679b73da3b99a41a3
masc <masc@disappear.de>
2015-08-05 23:00:18 +0200

    ADDED sx.Dispose: logging support

--- src/main/java/sx/Dispose.java
+++ src/main/java/sx/Dispose.java
@@ -1,9 +1,15 @@
 package sx;
 
+import com.google.common.base.Stopwatch;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Created by masc on 07.07.15.
  */
 public class Dispose {
+    static Log mLog = LogFactory.getLog(Dispose.class);
+
     /**
      * Customized disposal
      * @param r Dispose operation
@@ -18,17 +24,23 @@ public class Dispose {
     }
 
     /**
-     * Safely disposes an instance implementing @link Disposable
-     *
+     * Safely disposes an instance implementing @link Disposable, logging, timing and errors
      * @param d Disposable instance
      * @param exceptionHandler Optional exception handler
      */
     public static void safely(Disposable d, Action<Exception> exceptionHandler) {
-        safely((Runnable) () -> d.dispose(), exceptionHandler);
+        mLog.info(String.format("Disposing [%s]", d.getClass().getName()));
+        Stopwatch sw = Stopwatch.createStarted();
+        safely((Runnable) () -> d.dispose(), (e) -> {
+            mLog.error(e.getMessage(), e);
+            exceptionHandler.perform(e);
+        }
+        );
+        mLog.info(String.format("Disposed [%s] in [%s]", d.getClass().getName(), sw.toString()));
     }
 
     /**
-     * Safely disposes an instance implementing @link Disposable
+     * Safely disposes an instance implementing @link Disposable, logging timing and errors
      * @param d
      */
     public static void safely(Disposable d) {
