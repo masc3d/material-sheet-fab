8bcbbe01a4a90e2877b92f53d50bf668f05935bd
masc <masc@disappear.de>
2015-08-12 19:58:11 +0200

    REMOVED leoz gradle packager: unused method

--- buildSrc/src/main/groovy/org/deku/gradle/PackagerUtils.groovy
+++ buildSrc/src/main/groovy/org/deku/gradle/PackagerUtils.groovy
@@ -107,26 +107,4 @@ class PackagerUtils {
 
         return new PlatformArch(platform, cpuArch)
     }
-
-    public static String archIdentifier() {
-        String prefix = SystemUtils.IS_OS_WINDOWS ? "win"
-                : SystemUtils.IS_OS_LINUX ? "linux"
-                : SystemUtils.IS_OS_MAC_OSX ? "osx"
-                : null;
-
-        if (!prefix)
-            throw IllegalStateException("Unsupported platform");
-
-        switch (SystemUtils.OS_ARCH) {
-        // 64bit
-            case "amd64":
-            case "x86_64": prefix += "64"; break;
-        // 32bit
-            case "x86": prefix += "32"; break;
-        // Unsupported
-            default: throw IllegalStateException("Unsupported architecture [${SystemUtils.OS_ARCH}]");
-        }
-
-        return prefix
-    }
 }
