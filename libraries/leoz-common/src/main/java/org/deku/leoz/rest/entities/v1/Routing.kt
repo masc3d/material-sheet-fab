117557d587c9b973e783564474960ea277c4005c
masc <masc@disappear.de>
2015-08-14 11:01:10 +0200

    UPDATED: REST API doc strings

--- libraries/leoz-common/src/main/java/org/deku/leoz/rest/entities/v1/Routing.kt
+++ libraries/leoz-common/src/main/java/org/deku/leoz/rest/entities/v1/Routing.kt
@@ -19,9 +19,9 @@ public class Routing {
      * Routing service request response member
      * Created by JT on 23.06.15.
      */
-    ApiModel(value = "Participant", description = "Response Participant. Delivery or consignee")
+    ApiModel(value = "Participant", description = "Response participant. Sender or consignee")
     public class Participant {
-        ApiModelProperty(dataType = "string", example = "020", position = 10, required = true, value = "Stationnumber", allowableValues = "010 - 999")
+        ApiModelProperty(dataType = "string", example = "020", position = 10, required = true, value = "Station number", allowableValues = "010 - 999")
         public var station: Int? = 0
         public val stationFormatted: String?
             get() = if (station != null) Strings.padStart(station.toString(), 3, '0') else null
@@ -29,25 +29,25 @@ public class Routing {
         public var country: String = ""
         ApiModelProperty(dataType = "string", example = "01-1010", position = 30, required = true, value = "Zipcode contry conform")
         public var zipCode: String = ""
-        ApiModelProperty(dataType = "string", example = "WR", position = 40, required = true, value = "specify Zone", allowableValues = "A,B,C,D,WR,UL")
+        ApiModelProperty(dataType = "string", example = "WR", position = 40, required = true, value = "Zone", allowableValues = "A,B,C,D,WR,UL")
         public var zone: String = ""
-        ApiModelProperty(dataType = "string", example = "X", position = 50, required = true, value = "specify Sector", allowableValues = "A-Z")
+        ApiModelProperty(dataType = "string", example = "X", position = 50, required = true, value = "Sector", allowableValues = "A-Z")
         public var sector: String? = ""
-        ApiModelProperty(dataType = "string", example = "Workday", position = 60, required = true, value = "Type of Day")
+        ApiModelProperty(dataType = "string", example = "Workday", position = 60, required = true, value = "Type of day")
         public var dayType: String = DayType.Workday.toString()
-        ApiModelProperty(example = "false", position = 70, required = true, value = "specify Islands")
+        ApiModelProperty(example = "false", position = 70, required = true, value = "Is island")
         public var island: Boolean? = false
-        ApiModelProperty(dataType = "integer", example = "1", position = 80, required = true, value = "termtime in days", allowableValues = ">=1")
+        ApiModelProperty(dataType = "integer", example = "1", position = 80, required = true, value = "Termtime in days", allowableValues = ">=1")
         public var term: Int? = 1
         ApiModelProperty(dataType = "string", example = "08:01", position = 90, required = true, value = "Earliest time of delivery", allowableValues = "00:00 - 23:59")
         public var earliestTimeOfDelivery: ShortTime? = ShortTime()
-        ApiModelProperty(dataType = "string", example = "12:00", position = 120, required = true, value = "delivery time until on Saturday", allowableValues = "00:00 - 23:59")
+        ApiModelProperty(dataType = "string", example = "12:00", position = 120, required = true, value = "Delivery time until on saturday", allowableValues = "00:00 - 23:59")
         public var saturdayDeliveryUntil: ShortTime = ShortTime()
-        ApiModelProperty(dataType = "string", example = "12:00", position = 130, required = true, value = "delivery time until on Sunday", allowableValues = "00:00 - 23:59")
+        ApiModelProperty(dataType = "string", example = "12:00", position = 130, required = true, value = "Delivery time until on sunday", allowableValues = "00:00 - 23:59")
         public var sundayDeliveryUntil: ShortTime = ShortTime()
-        ApiModelProperty(dataType = "string", example = "16:00", position = 140, required = true, value = "pick up time until", allowableValues = "00:00 - 23:59")
+        ApiModelProperty(dataType = "string", example = "16:00", position = 140, required = true, value = "Pick up time until", allowableValues = "00:00 - 23:59")
         public var pickupUntil: ShortTime = ShortTime()
-        ApiModelProperty(dataType = "string", example = "AH", position = 150, required = true, value = "Partner Manager")
+        ApiModelProperty(dataType = "string", example = "AH", position = 150, required = true, value = "Partner manager")
         public var partnerManager: String = ""
 
         ApiModelProperty(hidden = true)
@@ -60,15 +60,15 @@ public class Routing {
     public var sender: Routing.Participant? = Participant()
     ApiModelProperty(position = 40)
     public var consignee: Routing.Participant? = Participant()
-    ApiModelProperty(dataType = "string", example = "F,N", position = 50, required = true, value = "Used via Hubs: \"F,N\"")
+    ApiModelProperty(dataType = "string", example = "F,N", position = 50, required = true, value = "Used via hubs: \"F,N\"")
     public var viaHubs: Array<String>? = null
-    ApiModelProperty(dataType = "string", example = "F,N 100", position = 60, required = true, value = "Routingstring on Label: \"F,N 100\"")
+    ApiModelProperty(dataType = "string", example = "F,N 100", position = 60, required = true, value = "Routing label content:  \"F,N 100\"")
     public var labelContent: String = ""
-    ApiModelProperty(dataType = "string", example = "OK", position = 70, required = true, value = "Infomassage: \"OK\"")
+    ApiModelProperty(dataType = "string", example = "OK", position = 70, required = true, value = "Info message: \"OK\"")
     public var message: String = ""
-    ApiModelProperty(dataType = "date", example = "2015-06-01", position = 10, required = true, value = "Senddate", allowableValues = "2015-06-01")
+    ApiModelProperty(dataType = "date", example = "2015-06-01", position = 10, required = true, value = "Send date", allowableValues = "2015-06-01")
     public var sendDate: ShortDate? = null
-    ApiModelProperty(dataType = "date", example = "2015-06-02", position = 20, required = false, value = "Deliverydate", allowableValues = "2015-06-01")
+    ApiModelProperty(dataType = "date", example = "2015-06-02", position = 20, required = false, value = "Delivery date", allowableValues = "2015-06-01")
     public var deliveryDate: ShortDate? = null
 
     public constructor() {  }
--- libraries/leoz-common/src/main/java/org/deku/leoz/rest/entities/v1/RoutingRequest.kt
+++ libraries/leoz-common/src/main/java/org/deku/leoz/rest/entities/v1/RoutingRequest.kt
@@ -13,27 +13,27 @@ public class RoutingRequest {
      * Sender or consignee attributes
      * Created by masc on 23.06.15.
      */
-    @ApiModel(value = "RequestParticipant", description = "Request Participant. Delivery or consignee")
+    @ApiModel(value = "RequestParticipant", description = "Request participant. Sender or consignee")
     public class RequestParticipant {
         @ApiModelProperty(dataType = "string", example = "DE", value = "Country two-letter ISO-3166", position = 10, required = true)
         public var country: String? = null
-        @ApiModelProperty(dataType = "string", example = "36286", value = "Zip code accordant to country spezification", position = 20, required = true)
+        @ApiModelProperty(dataType = "string", example = "36286", value = "Zip code accordant to country specification", position = 20, required = true)
         public var zip: String? = null
         ApiModelProperty(dataType = "string", example = "09:00", position = 40, required = false, value = "Time window (from)", allowableValues = "00:00 - 23:59")
         public var timeFrom: String? = null
         ApiModelProperty(dataType = "string", example = "12:00", position = 50, required = false, value = "Time window (to)", allowableValues = "00:00 - 23:59")
         public var timeTo: String? = null
-        ApiModelProperty(dataType = "string", example = "020", position = 60, required = false, value = "Desired Stationnumber", allowableValues = "010 - 999")
+        ApiModelProperty(dataType = "string", example = "020", position = 60, required = false, value = "Desired station number", allowableValues = "010 - 999")
         public var desiredStation: String? = null
 
         public constructor() {  }
     }
 
-    ApiModelProperty(dataType = "date", example = "2015-06-01", position = 10, required = true, value = "Senddate", allowableValues = "2015-06-01")
+    ApiModelProperty(dataType = "date", example = "2015-06-01", position = 10, required = true, value = "Send date", allowableValues = "2015-06-01")
     public var sendDate: ShortDate? = null
-    ApiModelProperty(dataType = "date", example = "2015-06-02", position = 20, required = false, value = "Desired Deliverydate", allowableValues = "2015-06-02")
+    ApiModelProperty(dataType = "date", example = "2015-06-02", position = 20, required = false, value = "Desired delivery date", allowableValues = "2015-06-02")
     public var desiredDeliveryDate: ShortDate? = null
-    ApiModelProperty(value = "Sum of DeKu Servicvalues", position = 22, required = false)
+    ApiModelProperty(value = "Sum of DeKu servic values", position = 22, required = false)
     public var services: Int? = null
     ApiModelProperty(value = "Real weight", position = 24, required = false)
     public var weight: Float? = null
