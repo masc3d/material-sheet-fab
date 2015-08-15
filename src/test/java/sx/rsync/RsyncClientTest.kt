1d3fbafb452f07d6f2be6c9635f607587bbfd340
masc <masc@disappear.de>
2015-08-15 22:47:20 +0200

    WIP RsyncClient

--- src/test/java/sx/rsync/RsyncClientTest.kt
+++ src/test/java/sx/rsync/RsyncClientTest.kt
@@ -1,6 +1,8 @@
 package sx.rsync
 
 import org.junit.Test
+import java.net.URI
+import java.net.URL
 
 /**
  * Created by masc on 15.08.15.
@@ -10,6 +12,10 @@ class RsyncClientTest : RsyncTest() {
     @Test
     fun testClient() {
         val rsyncClient = RsyncClient(this.rsyncExecutablePath)
+        rsyncClient.source = URI("rsync://leoz@syntronix.de/leoz/test")
+        rsyncClient.destination = URI(".")
+        rsyncClient.password = "leoz"
+        rsyncClient.compression = 9
         rsyncClient.run()
     }
 }
