44b213c61b38223a67009396faa2b3e5fee67032
masc <masc@disappear.de>
2015-08-27 19:46:54 +0200

    ADDED rsync binaries ADDED Rsync: auto detection

--- src/main/java/sx/rsync/Rsync.kt
+++ src/main/java/sx/rsync/Rsync.kt
@@ -2,15 +2,39 @@ package sx.rsync
 
 import com.google.common.base.StandardSystemProperty
 import sx.ProcessExecutor
+import sx.platform.PlatformId
 import java.io.File
 import java.net.URL
+import java.nio.file.Path
+import java.nio.file.Paths
 
 /**
  * Created by masc on 15.08.15.
  */
 public open class Rsync() {
     companion object {
-        /** Path to rsync executable */
+        /** Path to rsync executable.
+         * When not set explicitly, getter tries auto detect by scanning current and parent directories for
+         * relative path 'bin/<platformid>' */
         public var executablePath: File? = null
+            get() {
+                if ($executablePath != null)
+                    return $executablePath
+
+                // Search for executable in current and all parent paths
+                var binRelPath = Paths.get("bin").resolve(PlatformId.current().toString())
+
+                var path = Paths.get("").toAbsolutePath()
+                do {
+                    var binPath = path.resolve(binRelPath)
+                    if (binPath.toFile().exists()) {
+                        $executablePath = binPath.resolve("sx-rsync").toFile()
+                        return $executablePath
+                    }
+                    path = path.getParent()
+                } while(path != null)
+
+                throw IllegalStateException("Could not find sx-rsync executable")
+            }
     }
 }
