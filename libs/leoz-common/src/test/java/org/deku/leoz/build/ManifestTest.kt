cc881f4db7968862daad14f913969082d08dcabe
masc <masc@disappear.de>
2015-08-28 23:13:02 +0200

    REMOVED unused test

--- libs/leoz-common/src/test/java/org/deku/leoz/build/ManifestTest.kt
+++ libs/leoz-common/src/test/java/org/deku/leoz/build/ManifestTest.kt
@@ -6,34 +6,3 @@ import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.File
 
-/**
- * Created by masc on 27.08.15.
- */
-public class ManifestTest {
-    val testPath = File("/Users/n3/Projects/customers/leoz-release/leoz-central/osx64")
-
-    @Test
-    public fun testCreate() {
-        Manifest.create(testPath)
-    }
-
-    private fun serializeToString(m: Manifest): ByteArrayOutputStream {
-        var b = ByteArrayOutputStream()
-        m.save(b)
-        return b
-    }
-
-    @Test
-    public fun testSerialize() {
-        var m = Manifest.create(testPath)
-        println(this.serializeToString(m))
-    }
-
-    @Test
-    public fun testDeserialize() {
-        Manifest.load(
-                ByteArrayInputStream(
-                        this.serializeToString(
-                                Manifest.create(testPath)).toByteArray()))
-    }
-}
