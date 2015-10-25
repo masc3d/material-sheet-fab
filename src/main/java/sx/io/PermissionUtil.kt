bcdc6a8635927cfa0f5940f4dfeab77e963e0271
masc <masc@disappear.de>
2015-10-25 23:06:57 +0100

    FIXED RsyncClient: executability flag ADDED PermissionUtil

---
+++ src/main/java/sx/io/PermissionUtil.kt
@@ -0,0 +1,31 @@
+package sx.io
+
+import org.apache.commons.lang3.SystemUtils
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.attribute.*
+
+/**
+ * Permission utility functions
+ * Created by n3 on 25-Oct-15.
+ */
+object PermissionUtil {
+    /**
+     * Applies acl and owner of given path to all child objects (files and folders)
+     * @param path Root path
+     */
+    fun applyAclRecursively(path: File) {
+        if (SystemUtils.IS_OS_WINDOWS) {
+            val rootFav = Files.getFileAttributeView(path.toPath(), AclFileAttributeView::class.java)
+
+            path.walk().forEach { f ->
+                // Get file attribute view
+                val fav = Files.getFileAttributeView(f.toPath(), AclFileAttributeView::class.java)
+
+                fav.owner = rootFav.owner
+                fav.acl = rootFav.acl
+            }
+        }
+        // TODO: add support for other platforms
+    }
+}
