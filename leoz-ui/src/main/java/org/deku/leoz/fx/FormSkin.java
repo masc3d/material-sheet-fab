b24714c7636ef00fb39d50ab4f17e1a026e8a03b
masc <masc@disappear.de>
2016-02-01 15:16:38 +0100

    FIXED compatibility with sshd-core-1.1.0 FIXED leoz-ui: compatibility with fxform2-8.0.15

--- leoz-ui/src/main/java/org/deku/leoz/fx/FormSkin.java
+++ leoz-ui/src/main/java/org/deku/leoz/fx/FormSkin.java
@@ -44,7 +44,7 @@ public class FormSkin extends FXFormSkin {
     private Node createTitleNode() {
         Label label = new AutoHidableLabel();
         label.getStyleClass().add("form-title");
-        label.textProperty().bind(fxForm.titleProperty());
+        label.textProperty().bind(this.getSkinnable().titleProperty());
         return label;
     }
 
