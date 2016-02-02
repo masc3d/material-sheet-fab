076371aa72c36738b58a2b821facece62a569df9
masc <masc@disappear.de>
2016-02-02 16:09:28 +0100

    UPDATED EntityPublisher: removed message priority for testing

--- leoz-node/src/main/java/org/deku/leoz/node/data/sync/EntityPublisher.java
+++ leoz-node/src/main/java/org/deku/leoz/node/data/sync/EntityPublisher.java
@@ -104,7 +104,7 @@ public class EntityPublisher extends SpringJmsListener {
         mLog.debug(euMessage);
 
         MessageProducer mp = session.createProducer(message.getJMSReplyTo());
-        mp.setPriority(1);
+        //mp.setPriority(1);
         mp.setTimeToLive(TimeUnit.MINUTES.toMillis(1));
         mp.send(messageConverter.toMessage(euMessage, session));
 
