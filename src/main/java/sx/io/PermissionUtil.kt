6aa19321249efcd596809fbb80096b9e1429df36
masc <masc@disappear.de>
2016-02-03 11:42:43 +0100

    MOVED win32/generic permission functionality to sx.PermissionUtil, referring changes

--- src/main/java/sx/io/PermissionUtil.kt
+++ src/main/java/sx/io/PermissionUtil.kt
@@ -1,33 +1,76 @@
 package sx.io
 
-import org.apache.commons.lang3.SystemUtils
+import com.sun.jna.platform.win32.Advapi32Util
 import java.io.File
+import java.nio.file.FileSystems
 import java.nio.file.Files
-import java.nio.file.attribute.AclFileAttributeView
+import java.nio.file.attribute.*
+import java.util.*
 
 /**
  * Permission utility functions
  * Created by n3 on 25-Oct-15.
  */
 object PermissionUtil {
-    val SID_EVERYONE = "S-1-1-0"
+    object Win32 {
+        val SID_EVERYONE = "S-1-1-0"
+
+        /**
+         * Returns FQN for a sid
+         * @param sid Account SID
+         */
+        fun sidToFqn(sid: String): String {
+            return Advapi32Util.getAccountBySid(sid).fqn
+        }
+
+        /**
+         * FQN of the win32 `Everyone` account
+         */
+        val fqnEveryone: String
+            get() = sidToFqn(SID_EVERYONE)
+    }
 
     /**
-     * Applies acl and owner of given path to all child objects (files and folders)
+     * Applies ACL and owner of given path to all child objects (files and folders)
      * @param path Root path
      */
     fun applyAclRecursively(path: File) {
-        if (SystemUtils.IS_OS_WINDOWS) {
-            val rootFav = Files.getFileAttributeView(path.toPath(), AclFileAttributeView::class.java)
+        val rootFav = Files.getFileAttributeView(path.toPath(), AclFileAttributeView::class.java)
 
-            path.walk().forEach { f ->
-                // Get file attribute view
-                val fav = Files.getFileAttributeView(f.toPath(), AclFileAttributeView::class.java)
+        path.walk().forEach { f ->
+            // Get file attribute view
+            val fav = Files.getFileAttributeView(f.toPath(), AclFileAttributeView::class.java)
 
-                fav.owner = rootFav.owner
-                fav.acl = rootFav.acl
-            }
+            fav.owner = rootFav.owner
+            fav.acl = rootFav.acl
         }
-        // TODO: add support for other platforms
+    }
+
+    /**
+     * Set ACL to allow everything for specific principals, replacing existing permissions
+     * @param path Path
+     * @param inherit Inherit permissions
+     * @param principals Principals to grant access for
+     */
+    fun setAclAllowEverything(path: File, inherit: Boolean = true, vararg principals: String) {
+        // Get file attribute view
+        var fav = Files.getFileAttributeView(path.toPath(), AclFileAttributeView::class.java)
+
+        // Lookup principal
+        var fs = FileSystems.getDefault()
+        var ups: UserPrincipalLookupService = fs.userPrincipalLookupService
+
+        // Set ACL
+        fav.acl = principals.map { p ->
+            var gp = ups.lookupPrincipalByName(p)
+
+            var aclb = AclEntry.newBuilder()
+            aclb.setPermissions(EnumSet.allOf(AclEntryPermission::class.java))
+            aclb.setPrincipal(gp)
+            if (inherit)
+                aclb.setFlags(AclEntryFlag.DIRECTORY_INHERIT, AclEntryFlag.FILE_INHERIT)
+            aclb.setType(AclEntryType.ALLOW)
+            aclb.build()
+        }
     }
 }
