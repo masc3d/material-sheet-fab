6b57bcd20b4501e7179593da5909b01786e85976
masc <masc@disappear.de>
2016-02-10 00:24:01 +0100

    FIXED UpdateInfoRequestHandler: faiure to respond (due to eg. consumer interruption/deleted queue) did not dequeue message UPDATED leoz-central version to 0.32

--- leoz-central/src/main/java/org/deku/leoz/central/messaging/handlers/UpdateInfoRequestHandler.kt
+++ leoz-central/src/main/java/org/deku/leoz/central/messaging/handlers/UpdateInfoRequestHandler.kt
@@ -1,11 +1,11 @@
 package org.deku.leoz.central.messaging.handlers
 
+import org.apache.commons.logging.LogFactory
 import org.deku.leoz.bundle.entities.UpdateInfo
 import org.deku.leoz.bundle.entities.UpdateInfoRequest
 import sx.jms.Channel
 import sx.jms.Converter
 import sx.jms.Handler
-import java.time.Duration
 import javax.jms.Message
 import javax.jms.Session
 
@@ -17,20 +17,26 @@ class UpdateInfoRequestHandler
 :
         Handler<UpdateInfoRequest>
 {
+    private val log = LogFactory.getLog(this.javaClass)
+
     override fun onMessage(message: UpdateInfoRequest, converter: Converter, jmsMessage: Message, session: Session) {
         val updateInfoRequest = message
         // TODO: Query bundle name/version against db
         val versionPattern = "+RELEASE"
 
-        val channel = Channel(
-                session = session,
-                destination = jmsMessage.jmsReplyTo,
-                converter = converter)
+        try {
+            val channel = Channel(
+                    session = session,
+                    destination = jmsMessage.jmsReplyTo,
+                    converter = converter)
 
-        channel.send(UpdateInfo(
-                updateInfoRequest.bundleName,
-                versionPattern))
+            channel.send(UpdateInfo(
+                    updateInfoRequest.bundleName,
+                    versionPattern))
 
-        channel.commit()
+            channel.commit()
+        } catch(e: Exception) {
+            log.error(e.message, e)
+        }
     }
 }
