77dcbe52e9b08e1590c621b08795de7c71f392bb
masc <masc@disappear.de>
2016-02-22 18:15:53 +0100

    ADDED jms QueueTest

---
+++ libs/leoz-common/src/test/java/org/deku/leoz/messaging/QueueTest.kt
@@ -0,0 +1,41 @@
+package org.deku.leoz.messaging
+
+import org.apache.commons.logging.LogFactory
+import org.deku.leoz.MessagingTest
+import org.deku.leoz.config.messaging.ActiveMQConfiguration
+import org.junit.Test
+import javax.jms.DeliveryMode
+
+/**
+ * Created by masc on 20/02/16.
+ */
+class QueueTest : MessagingTest() {
+    val log = LogFactory.getLog(this.javaClass)
+    val configuration = ActiveMQConfiguration.instance
+
+    @Test
+    fun testTemporaryQueueSendReceiveWithTransaction() {
+        val cn = this.configuration.broker.connectionFactory.createConnection()
+        cn.start()
+        val s = cn.createSession(true, -1)
+        val q = s.createTemporaryQueue()
+        val mp = s.createProducer(q)
+        mp.deliveryMode = DeliveryMode.NON_PERSISTENT
+        mp.send(s.createTextMessage("hello"))
+        s.commit()
+
+        val s2 = cn.createSession(true, -1)
+        val mc = s2.createConsumer(q)
+        val msg = mc.receive()
+        //        msg.acknowledge()
+        log.info(msg)
+        s2.commit()
+        mc.close()
+
+        s2.close()
+        s.close()
+        q.delete()
+        // TODO. verify that temp queue doesn't have pending entries (bug with activemq upto 5.13.1 when using temp queues within transaction)
+        Thread.sleep(Long.MAX_VALUE)
+    }
+}
