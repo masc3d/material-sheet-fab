966d62e0347b56be39ffb178b1f50a8b561d27a9
masc <masc@disappear.de>
2016-03-03 17:37:38 +0100

    ADDED legacy implementation of Disposable UPDATED Dispose: added support for legacy impementation of Disposable FIXED build.gradle: javax.ws.rs should be provided dependency

---
+++ src/main/java/sx/legacy/Disposable.kt
@@ -0,0 +1,9 @@
+package sx.legacy
+
+/**
+ * Legacy version of Disposable interface, compatible with 1.6 for older jdk/android support
+ * Created by masc on 03/03/16.
+ */
+interface Disposable {
+    fun close()
+}
