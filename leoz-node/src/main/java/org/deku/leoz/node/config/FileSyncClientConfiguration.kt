d00ee4578cb2d2205af5f26801a9fa87f7043d3b
masc <masc@disappear.de>
2016-03-15 16:58:20 +0100

    UPDATED filesync: migrated to service class

--- leoz-node/src/main/java/org/deku/leoz/node/config/FileSyncClientConfiguration.kt
+++ leoz-node/src/main/java/org/deku/leoz/node/config/FileSyncClientConfiguration.kt
@@ -3,26 +3,23 @@ package org.deku.leoz.node.config
 import org.apache.commons.logging.LogFactory
 import org.deku.leoz.config.RsyncConfiguration
 import org.deku.leoz.node.App
-import org.deku.leoz.node.filesync.FileSyncTask
 import org.deku.leoz.node.peer.RemotePeerSettings
+import org.deku.leoz.node.services.FileSyncClientService
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Lazy
 import org.springframework.context.annotation.Profile
 import sx.ssh.SshTunnelProvider
-import java.util.concurrent.Executors
-import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.TimeUnit
 import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
 import javax.inject.Inject
 
 /**
- * Created by n3 on 08-Mar-16.
+ * Created by masc on 08-Mar-16.
  */
 @Configuration
 @Profile(App.PROFILE_CLIENT_NODE)
 @Lazy(false)
-open class FileSyncConfiguration {
+open class FileSyncClientConfiguration {
     private val log = LogFactory.getLog(this.javaClass)
 
     @Inject
@@ -31,34 +28,25 @@ open class FileSyncConfiguration {
     @Inject
     private lateinit var sshTunnelProvider: SshTunnelProvider
 
-    /** Scheduler  */
-    private val executorService: ScheduledExecutorService
-
-    init {
-        this.executorService = Executors.newSingleThreadScheduledExecutor()
-    }
+    @Inject
+    private lateinit var fileSyncClientService: FileSyncClientService
 
     @PostConstruct
     fun onInitialize() {
-        val fs = FileSyncTask(
+        this.fileSyncClientService.addTask(FileSyncClientService.Task(
                 sourcePath = StorageConfiguration.instance.transferDataDirectory,
                 rsyncModuleUri = RsyncConfiguration.createRsyncUri(
                         remotePeerSettings.host!!,
                         13002,
                         RsyncConfiguration.ModuleNames.TRANSFER),
                 rsyncPassword = RsyncConfiguration.PASSWORD,
-                sshTunnelProvider = this.sshTunnelProvider)
+                sshTunnelProvider = this.sshTunnelProvider))
 
-        log.info("Starting filesync scheduler")
-        this.executorService.scheduleWithFixedDelay(fs,
-                // Initial delay
-                0,
-                // Interval
-                10, TimeUnit.SECONDS)
+        this.fileSyncClientService.start()
     }
 
     @PreDestroy
     fun onDestroy() {
-        this.executorService.shutdown()
+        this.fileSyncClientService.close()
     }
 }
---
+++ leoz-node/src/main/java/org/deku/leoz/node/services/FileSyncClientService.kt
@@ -0,0 +1,119 @@
+package org.deku.leoz.node.services
+
+import org.apache.commons.logging.LogFactory
+import sx.concurrent.Service
+import sx.rsync.Rsync
+import sx.rsync.RsyncClient
+import sx.ssh.SshTunnelProvider
+import java.io.File
+import java.time.Duration
+import java.util.*
+import java.util.concurrent.ScheduledExecutorService
+import javax.inject.Inject
+import javax.inject.Named
+
+/**
+ * Created by masc on 11/03/16.
+ */
+@Named
+class FileSyncClientService @Inject constructor(
+        executorService: ScheduledExecutorService)
+:
+        Service(executorService = executorService,
+                period = Duration.ofSeconds(10)) {
+    private val log = LogFactory.getLog(this.javaClass)
+    private val tasks = ArrayList<Task>()
+
+    /**
+     * File synchronization task
+     * Created by masc on 07-Mar-16.
+     * @param sourcePath Source path
+     * @param destinationPath Destination path
+     * @param rsyncModuleUri Destination rsync module
+     * @param rsyncPassword Destination rsync password
+     * @param sshTunnelProvider SSH tunnel provider for rsync connections
+     */
+    class Task(
+            val sourcePath: File,
+            val rsyncModuleUri: Rsync.URI,
+            val rsyncPassword: String = "",
+            val sshTunnelProvider: SshTunnelProvider? = null)
+    :
+            Runnable {
+        private val log = LogFactory.getLog(this.javaClass)
+
+        /**
+         * Rsync client factory helper
+         */
+        private fun createRsyncClient(): RsyncClient {
+            val rc = RsyncClient()
+            rc.password = this.rsyncPassword
+            rc.compression = 2
+            rc.preservePermissions = false
+            rc.preserveExecutability = true
+            rc.preserveGroup = false
+            rc.preserveOwner = false
+            // Remove tranferred files
+            rc.removeSourceFiles = true
+            // Do not remove destination files
+            rc.delete = false
+            rc.sshTunnelProvider = this.sshTunnelProvider
+            return rc
+        }
+
+        override fun run() {
+            try {
+                if (!sourcePath.exists())
+                    throw IllegalStateException("Source path does not exist [${sourcePath}]")
+
+                if (sourcePath.listFiles().count() > 0) {
+                    log.info("Synchronizing [${sourcePath}] -> [${rsyncModuleUri}]")
+                    val rsyncClient = this.createRsyncClient()
+                    rsyncClient.sync(
+                            Rsync.URI(this.sourcePath),
+                            rsyncModuleUri,
+                            { r ->
+                                log.info("Synchronizing ${r.path}")
+                            })
+
+                    // Remove empty directories
+                    sourcePath.walkBottomUp().forEach {
+                        if (!it.equals(sourcePath) &&
+                                it.isDirectory &&
+                                it.listFiles().count() == 0) {
+                            it.delete()
+                        }
+                    }
+                }
+            } catch(e: Exception) {
+                log.error(e.message, e)
+            }
+        }
+    }
+
+    fun addTask(task: Task) {
+        synchronized(this.tasks) {
+            this.tasks.add(task)
+        }
+    }
+
+    /**
+     * Service implementation
+     */
+    override fun run() {
+        // Copy tasks
+        val tasks = ArrayList<Task>()
+        synchronized(this.tasks) {
+            tasks.addAll(this.tasks)
+        }
+
+        // Run tasks
+        tasks.forEach {
+            try {
+                it.run()
+            } catch(e: Exception) {
+                log.error(e.message, e)
+            }
+        }
+    }
+}
