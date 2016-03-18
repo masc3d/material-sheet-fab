e00381af34a60098195d215f9581512498b75312
masc <masc@disappear.de>
2016-03-18 16:12:35 +0100

    ADDED FileSyncMessage

---
+++ leoz-node/src/main/java/org/deku/leoz/node/messaging/entities/FileSyncMessage.kt
@@ -0,0 +1,14 @@
+package org.deku.leoz.node.messaging.entities
+
+import java.io.Serializable
+
+/**
+ * Notifies another node that files are available to be synchronized
+ * Created by masc on 18/03/16.
+ */
+abstract class FileSyncMessage(
+        var key: String = ""
+) : Serializable {
+    private val serialVersionUID = 2805490723264764563L
+
+}
