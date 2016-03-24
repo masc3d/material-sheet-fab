39aee8bc013a15642f39d2eec19d5973416d053d
masc <masc@disappear.de>
2016-03-24 01:43:34 +0100

    UPDATED BundleConfiguration: added local BundleRepository

--- leoz-node/src/main/java/org/deku/leoz/node/config/BundleConfiguration.kt
+++ leoz-node/src/main/java/org/deku/leoz/node/config/BundleConfiguration.kt
@@ -1,12 +1,19 @@
 package org.deku.leoz.node.config
 
 import org.deku.leoz.bundle.BundleInstaller
+import org.deku.leoz.bundle.BundleRepository
 import org.deku.leoz.config.BundleConfiguration
+import sx.rsync.Rsync
 
 /**
  * Created by masc on 09/03/16.
  */
 object BundleConfiguration : BundleConfiguration() {
+    val localRepository: BundleRepository by lazy {
+        BundleRepository(
+                rsyncModuleUri = Rsync.URI(StorageConfiguration.instance.bundleRepositoryDirectory))
+    }
+
     /**
      * Application wide bundle installer
      */
