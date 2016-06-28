da58b87f6c2a0335a134d503b1b87a464ad3bc08
masc <masc@disappear.de>
2016-06-28 19:45:35 +0200

    UPDATED a few classes to kt

---
+++ src/main/java/sx/android/BitmapUtil.kt
@@ -0,0 +1,38 @@
+package sx.android
+
+import android.graphics.Bitmap
+
+/**
+ * Created by masc on 02.12.14.
+ */
+object BitmapUtil {
+    /**
+     * Scale bitmap to square image, cropping as needed
+     * @param src  Source bitmap
+     * @param size Square size
+     * @return
+     */
+    fun scaleSquare(src: Bitmap, size: Int): Bitmap {
+        // Calculate thumbnail scale/crop parameters
+        var newWidth = 0
+        var newHeight = 0
+        val leftOffset: Int
+        val topOffset: Int
+        if (src.width < src.height) {
+            val ratio = src.height.toDouble() / src.width.toDouble()
+            newWidth = size
+            newHeight = (size * ratio).toInt()
+            leftOffset = 0
+            topOffset = (newHeight - size) / 2
+        } else {
+            val ratio = src.width.toDouble() / src.height.toDouble()
+            newWidth = (size * ratio).toInt()
+            newHeight = size
+            leftOffset = (newWidth - 200) / 2
+            topOffset = 0
+        }
+
+        val scaled = Bitmap.createScaledBitmap(src, newWidth, newHeight, false)
+        return Bitmap.createBitmap(scaled, leftOffset, topOffset, size, size)
+    }
+}
