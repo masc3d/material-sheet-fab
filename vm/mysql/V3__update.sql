717518c04b50db32313e4e0b0b4f031b803a7728
Jürgen Töpper <juergen.toepper@derkurier.de>
2016-07-08 16:45:31 +0200

    Schemaänderung für die Umstellung der Synch-Basis von Timestamp auf Counter -> #68

---
+++ vm/mysql/V3__update.sql
@@ -0,0 +1,37 @@
+CREATE TABLE `sys_synch` (
+  `idsys_synch` int(11) NOT NULL,
+  `tablename` varchar(45) DEFAULT NULL,
+  `counter` BIGINT NOT NULL DEFAULT 0 ,
+  PRIMARY KEY (`idsys_synch`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `dekuclient`.`sys_synch` (`idsys_synch`, `tablename`, `counter`) VALUES ('1', 'mst_station', '0');
+
+ALTER TABLE `dekuclient`.`mst_station` 
+ADD COLUMN `synchid` BIGINT NOT NULL DEFAULT 0 ;
+
+USE `dekuclient`;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS dekuclient.mst_station_BEFORE_INSERT$$
+USE `dekuclient`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `dekuclient`.`mst_station_BEFORE_INSERT` BEFORE INSERT ON `mst_station` FOR EACH ROW
+BEGIN
+
+END
+$$
+DELIMITER ;
+USE `dekuclient`;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS dekuclient.mst_station_BEFORE_UPDATE$$
+USE `dekuclient`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `dekuclient`.`mst_station_BEFORE_UPDATE` BEFORE UPDATE ON `mst_station` FOR EACH ROW
+BEGIN
+	update sys_synch set counter=counter+1 where tablename='mst_station';
+    set @counter = (select counter from sys_synch where tablename='mst_station');
+	set new.synchid= @counter;
+END$$
+DELIMITER ;
