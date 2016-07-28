d51e375d03ec63aed23d9fb0e2ecd352a55910ad
masc <masc@disappear.de>
2016-07-28 12:58:39 +0200

    UPDATED mysql sync additions: renamed sync field

--- vm/mysql/V3__update.sql
+++ vm/mysql/V3__update.sql
@@ -3,16 +3,16 @@ USE `dekuclient`;
 CREATE TABLE `sys_sync` (
   `id`         INT(11) NOT NULL,
   `table_name` VARCHAR(45)      DEFAULT NULL,
-  `id_sync`    BIGINT  NOT NULL DEFAULT 0,
+  `sync_id`    BIGINT  NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
 )
   ENGINE = MyISAM
   DEFAULT CHARSET = latin1;
 
-INSERT INTO `dekuclient`.`sys_sync` (`id`, `table_name`, `id_sync`) VALUES ('1', 'mst_station', '0');
+INSERT INTO `dekuclient`.`sys_sync` (`id`, `table_name`, `sync_id`) VALUES ('1', 'mst_station', '0');
 
 ALTER TABLE `dekuclient`.`mst_station`
-  ADD COLUMN `id_sync` BIGINT NOT NULL DEFAULT 0;
+  ADD COLUMN `sync_id` BIGINT NOT NULL DEFAULT 0;
 
 DELIMITER $$
 
@@ -21,14 +21,14 @@ CREATE FUNCTION f_sync_increment_unsafe(p_table_name VARCHAR(50))
   RETURNS BIGINT
   BEGIN
     UPDATE sys_sync
-    SET id_sync = id_sync + 1
+    SET sync_id = sync_id + 1
     WHERE table_name = p_table_name;
 
-    SET @id_sync = (SELECT id_sync
+    SET @sync_id = (SELECT sync_id
                     FROM sys_sync
                     WHERE table_name = p_table_name);
 
-    RETURN @id_sync;
+    RETURN @sync_id;
   END
 $$
 
@@ -36,29 +36,29 @@ DROP FUNCTION IF EXISTS f_sync_increment$$
 CREATE FUNCTION f_sync_increment(p_table_name VARCHAR(50))
   RETURNS BIGINT
   BEGIN
-    SET @id_sync = (SELECT id_sync
+    SET @sync_id = (SELECT sync_id
                     FROM sys_sync
                     WHERE table_name = p_table_name FOR UPDATE );
-    SET @id_sync = @id_sync + 1;
+    SET @sync_id = @sync_id + 1;
 
     UPDATE sys_sync
-    SET id_sync = @id_sync
+    SET sync_id = @sync_id
     WHERE table_name = p_table_name;
 
-    RETURN @id_sync;
+    RETURN @sync_id;
   END
 $$
 
 DROP TRIGGER IF EXISTS dekuclient.mst_station_BEFORE_INSERT$$
 CREATE DEFINER = CURRENT_USER TRIGGER `dekuclient`.`mst_station_BEFORE_INSERT` BEFORE INSERT ON `mst_station` FOR EACH ROW
   BEGIN
-    SET NEW.id_sync = f_sync_increment('mst_station');
+    SET NEW.sync_id = f_sync_increment('mst_station');
   END
 $$
 
 DROP TRIGGER IF EXISTS dekuclient.mst_station_BEFORE_UPDATE$$
 CREATE DEFINER = CURRENT_USER TRIGGER `dekuclient`.`mst_station_BEFORE_UPDATE` BEFORE UPDATE ON `mst_station` FOR EACH ROW
   BEGIN
-    SET NEW.id_sync = f_sync_increment('mst_station');
+    SET NEW.sync_id = f_sync_increment('mst_station');
   END
 $$
