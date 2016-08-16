c96ab957118e72de5170a94b85ca99f5f7714c10
masc <masc@disappear.de>
2016-08-16 10:30:50 +0200

    UPDATED ActiveMQConfiguration: added support for parameterizing connection factory (for connecting to remote broker)

--- libs/leoz-common/src/main/java/org/deku/leoz/config/messaging/ActiveMQConfiguration.kt
+++ libs/leoz-common/src/main/java/org/deku/leoz/config/messaging/ActiveMQConfiguration.kt
@@ -5,25 +5,37 @@ import sx.jms.Channel
 import sx.jms.converters.DefaultConverter
 import sx.jms.Broker
 import sx.jms.activemq.ActiveMQBroker
+import sx.jms.activemq.ActiveMQFactory
+import javax.jms.ConnectionFactory
 
 /**
  * ActiveMQ specific messaging configuration
+ * @param connectionFactory Optional connection factory. If not set reverts to the local broker URI.
  * Created by masc on 16.04.15.
  */
-class ActiveMQConfiguration private constructor() : MessagingConfiguration {
+class ActiveMQConfiguration(connectionFactory: ConnectionFactory? = null) : MessagingConfiguration {
 
     companion object {
         val USERNAME = "leoz"
         val PASSWORD = "iUbmQRejRI1P3SNtzwIM7wAgNazURPcVcBU7SftyZ0oha9FlnAdGAmXdEQwYlKFC"
         val GROUPNAME = "leoz"
 
-        /** Singleton instance  */
+        /**
+         * Singleton instance of a messaging configuration accessing a local/embedded broker
+         */
         @JvmStatic val instance by lazy({
             ActiveMQConfiguration()
         })
     }
 
+    /**
+     * Connection factory
+     */
+    var connectionFactory: ConnectionFactory
+        private set
+
     init {
+        this.connectionFactory = connectionFactory ?: this.broker.connectionFactory
         // Configure broker authentication
         this.broker.user = Broker.User(USERNAME, PASSWORD, GROUPNAME)
     }
@@ -33,8 +45,8 @@ class ActiveMQConfiguration private constructor() : MessagingConfiguration {
 
     override val centralQueue: Channel.Configuration by lazy({
         Channel.Configuration(
-                connectionFactory = this.broker.connectionFactory,
-                destination = this.broker.createQueue("leoz.central.queue"),
+                connectionFactory = this.connectionFactory,
+                destination = ActiveMQFactory.instance.createQueue("leoz.central.queue"),
                 converter = DefaultConverter(
                         DefaultConverter.SerializationType.KRYO,
                         DefaultConverter.CompressionType.GZIP))
@@ -42,8 +54,8 @@ class ActiveMQConfiguration private constructor() : MessagingConfiguration {
 
     override val centralLogQueue: Channel.Configuration by lazy {
         val c = Channel.Configuration(
-                connectionFactory = this.broker.connectionFactory,
-                destination = this.broker.createQueue("leoz.log.queue"),
+                connectionFactory = this.connectionFactory,
+                destination = ActiveMQFactory.instance.createQueue("leoz.log.queue"),
                 deliveryMode = Channel.DeliveryMode.Persistent,
                 converter = DefaultConverter(
                         DefaultConverter.SerializationType.KRYO,
@@ -55,8 +67,8 @@ class ActiveMQConfiguration private constructor() : MessagingConfiguration {
 
     override val entitySyncQueue: Channel.Configuration by lazy {
         Channel.Configuration(
-                connectionFactory = this.broker.connectionFactory,
-                destination = this.broker.createQueue("leoz.entity-sync.queue"),
+                connectionFactory = this.connectionFactory,
+                destination = ActiveMQFactory.instance.createQueue("leoz.entity-sync.queue"),
                 converter = DefaultConverter(
                         DefaultConverter.SerializationType.KRYO,
                         DefaultConverter.CompressionType.GZIP))
@@ -64,24 +76,24 @@ class ActiveMQConfiguration private constructor() : MessagingConfiguration {
 
     override val entitySyncTopic: Channel.Configuration by lazy {
         Channel.Configuration(
-                connectionFactory = this.broker.connectionFactory,
-                destination = this.broker.createTopic("leoz.entity-sync.topic"),
+                connectionFactory = this.connectionFactory,
+                destination = ActiveMQFactory.instance.createTopic("leoz.entity-sync.topic"),
                 converter = DefaultConverter(
                         DefaultConverter.SerializationType.KRYO,
                         DefaultConverter.CompressionType.GZIP))
     }
 
     override fun nodeQueue(identityKey: Identity.Key): Channel.Configuration {
-        return Channel.Configuration(connectionFactory = this.broker.connectionFactory,
-                destination = this.broker.createQueue("leoz.node.queue." + identityKey.short),
+        return Channel.Configuration(connectionFactory = this.connectionFactory,
+                destination = ActiveMQFactory.instance.createQueue("leoz.node.queue." + identityKey.short),
                 converter = DefaultConverter(
                         DefaultConverter.SerializationType.KRYO,
                         DefaultConverter.CompressionType.GZIP))
     }
 
     override val nodeNotificationTopic: Channel.Configuration by lazy {
-        Channel.Configuration(connectionFactory = this.broker.connectionFactory,
-                destination = this.broker.createTopic("leoz.node.notification.topic"),
+        Channel.Configuration(connectionFactory = this.connectionFactory,
+                destination = ActiveMQFactory.instance.createTopic("leoz.node.notification.topic"),
                 converter = DefaultConverter(
                         DefaultConverter.SerializationType.KRYO,
                         DefaultConverter.CompressionType.GZIP))
