798dbca75e21084d2c656b2182a4e563456e0bc0
masc <masc@disappear.de>
2016-08-23 00:31:12 +0200

    UPDATED DiscoveryService: added support for multihomed/multicast, improved logging

--- libs/leoz-common/src/main/java/org/deku/leoz/discovery/DiscoveryService.kt
+++ libs/leoz-common/src/main/java/org/deku/leoz/discovery/DiscoveryService.kt
@@ -4,12 +4,13 @@ import org.deku.leoz.bundle.Bundles
 import org.slf4j.LoggerFactory
 import sx.LazyInstance
 import sx.Lifecycle
-import sx.logging.slf4j.info
 import java.net.InetAddress
-import javax.jmdns.JmDNS
+import java.net.NetworkInterface
+import javax.jmdns.JmmDNS
 import javax.jmdns.ServiceEvent
 import javax.jmdns.ServiceListener
 import javax.jmdns.ServiceTypeListener
+import javax.jmdns.impl.NetworkTopologyDiscoveryImpl
 
 /**
  * Leoz discovery service
@@ -24,92 +25,148 @@ class DiscoveryService(
         vararg serviceInfos: ServiceInfo) : Lifecycle {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    /** Discoverable service infos */
     private val serviceInfos: Array<out ServiceInfo>
 
     init {
+        // The only way to customize jmDNS port for now.
         System.setProperty("net.mdns.port", port.toString())
         this.serviceInfos = serviceInfos
     }
 
-    /** JmDns instance.
+    /**
+     * jmmDNS multihomed instance
      * The instance state of also indicates if this service is started/running or not
-     **/
-    private val lazyJmdns = LazyInstance<JmDNS>({ JmDNS.create(InetAddress.getLocalHost()) })
-    private val jmdns: JmDNS get() = lazyJmdns.get()
+     */
+    private val lazyJmmdns = LazyInstance<JmmDNS>({ JmmDNS.Factory.getInstance() })
+    private val jmmdns: JmmDNS get() = lazyJmmdns.get()
 
     /**
-     * JmDns/zeroconf service type listener
+     * jmDNS service type listener
      */
     private val serviceTypeListener = object : ServiceTypeListener {
-        override fun subTypeForServiceTypeAdded(event: ServiceEvent?) {
-            log.trace("subTypeForServiceTypeAdded ${event}")
+        override fun subTypeForServiceTypeAdded(event: ServiceEvent) {
+            log.debug("subTypeForServiceTypeAdded ${event.type}")
         }
 
-        override fun serviceTypeAdded(event: ServiceEvent?) {
-            log.trace("serviceTypeAdded ${event}")
-            jmdns.addServiceListener(event!!.type, serviceListener)
+        override fun serviceTypeAdded(event: ServiceEvent) {
+            log.debug("serviceTypeAdded ${event.type}")
+            this@DiscoveryService.jmmdns.addServiceListener(event.type, serviceListener)
         }
     }
 
+    /**
+     * jmDNS service listener
+     */
     private val serviceListener = object : ServiceListener {
-        override fun serviceResolved(event: ServiceEvent?) {
-            log.trace("serviceResolved ${event}")
+        override fun serviceResolved(event: ServiceEvent) {
+            log.debug("serviceResolved ${event.info.hostAddresses.joinToString(",")}")
         }
 
-        override fun serviceRemoved(event: ServiceEvent?) {
-            log.trace("serviceRemoved ${event}")
+        override fun serviceRemoved(event: ServiceEvent) {
+            log.debug("serviceRemoved ${event.info.hostAddresses.joinToString(",")}")
         }
 
-        override fun serviceAdded(event: ServiceEvent?) {
-            log.trace("serviceAdded ${event}")
+        override fun serviceAdded(event: ServiceEvent) {
+            log.debug("serviceAdded ${event.dns.hostName} ${event.type}")
         }
-
     }
 
+    /**
+     * Applies service configuration ot jmdns.
+     * This method may be called repeatedly, eg., when network topology changes.
+     */
+    private fun configureServices() {
+        // REMARK: it's safe to call .addXXX methods of jmDNS multiple times as they check for duplicate listeners/services
+
+        // Add service type listener
+        this.jmmdns.addServiceTypeListener(this.serviceTypeListener)
+
+        this.serviceInfos.map {
+            // Translate generic service info to jmDNS service info
+            val type = "_${it.serviceType.value}._tcp."
+            val name = it.serviceType.value
+            val subtype = bundleType?.value ?: ""
+            val text = ""
+            javax.jmdns.ServiceInfo.create(type, name, subtype, port, text)
+        }.forEach {
+            // Register and force persistent updates
+            // TODO: figure out why jmDNS is r{eally slow on those operations (even much worse on Windows)
+            this.jmmdns.registerService(it)
+            this.jmmdns.requestServiceInfo(it.type, "", true)
+        }
+    }
+
+    /**
+     * Network topology discovery, customizing network interface/address filter
+     */
+    class NetworkTopologyDiscovery : NetworkTopologyDiscoveryImpl() {
+        private val log = LoggerFactory.getLogger(this.javaClass)
+
+        override fun useInetAddress(networkInterface: NetworkInterface?, interfaceAddress: InetAddress?): Boolean {
+            if (interfaceAddress == null)
+                return false
+
+            val use = super.useInetAddress(networkInterface, interfaceAddress)
+            log.trace("use=${use} ${networkInterface} ${interfaceAddress} ${interfaceAddress.isLinkLocalAddress} ${interfaceAddress.isSiteLocalAddress}")
+
+            // Only listen on private/site local addresses
+            if (!interfaceAddress.isSiteLocalAddress)
+                return false
+
+            return use
+        }
+    }
+
+    /**
+     * Start discovery service
+     */
     override fun start() {
-        if (lazyJmdns.isSet) {
+        if (lazyJmmdns.isSet) {
             // Already running
             return
         }
 
-        log.trace("Starting discovery service")
+        log.info("Starting discovery service")
+
+        javax.jmdns.NetworkTopologyDiscovery.Factory.setClassDelegate {
+            NetworkTopologyDiscovery()
+        }
 
         try {
-            // Add service type listener
-            this.jmdns.addServiceTypeListener(this.serviceTypeListener)
-
-            // Map to JmDns service types and register
-            this.serviceInfos.map {
-                val type = "_${it.serviceType.value}._tcp."
-                val name = it.serviceType.value
-                val subtype = bundleType?.value ?: ""
-                val text = ""
-                javax.jmdns.ServiceInfo.create(type, name, subtype, port, text)
-            }.forEach {
-                log.trace("Registering ${it}")
-                this.jmdns.registerService(it)
-                log.trace("Registered ${it}")
-            }
+            this.configureServices()
         } catch(e: Throwable) {
-            this.lazyJmdns.reset()
+            this.lazyJmmdns.reset()
             throw e
         }
+
+        log.info("Started discovery service")
     }
 
+    /**
+     * Stop discovery service
+     */
     override fun stop() {
-        this.lazyJmdns.ifSet {
+        this.lazyJmmdns.ifSet {
             log.info("Stopping discovery service")
             it.close()
+            log.info("Stopped discovery service")
         }
-        this.lazyJmdns.reset()
+        this.lazyJmmdns.reset()
     }
 
+    /**
+     * Restart discovery service
+     */
     override fun restart() {
         this.stop()
         this.start()
     }
 
+    /**
+     * Indicates if discovery service is running
+     */
     override fun isRunning(): Boolean {
-        return this.lazyJmdns.isSet
+        return this.lazyJmmdns.isSet
     }
 }
--- libs/leoz-common/src/test/java/org/deku/leoz/discovery/DiscoveryServiceTest.kt
+++ libs/leoz-common/src/test/java/org/deku/leoz/discovery/DiscoveryServiceTest.kt
@@ -3,6 +3,7 @@ package org.deku.leoz.discovery
 import org.deku.leoz.bundle.Bundles
 import org.deku.leoz.config.DiscoveryConfiguration
 import org.junit.Test
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by masc on 22/08/16.
@@ -17,6 +18,7 @@ class DiscoveryServiceTest {
     @Test
     fun testDiscoveryService() {
         this.discoveryService.start()
+        Thread.sleep(Long.MAX_VALUE)
         this.discoveryService.stop()
     }
 }
