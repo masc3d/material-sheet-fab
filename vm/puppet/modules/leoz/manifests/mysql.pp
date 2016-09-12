d93f5996d2c880614f961c96a32b01146b832dac
masc <masc@disappear.de>
2016-09-12 16:35:54 +0200

    UPDATED vm/mysql: movec `sql-mode` parameters to mysql configuration

--- vm/puppet/modules/leoz/manifests/mysql.pp
+++ vm/puppet/modules/leoz/manifests/mysql.pp
@@ -6,7 +6,8 @@ class leoz::mysql (
     root_password => 'root',
     override_options => { 
       'mysqld' => {
-        'bind-address' => '0.0.0.0' 
+        'bind-address' => '0.0.0.0',
+        'sql-mode' => 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'
        }
     },
     restart => true
@@ -29,7 +30,6 @@ class leoz::mysql (
   ::mysql::db { 'dekuclient':
     user => 'leoz',
     password => 'leoz',
-    host => '%',
-    sql => '/vagrant/mysql/setup.sql'
+    host => '%'
   }
 }
