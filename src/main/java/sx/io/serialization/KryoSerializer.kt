41a0adcac6ad5cff4f15dcd93a371e0aac0b7002
masc <masc@disappear.de>
2016-09-15 12:07:28 +0200

    FIXED KryoSerializer: fails on remapping classes when original class is not found

--- src/main/java/sx/io/serialization/KryoSerializer.kt
+++ src/main/java/sx/io/serialization/KryoSerializer.kt
@@ -1,19 +1,18 @@
 package sx.io.serialization
 
 import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.KryoException
 import com.esotericsoftware.kryo.Registration
 import com.esotericsoftware.kryo.io.Input
 import com.esotericsoftware.kryo.io.Output
 import com.esotericsoftware.kryo.pool.KryoPool
 import com.esotericsoftware.kryo.serializers.CompatibleFieldSerializer
-import com.esotericsoftware.kryo.util.DefaultClassResolver
-import com.esotericsoftware.kryo.util.IdentityObjectIntMap
-import com.esotericsoftware.kryo.util.MapReferenceResolver
+import com.esotericsoftware.kryo.util.*
+import com.esotericsoftware.minlog.Log
 import org.slf4j.LoggerFactory
 import java.io.InputStream
 import java.io.OutputStream
 import java.lang.reflect.Array
-import java.util.*
 
 /**
  * Kryo serializer with support for @Serializable annotations.
@@ -71,51 +70,103 @@ class KryoSerializer(
     class ClassResolver : DefaultClassResolver() {
         private val log = LoggerFactory.getLogger(this.javaClass)
 
-        override fun writeName(output: Output?, type: Class<*>?, registration: Registration?) {
-            super.writeName(output, type, registration)
+        private class SerializableType(
+                val uid: Long,
+                val isArray: Boolean
+        ) {
+            fun write(output: Output) {
+                output.writeLong(this.uid)
+                output.writeBoolean(this.isArray)
+            }
 
+            companion object {
+                fun read(input: Input): SerializableType {
+                    return SerializableType(
+                            uid = input.readLong(),
+                            isArray = input.readBoolean())
+                }
+            }
+        }
+
+        private var _lastSerializableType: SerializableType? = null
+
+        override fun writeName(output: Output?, type: Class<*>?, registration: Registration?) {
+            // The following code is mostly a replica of the base class logic plus support for writing @Serializable info
+            output!!.writeVarInt(NAME + 2, true)
+            if (classToNameId != null) {
+                val nameId = classToNameId.get(type, -1)
+                if (nameId != -1) {
+                    if (Log.TRACE) Log.trace("kryo", "Write class name reference " + nameId + ": " + type!!.name)
+                    output.writeVarInt(nameId, true)
+                    return
+                }
+            }
+            // Only write the class name the first time encountered in object graph.
+            if (Log.TRACE) Log.trace("kryo", "Write class name: " + type!!.name)
+            val nameId = nextNameId++
+            if (classToNameId == null) classToNameId = IdentityObjectIntMap()
+            classToNameId.put(type, nameId)
+            output.writeVarInt(nameId, true)
+            output.writeString(type!!.getName())
+
+            //region ** Kryo protocol addition, write @Serializable metainfo **
             // Determine type to register. In case of arrays using componentType
-            val typeToRegister = if (type!!.isArray)
+            val typeToRegister = if (type.isArray)
                 type.componentType
             else
                 type
 
-            val uid = Serializer.register(typeToRegister) ?: 0L
+            val uid = if (!typeToRegister.equals(Any::class.java)) Serializer.register(typeToRegister) else 0L
 
             // Write @Serializable uid to kryo output stream
-            output!!.writeLong(uid)
+            SerializableType(uid = uid, isArray = type.isArray).write(output)
+            //endregion
         }
 
         override fun readName(input: Input?): Registration {
-            val r = super.readName(input)
+            // The following code is mostly a replica of the base class logic plus support for writing @Serializable info
+            val nameId = input!!.readVarInt(true)
+            if (nameIdToClass == null) nameIdToClass = IntMap()
+            var type: Class<*>? = nameIdToClass.get(nameId)
+            if (type == null) {
+                // Only read the class name the first time encountered in object graph.
+                val className = input.readString()
 
-            // Read @Serializable uid from kryo stream
-            val uid = input!!.readLong()
+                //region ** Kryo protocol addition, read @Serializable metainfo **
+                val serializableType = SerializableType.read(input)
+                if (serializableType.uid != 0L) {
+                    type = Serializer.lookup(serializableType.uid)
+                    if (type != null && serializableType.isArray) {
+                        type = Array.newInstance(type, 0).javaClass
+                    }
+                }
+                //endregion
 
-            if (uid == 0L)
-                return r
+                if (type == null) {
+                    type = getTypeByName(className)
+                    if (type == null) {
+                        try {
+                            type = Class.forName(className, false, kryo.classLoader)
+                        } catch (ex: ClassNotFoundException) {
+                            if (Log.WARN) Log.warn("kryo", "Unable to load class $className with kryo's ClassLoader. Retrying with current..")
+                            try {
+                                type = Class.forName(className)
+                            } catch (e: ClassNotFoundException) {
+                                throw KryoException("Unable to find class: " + className, ex)
+                            }
 
-            // Look up class
-            val cls = if (r.type.isArray) {
-                Array.newInstance(Serializer.lookup(uid), 0).javaClass
+                        }
+
+                        if (nameToClass == null) nameToClass = ObjectMap()
+                        nameToClass.put(className, type)
+                    }
+                }
+                nameIdToClass.put(nameId, type)
+                if (Log.TRACE) Log.trace("kryo", "Read class name: " + className)
             } else {
-                Serializer.lookup(uid)
+                if (Log.TRACE) Log.trace("kryo", "Read class name reference " + nameId + ": " + type.name)
             }
-
-            // Check if serializer mapped type is different
-            if (cls != null && !cls.equals(r.type)) {
-                log.debug("Replacing kryo mapping for ${r.type.name} with ${cls.name}")
-                // Override entries in ClassResolver's lookup dictionaries
-                this.nameToClass.put(r.type.name, cls)
-                val key = this.nameIdToClass.findKey(r.type, true, -1)
-                if (key < 0) throw IllegalStateException("Class ${r.type} not found in ClassResolver lookup dictionary")
-                this.nameIdToClass.put(key, cls)
-
-                // Return resolved registration
-                return this.kryo.getRegistration(cls)
-            } else {
-                return r
-            }
+            return kryo.getRegistration(type!!)
         }
     }
 
