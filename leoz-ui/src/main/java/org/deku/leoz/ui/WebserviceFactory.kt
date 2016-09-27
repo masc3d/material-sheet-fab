d556cd58a65cbe8ac8774fec5d524f6ee7ef8f2d
masc <masc@disappear.de>
2016-09-27 21:22:43 +0200

    UPDATED WebserviceFactory * added support for updating web target from discovery events * ignoring SSL certificates (for now)

--- leoz-ui/src/main/java/org/deku/leoz/ui/WebserviceFactory.kt
+++ leoz-ui/src/main/java/org/deku/leoz/ui/WebserviceFactory.kt
@@ -1,13 +1,27 @@
 package org.deku.leoz.ui
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.conf.global
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.lazy
+import org.deku.leoz.discovery.DiscoveryInfo
+import org.deku.leoz.discovery.DiscoveryService
 import org.deku.leoz.rest.services.internal.v1.StationService
 import org.glassfish.jersey.client.ClientProperties
 import org.glassfish.jersey.client.proxy.WebResourceFactory
 import org.glassfish.jersey.logging.LoggingFeature
 import org.slf4j.LoggerFactory
+import sx.LazyInstance
+import sx.net.UdpDiscoveryService
 import java.util.logging.Level
 import java.util.logging.Logger
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import javax.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.ws.rs.client.Client
 import javax.ws.rs.client.ClientBuilder
 import javax.ws.rs.client.WebTarget
 
@@ -20,9 +34,33 @@ object WebserviceFactory {
 
     private val log = LoggerFactory.getLogger(WebserviceFactory::class.java)
 
+    private val discoveryService: DiscoveryService by Kodein.global.lazy.instance()
+
+    /**
+     * Creates a client ignoring SSL certificate (local node connections)
+     */
+    private fun createClientIgnoringSSL(): Client {
+        val sslcontext = SSLContext.getInstance("TLS")
+        sslcontext.init(null, arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkServerTrusted(p0: Array<out java.security.cert.X509Certificate>?, p1: String?) {
+            }
+
+            override fun checkClientTrusted(p0: Array<out java.security.cert.X509Certificate>?, p1: String?) {
+            }
+
+            override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> {
+                return arrayOf<X509Certificate>()
+            }
+        }), java.security.SecureRandom())
+        return ClientBuilder.newBuilder().sslContext(sslcontext).hostnameVerifier { s1, s2 -> true }.build()
+    }
+
+    /**
+     * Webservice client
+     */
     private val client by lazy({
         // Setup client
-        val client = ClientBuilder.newClient()
+        val client = this.createClientIgnoringSSL() //ClientBuilder.newClient()
         client.register(JacksonJsonProvider::class.java)
         client.property(ClientProperties.CONNECT_TIMEOUT, 2000)
         client.property(ClientProperties.READ_TIMEOUT, 2000)
@@ -35,16 +73,43 @@ object WebserviceFactory {
         client
     })
 
-    private val webTarget: WebTarget by lazy({
-        // Setup webtarget
-        this.client.target("http://127.0.0.1:13000/rs/api")
+    private val _webTarget = LazyInstance<WebTarget>( { this.client.target("http://127.0.0.1:13000/rs/api")
     })
+
+    /**
+     * Update web target
+     */
+    private fun update() {
+        var foundService: DiscoveryInfo.Service? = null
+        val host = this.discoveryService.directory.firstOrNull {
+            foundService = it.info?.services?.firstOrNull { it.type == DiscoveryInfo.ServiceType.HTTPS }
+            foundService != null
+        }
+
+        val service = foundService
+
+        if (host != null && service != null) {
+            val url = "https://${host.address.toString()}:${service.port}/rs/api"
+            log.info("Setting webservice url to ${url}")
+            _webTarget.reset {
+                this.client.target("https://${host.address.hostAddress}:${service.port}/rs/api")
+            }
+        }
+    }
+
+    init {
+        this.discoveryService.rxOnUpdate.subscribe {
+            this.update()
+        }
+
+        this.update()
+    }
 
     /** Convenience method to get service instance
      * @param c service interface class
      */
     fun <T> getService(c: Class<T>): T {
-        val instance = WebResourceFactory.newResource(c, webTarget)
+        val instance = WebResourceFactory.newResource(c, _webTarget.get())
         return instance
     }
 
