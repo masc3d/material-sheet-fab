33b4f2e819485cb4917b4ee7f41ffef193b20f48
masc <masc@disappear.de>
2016-09-29 11:33:53 +0200

    UDPATED Main: startup time improvements

--- leoz-ui/src/main/java/org/deku/leoz/ui/Main.kt
+++ leoz-ui/src/main/java/org/deku/leoz/ui/Main.kt
@@ -5,15 +5,20 @@ import com.github.salomonbrys.kodein.conf.global
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.lazy
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.fxml.FXMLLoader
 import javafx.geometry.Rectangle2D
 import javafx.scene.Parent
 import javafx.scene.Scene
+import javafx.scene.control.ProgressIndicator
 import javafx.scene.image.Image
+import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
 import javafx.scene.layout.Pane
 import javafx.scene.text.Font
 import javafx.stage.Screen
 import javafx.stage.Stage
+import javafx.stage.StageStyle
 import org.controlsfx.control.Notifications
 import org.deku.leoz.discovery.DiscoveryService
 import org.deku.leoz.ui.bridge.LeoBridge
@@ -22,6 +27,7 @@ import org.deku.leoz.ui.config.LogConfiguration
 import org.deku.leoz.ui.config.StorageConfiguration
 import org.deku.leoz.ui.fx.Controller
 import org.deku.leoz.ui.fx.MainController
+import org.deku.leoz.ui.fx.StartupController
 import org.slf4j.LoggerFactory
 import sx.util.Utf8ResourceBundleControl
 import java.io.IOException
@@ -98,73 +104,95 @@ class Main : Application() {
 
         log.debug("Initializing injection")
 
-        StorageConfiguration.initalize()
-
-        LogConfiguration.logFile = StorageConfiguration.logFile
-        LogConfiguration.initialize()
-
         // Setup injection
         Kodein.global.addImport(Configurations.application)
         Kodein.global.addImport(Configurations.messenging)
 
-        log.debug("Detaching background initialization")
-
+        log.debug("Initializing executor")
         // Initialize application
         val executor: ExecutorService = Kodein.global.instance()
-
-        // Background task initialization
-        executor.submit {
-            val leoBridge: LeoBridge = Kodein.global.instance()
-
-            // Start leo bridge (asynchronously so it doesn't slow down startup)
-            try {
-                leoBridge.ovMessageReceived.subscribe {
-                    this.toForeground()
-                }
-                leoBridge.start()
-            } catch (e: IOException) {
-                log.error(e.message, e)
-            }
-
-            // Start discovery service
-            Kodein.global.instance<DiscoveryService>().start()
-        }
 
         log.debug("Initializing primary stage")
 
         // Setup stage
         this.primaryStage = primaryStage
+        val splashStage = Stage()
 
-        log.debug("Loading fonts")
-        // Load embedded fonts
-        this.loadFont("/fonts/Futura-CondensedExtraBold.ttf")
-        this.loadFont("/fonts/Futura-CondensedMedium.ttf")
-        this.loadFont("/fonts/Futura-Medium.ttf")
-        this.loadFont("/fonts/Futura-MediumItalic.ttf")
-        this.loadFont("/fonts/Menlo-Regular.ttf")
-        this.loadFont("/fonts/Menlo-Bold.ttf")
+        executor.submit {
+            log.debug("Initializing storage")
+            StorageConfiguration.initalize()
 
-        // Main scene
-        //TODO: User preferences? Check if last size and position should be remembered. Dont think so (PHPR)
-        val primScreenBounds: Rectangle2D = Screen.getPrimary().visualBounds // Used to access the computers screen resolution/size
+            log.debug("Initializing log")
+            LogConfiguration.logFile = StorageConfiguration.logFile
+            LogConfiguration.initialize()
 
-        //Set default scene size which is used when primary stage is no more in maximized mode
-        val width = if(primScreenBounds.width < 1366.0) primScreenBounds.width - 50 else 1366.0
-        val height = if(primScreenBounds.height < 768.0) primScreenBounds.height - 50 else 768.0
+            // Sidechained UI initialization
+            executor.submit {
+                log.debug("Loading fonts")
+                // Load embedded fonts
+                this.loadFont("/fonts/Futura-CondensedExtraBold.ttf")
+                this.loadFont("/fonts/Futura-CondensedMedium.ttf")
+                this.loadFont("/fonts/Futura-Medium.ttf")
+                this.loadFont("/fonts/Futura-MediumItalic.ttf")
+                this.loadFont("/fonts/Menlo-Regular.ttf")
+                this.loadFont("/fonts/Menlo-Bold.ttf")
 
-        log.debug("Creating main controller")
-        val scene = Scene(mainController.fxRoot, width, height)
-        log.debug("Created main controller")
+                val root = mainController.fxRoot
 
-        primaryStage.title = this.i18n.resources.getString("global.title")!!
-        primaryStage.icons.add(Image(this.javaClass.getResourceAsStream("/images/DEKU.icon.256px.png")))
-        primaryStage.scene = scene
+                Platform.runLater {
+                    // Main scene
+                    //TODO: User preferences? Check if last size and position should be remembered. Dont think so (PHPR)
+                    val primScreenBounds: Rectangle2D = Screen.getPrimary().visualBounds // Used to access the computers screen resolution/size
 
-        log.info("Showing primary stage")
-        // Maximizing by default is usually annoying for users and developers alike.
-        // Default should be the minimum supported size.
-        // If it's necessary to maximize by default on partidular occasions/installations it should be paremeterized.
-        primaryStage.show()
+                    //Set default scene size which is used when primary stage is no more in maximized mode
+                    val width = if(primScreenBounds.width < 1366.0) primScreenBounds.width - 50 else 1366.0
+                    val height = if(primScreenBounds.height < 768.0) primScreenBounds.height - 50 else 768.0
+
+                    val scene = Scene(root, width, height)
+
+                    primaryStage.title = this.i18n.resources.getString("global.title")!!
+                    primaryStage.icons.add(Image(this.javaClass.getResourceAsStream("/images/DEKU.icon.256px.png")))
+                    primaryStage.scene = scene
+
+                    log.info("Showing primary stage")
+                    // Maximizing by default is usually annoying for users and developers alike.
+                    // Default should be the minimum supported size.
+                    // If it's necessary to maximize by default on partidular occasions/installations it should be paremeterized.
+                    primaryStage.show()
+                    splashStage.close()
+
+                    executor.submit {
+                        // Start discovery service
+                        Kodein.global.instance<DiscoveryService>().start()
+
+                        // Background task initialization
+                        log.info("Staring LeoBridge")
+                        val leoBridge: LeoBridge = Kodein.global.instance()
+
+                        // Start leo bridge (asynchronously so it doesn't slow down startup)
+                        try {
+                            leoBridge.ovMessageReceived.subscribe {
+                                this.toForeground()
+                            }
+                            leoBridge.start()
+                        } catch (e: IOException) {
+                            log.error(e.message, e)
+                        }
+                    }
+                }
+            }
+        }
+
+        log.debug("Showing splash stage")
+        splashStage.initStyle(StageStyle.TRANSPARENT)
+        val splashView = ProgressIndicator()
+        splashView.background = Background.EMPTY
+        splashStage.scene = Scene(splashView, 40.0, 40.0)
+        splashStage.show()
+
+        log.debug("Shown splash stage")
+
+        log.info("End start")
     }
 
     @Throws(Exception::class)
