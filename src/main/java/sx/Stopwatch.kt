1d7a2932cc9bce2cbcbd7b92e92a5ad4398ed02d
masc <masc@disappear.de>
2016-10-11 19:07:06 +0200

    Merge branch 'deku'
    * deku:
      FIXED sx.concurrent.Thread: missing import
      CLEANUP build files
      Minor cleanup
      Comments
      FIXED JarManifest. should not return nulls
    
    # Conflicts:
    #	build.gradle

--- src/main/java/sx/Stopwatch.kt
+++ src/main/java/sx/Stopwatch.kt
@@ -44,21 +44,13 @@ class Stopwatch {
      * A time source; returns a time value representing the number of nanoseconds elapsed since some
      * fixed but arbitrary point in time. Note that most users should use [Stopwatch] instead of
      * interacting with this class directly.
-
      *
      * **Warning:** this interface can only be used to measure elapsed time, not wall time.
-
      * @author Kevin Bourrillion
-     * *
      * @since 10.0
      * *     ([mostly source-compatible](https://github.com/google/guava/wiki/Compatibility) since 9.0)
      */
-    abstract class Ticker
-    /**
-     * Constructor for use by subclasses.
-     */
-    protected constructor() {
-
+    abstract class Ticker protected constructor() {
         /**
          * Returns the number of nanoseconds elapsed since this ticker's fixed
          * point of reference.
--- src/main/java/sx/platform/PlatformId.kt
+++ src/main/java/sx/platform/PlatformId.kt
@@ -22,7 +22,7 @@ class PlatformId(val operatingSystem: OperatingSystem, val cpuArch: CpuArch) {
 
     companion object {
         @JvmStatic fun parse(identifier: String): PlatformId {
-            var id  = identifier.toLowerCase()
+            val id  = identifier.toLowerCase()
 
             val regex = Regex("^([a-z]+)([0-9]{2}?)$")
             val matchResult = regex.find(id)
@@ -30,14 +30,14 @@ class PlatformId(val operatingSystem: OperatingSystem, val cpuArch: CpuArch) {
             if (matchResult == null)
                 throw IllegalArgumentException("Invalid platform id [${identifier}]")
 
-            var sPlatform = matchResult.groups[1]?.value
-            var sCpuArch = matchResult.groups[2]?.value
+            val sPlatform = matchResult.groups[1]?.value
+            val sCpuArch = matchResult.groups[2]?.value
 
-            var p = OperatingSystem.values().firstOrNull() { it.toString().equals(sPlatform) }
+            val p = OperatingSystem.values().firstOrNull() { it.toString().equals(sPlatform) }
             if (p == null)
                 throw IllegalArgumentException("Unknown platform identifier [${sPlatform}]")
 
-            var ca = CpuArch.values().firstOrNull() { it.toString().equals(sCpuArch) }
+            val ca = CpuArch.values().firstOrNull() { it.toString().equals(sCpuArch) }
             if (ca == null)
                 throw IllegalArgumentException("Unknown cpu arch identifier [${sCpuArch}]")
 
@@ -45,14 +45,14 @@ class PlatformId(val operatingSystem: OperatingSystem, val cpuArch: CpuArch) {
         }
 
         @JvmStatic fun current(): PlatformId {
-            var platform = when {
+            val platform = when {
                 SystemUtils.IS_OS_WINDOWS -> OperatingSystem.WINDOWS
                 SystemUtils.IS_OS_LINUX -> OperatingSystem.LINUX
                 SystemUtils.IS_OS_MAC_OSX -> OperatingSystem.OSX
                 else -> throw IllegalStateException("Unsupported platform")
             }
 
-            var cpuArch = when(SystemUtils.OS_ARCH) {
+            val cpuArch = when(SystemUtils.OS_ARCH) {
                 "amd64", "x86_64" -> CpuArch.X64
                 "x86" -> CpuArch.X86
                 else -> throw IllegalStateException("Unsupported architecture [${SystemUtils.OS_ARCH}]")
@@ -65,6 +65,12 @@ class PlatformId(val operatingSystem: OperatingSystem, val cpuArch: CpuArch) {
     override fun equals(other: Any?): Boolean {
         return if (other is PlatformId) this.cpuArch == other.cpuArch && this.operatingSystem == other.operatingSystem
         else false
+    }
+
+    override fun hashCode(): Int{
+        var result = operatingSystem.hashCode()
+        result = 31 * result + cpuArch.hashCode()
+        return result
     }
 
     override fun toString(): String {
