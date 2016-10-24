7d965da03601f5232bdf65f1411ff129fb0e682d
masc <masc@disappear.de>
2016-10-24 23:55:55 +0200

    UPDATED RouteRepository: enabled cache via QueryHints

--- leoz-node/src/main/java/org/deku/leoz/node/data/repositories/master/RouteRepository.kt
+++ leoz-node/src/main/java/org/deku/leoz/node/data/repositories/master/RouteRepository.kt
@@ -3,8 +3,11 @@ package org.deku.leoz.node.data.repositories.master
 import com.querydsl.core.types.Predicate
 import org.deku.leoz.node.data.entities.MstRoute
 import org.deku.leoz.node.data.repositories.master.custom.RouteRepositoryCustom
+import org.eclipse.persistence.config.CacheUsage
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.data.querydsl.QueryDslPredicateExecutor
+import javax.persistence.QueryHint
 
 /**
  * Created by masc on 16.05.15.
@@ -13,10 +16,8 @@ interface RouteRepository :
         JpaRepository<MstRoute, Long>,
         QueryDslPredicateExecutor<MstRoute>,
         RouteRepositoryCustom {
-    // TODO: preliminary test for applying jpa query hints to spring-data queries. don't remove (just yet)
-    //    @QueryHints(value = {
-    //            @QueryHint(name = org.eclipse.persistence.config.QueryHints.QUERY_RESULTS_CACHE, value = "true"),
-    //            @QueryHint(name = org.eclipse.persistence.config.QueryHints.QUERY_RESULTS_CACHE_SIZE, value = "500")
-    //    }, forCounting = false)
+    @QueryHints(
+            QueryHint(name = org.eclipse.persistence.config.QueryHints.CACHE_USAGE, value = CacheUsage.CheckCacheThenDatabase)
+    )
     override fun findAll(predicate: Predicate): Iterable<MstRoute>
 }
