b6591d63cc1d25cd10c14ee7ef95888556ff8ae3
masc <masc@disappear.de>
2016-10-26 16:57:40 +0200

    ADDED first draft of ArtemisConfiguration

---
+++ leoz-common/src/main/java/org/deku/leoz/config/ArtemisConfiguration.kt
@@ -0,0 +1,47 @@
+package org.deku.leoz.config
+
+import org.apache.activemq.artemis.api.core.TransportConfiguration
+import org.apache.activemq.artemis.api.jms.ActiveMQJMSClient
+import org.apache.activemq.artemis.api.jms.JMSFactoryType
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory
+import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants
+import org.apache.activemq.artemis.jms.client.ActiveMQQueue
+import org.springframework.jms.connection.CachingConnectionFactory
+import sx.io.serialization.KryoSerializer
+import sx.io.serialization.gzip
+import sx.jms.Channel
+import sx.jms.converters.DefaultConverter
+
+/**
+ * Created by masc on 05/10/2016.
+ */
+object ArtemisConfiguration {
+    // Leoz broker configuration only has a single user which is defined here
+    val USERNAME = "leoz"
+    val PASSWORD = "iUbmQRejRI1P3SNtzwIM7wAgNazURPcVcBU7SftyZ0oha9FlnAdGAmXdEQwYlKFC"
+
+    val connectionFactory: CachingConnectionFactory by lazy {
+        val transportParams = mutableMapOf<String, Any>()
+        transportParams.put(TransportConstants.HOST_PROP_NAME, "0.0.0.0")
+        transportParams.put(TransportConstants.PORT_PROP_NAME, 61616)
+
+        val transportConfiguration = TransportConfiguration(NettyConnectorFactory::class.java.getName(), transportParams)
+
+        val cf = ActiveMQJMSClient.createConnectionFactoryWithoutHA(JMSFactoryType.CF, transportConfiguration)
+                .setUser(USERNAME)
+                .setPassword(PASSWORD)
+
+        CachingConnectionFactory(cf)
+    }
+
+    val centralLogQueue: Channel.Configuration by lazy {
+        val c = Channel.Configuration(
+                connectionFactory = this.connectionFactory,
+                destination = ActiveMQQueue("leoz.log.queue"),
+                deliveryMode = Channel.DeliveryMode.Persistent,
+                converter = DefaultConverter(KryoSerializer().gzip))
+
+        c.priority = 1
+        c
+    }
+}
