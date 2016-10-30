bfab14b40f8d1fbdb638b01706f406d9d2f62bce
masc <masc@disappear.de>
2016-10-31 00:30:21 +0100

    UPDATED ArtemisBorker: added first draft of cluster configuration

--- src/main/java/sx/jms/artemis/ArtemisBroker.kt
+++ src/main/java/sx/jms/artemis/ArtemisBroker.kt
@@ -1,19 +1,18 @@
 package sx.jms.artemis
 
 import org.apache.activemq.artemis.api.core.TransportConfiguration
+import org.apache.activemq.artemis.core.config.ClusterConnectionConfiguration
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl
-import org.apache.activemq.artemis.core.config.impl.SecurityConfiguration
 import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory
 import org.apache.activemq.artemis.core.remoting.impl.netty.TransportConstants
 import org.apache.activemq.artemis.core.security.Role
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ
-import org.apache.activemq.artemis.jms.server.config.impl.ConnectionFactoryConfigurationImpl
+import org.apache.activemq.artemis.core.settings.impl.AddressSettings
 import org.apache.activemq.artemis.jms.server.config.impl.JMSConfigurationImpl
 import org.apache.activemq.artemis.jms.server.embedded.EmbeddedJMS
-import org.apache.activemq.artemis.spi.core.security.ActiveMQJAASSecurityManager
 import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManagerImpl
 import sx.jms.Broker
-import java.util.*
 
 /**
  * Created by masc on 05/10/2016.
@@ -31,10 +30,15 @@ class ArtemisBroker : Broker(NATIVE_TCP_PORT) {
 
     override fun startImpl() {
         // Setup acceptors
-        val nettyAcceptorParams = mutableMapOf<String, Any>()
-        nettyAcceptorParams.put(TransportConstants.HOST_PROP_NAME, "0.0.0.0")
-        nettyAcceptorParams.put(TransportConstants.PORT_PROP_NAME, 61616)
-        nettyAcceptorParams.put(TransportConstants.USE_NIO_PROP_NAME, true)
+        val brokerAcceptorParams = mutableMapOf<String, Any>()
+        brokerAcceptorParams.put(TransportConstants.HOST_PROP_NAME, "0.0.0.0")
+        brokerAcceptorParams.put(TransportConstants.PORT_PROP_NAME, this.nativeTcpPort ?: NATIVE_TCP_PORT)
+        brokerAcceptorParams.put(TransportConstants.USE_NIO_PROP_NAME, true)
+
+        val brokerConnectorParams = mutableMapOf<String, Any>()
+        brokerConnectorParams.put(TransportConstants.HOST_PROP_NAME, "192.168.0.215")
+        brokerConnectorParams.put(TransportConstants.PORT_PROP_NAME, this.nativeTcpPort ?: NATIVE_TCP_PORT)
+        val BROKER_CONNECTOR_NAME = "broker-connector"
 
         // Setup security/authentication
         val adminRole = Role("admin", true, true, true, true, true, true, true, true)
@@ -54,12 +58,38 @@ class ArtemisBroker : Broker(NATIVE_TCP_PORT) {
                 .putSecurityRoles("#", setOf(adminRole))
                 .addAcceptorConfiguration(
                         TransportConfiguration(
-                                NettyAcceptorFactory::class.java.getName(), nettyAcceptorParams))
+                                NettyAcceptorFactory::class.java.getName(), brokerAcceptorParams))
 
-//                .addConnectorConfiguration("myConnector",
-//                        TransportConfiguration(
-//                                NettyAcceptorFactory::class.java.getName()))
+                .addConnectorConfiguration(BROKER_CONNECTOR_NAME,
+                        TransportConfiguration(
+                                NettyConnectorFactory::class.java.getName(), brokerConnectorParams))
 
+        val staticConnectors = this.peerBrokers.map {
+            val peerConnectorParams = mutableMapOf<String, Any>()
+            peerConnectorParams.put(TransportConstants.HOST_PROP_NAME, it.hostname)
+            peerConnectorParams.put(TransportConstants.PORT_PROP_NAME, it.port ?: NATIVE_TCP_PORT)
+
+            val PEER_CONNECTOR_NAME = "connector.peer.${it.hostname}"
+            configuration
+                    .addConnectorConfiguration(PEER_CONNECTOR_NAME,
+                            TransportConfiguration(
+                                    NettyConnectorFactory::class.java.getName(),
+                                    peerConnectorParams)
+                    )
+            PEER_CONNECTOR_NAME
+        }
+
+        configuration.addClusterConfiguration(
+                ClusterConnectionConfiguration()
+                        .setName("leoz-cluster")
+                        .setConnectorName(BROKER_CONNECTOR_NAME)
+                        .setStaticConnectors(staticConnectors)
+                //.setAllowDirectConnectionsOnly(true)
+        )
+
+        configuration.addAddressesSetting("jms.#",
+                AddressSettings()
+                        .setRedistributionDelay(0))
 
         // Setup JMS configuration
         val jmsConfig = JMSConfigurationImpl()
