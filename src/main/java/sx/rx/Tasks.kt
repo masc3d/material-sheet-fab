735ec8e01b09043b930d56cb680fe5cd2481caeb
masc <masc@disappear.de>
2016-11-16 21:12:36 +0100

    UPDATED leoz-boot: split model and controller

---
+++ src/main/java/sx/rx/Tasks.kt
@@ -0,0 +1,20 @@
+package sx.rx
+
+import rx.Observable
+
+/**
+ * RX Task
+ * Created by masc on 16/11/2016.
+ */
+fun <T> task(block: (observer: (onNext: T) -> Unit) -> Unit): Observable<T> {
+    return Observable.create<T> { sub ->
+        try {
+            block {
+                sub.onNext(it)
+            }
+            sub.onCompleted()
+        } catch(e: Throwable) {
+            sub.onError(e)
+        }
+    }
+}
