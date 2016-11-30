a6b7dc82ad9aa502890b7a8d367e5946902d0241
masc <masc@disappear.de>
2016-11-30 18:03:21 +0100

    UPDATED leoz-ui: added support for bundle process interface start/stop operations

--- src/main/java/sx/Process.kt
+++ src/main/java/sx/Process.kt
@@ -1,5 +1,6 @@
 package sx
 
+import org.apache.commons.lang3.SystemUtils
 import java.lang.management.ManagementFactory
 import java.lang.management.RuntimeMXBean
 
@@ -20,6 +21,16 @@ class Process private constructor(
 
             Process(pid = pid,
                     name = name)
+        }
+
+        /**
+         * Signals termination to a process
+         */
+        fun kill(pid: Long) {
+            when {
+                SystemUtils.IS_OS_WINDOWS -> { ProcessExecutor.run(listOf("taskkill", "/pid", pid.toString())) }
+                else -> { ProcessExecutor.run(listOf("kill", pid.toString())) }
+            }
         }
     }
 }
