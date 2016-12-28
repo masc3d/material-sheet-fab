189fea92753f0eb24185fa298e00da66f1298e4c
masc <masc@disappear.de>
2016-12-28 18:23:34 +0100

    UPDATED leoz-mobile: prepared serialization (performance) tests for android instrumentation

---
+++ leoz-mobile/leoz-mobile-app/src/androidTest/java/org/deku/leoz/mobile/rest/SerializationTest.kt
@@ -0,0 +1,8 @@
+package org.deku.leoz.mobile.rest
+
+/**
+ * Created by n3 on 13/12/2016.
+ */
+class SerializationTest {
+
+}
---
+++ leoz-mobile/leoz-mobile-app/src/androidTest/java/org/deku/leoz/mobile/serialization/KryoSerializerTest.kt
@@ -0,0 +1,35 @@
+package sx.io.serialization
+
+import org.junit.Test
+
+/**
+ * Created by masc on 13/09/16.
+ */
+class KryoSerializerTest : SerializerTest() {
+    private val _serializer = KryoSerializer()
+
+    @Test
+    fun testSerialization() {
+        super.testSerialization(_serializer)
+    }
+
+    @Test
+    fun testRefactoring() {
+        super.testRefactoring(_serializer)
+    }
+
+    @Test
+    fun testArraySerialization() {
+        super.testArraySerialization(_serializer)
+    }
+
+    @Test
+    fun testArrayRefactoring() {
+        super.testArrayRefactoring(_serializer)
+    }
+
+//    @Test
+//    fun testObjectArraySerialization() {
+//        super.testObjectArraySerialization(_serializer)
+//    }
+}
