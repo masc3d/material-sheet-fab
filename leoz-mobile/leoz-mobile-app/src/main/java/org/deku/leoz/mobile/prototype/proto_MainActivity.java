84d749dcada3411beff4f61e557e1148082f9510
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-02-02 09:23:37 +0100

    UI Tests

---
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/prototype/proto_MainActivity.java
@@ -0,0 +1,15 @@
+package org.deku.leoz.mobile.prototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.deku.leoz.mobile.R;
+
+public class proto_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proto__main);
+    }
+}
