d60d27228acf3bac69c829a88dcd1969439fbf89
masc <masc@disappear.de>
2017-02-13 12:43:46 +0100

    UPDATED UpdateService: reduced logging noise

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/update/UpdateService.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/update/UpdateService.kt
@@ -3,6 +3,8 @@ package org.deku.leoz.mobile.update
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.conf.global
 import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.lazy
+import org.deku.leoz.rest.service.internal.v1.BundleService
 import org.slf4j.LoggerFactory
 import sx.concurrent.Service
 import sx.time.seconds
@@ -20,7 +22,16 @@ class UpdateService(executorService: ScheduledExecutorService) : Service(
 ) {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    private val bundleService: BundleService by Kodein.global.lazy.instance()
+
     override fun run() {
         log.info("Update cycle")
+
+        try {
+            val updateInfo = this.bundleService.info("leoz-mobile", "snapshot")
+            log.info("${updateInfo}")
+        } catch(e: Exception) {
+            log.error(e.message)
+        }
    }
 }
