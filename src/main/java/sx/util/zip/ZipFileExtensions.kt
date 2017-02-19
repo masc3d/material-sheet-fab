4461dfacce9020ec261e4cd15686fddda87120c0
masc <masc@disappear.de>
2017-02-19 18:09:27 +0100

    ADDED sx.util.zip.ZipFileExtensions

---
+++ src/main/java/sx/util/zip/ZipFileExtensions.kt
@@ -0,0 +1,27 @@
+package sx.util.zip
+
+import org.slf4j.LoggerFactory
+import java.io.FileInputStream
+import java.io.IOException
+import java.util.zip.ZipException
+import java.util.zip.ZipFile
+import java.util.zip.ZipInputStream
+
+/**
+ * Verify zip archive
+ * Created by masc on 19/02/2017.
+ */
+fun ZipFile.verify() {
+    val log = LoggerFactory.getLogger(this.javaClass)
+
+    val entries = this.entries()
+    while (entries.hasMoreElements()) {
+        val entry = entries.nextElement()
+        val stream = this.getInputStream(entry)
+        val crc = entry.crc
+        val compressedSize = entry.compressedSize
+        val name = entry.name
+
+        log.debug("Verifying [${name}] crc [${crc}] compressedSize [${compressedSize}]")
+    }
+}
