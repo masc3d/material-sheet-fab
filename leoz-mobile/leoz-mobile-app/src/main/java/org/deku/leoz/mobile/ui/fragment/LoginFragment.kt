dbb48964c21b897cf9bcc6709775464467dfc1fe
masc <masc@disappear.de>
2017-02-27 12:21:09 +0100

    UPDATED leoz-mobile: show serial number

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/fragment/LoginFragment.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/fragment/LoginFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.conf.global
+import com.github.salomonbrys.kodein.instance
 import com.trello.rxlifecycle.components.support.RxAppCompatDialogFragment
+import kotlinx.android.synthetic.main.fragment_login.*
 import org.deku.leoz.mobile.R
+import sx.android.Device
 
 /**
  * Created by n3 on 26/02/2017.
@@ -19,5 +24,8 @@ class LoginFragment : RxAppCompatDialogFragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val device: Device = Kodein.global.instance()
+        this.uxSerialnumber.text = device.serial
     }
 }
