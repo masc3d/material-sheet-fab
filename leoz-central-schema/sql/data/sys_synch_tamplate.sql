4ca6b7362b8ae4195b37e1c012433f2a5b4c72b4
masc <masc@disappear.de>
2017-03-19 12:24:15 +0100

    FIXED leoz-central-schema: missing `sys_sync` entries

--- leoz-central-schema/sql/data/sys_synch_tamplate.sql
+++ leoz-central-schema/sql/data/sys_synch_tamplate.sql
@@ -8,6 +8,8 @@ ALTER TABLE `XXX`
   ADD COLUMN `sync_id` BIGINT NOT NULL DEFAULT 0,
   ADD INDEX `ix_sync_id` (`sync_id`) $$
 
+INSERT INTO `sys_sync` (`table_name`) VALUES ('XXX');
+
 CREATE DEFINER = CURRENT_USER TRIGGER `XXX_sync_insert` BEFORE INSERT ON `XXX` FOR EACH ROW
   BEGIN
     SET NEW.sync_id = f_sync_increment('XXX');
