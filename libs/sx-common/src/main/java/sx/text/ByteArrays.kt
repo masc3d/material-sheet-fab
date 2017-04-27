ab83d281192781cd02e8e09667647fbbcc002311
masc <masc@disappear.de>
2017-04-27 12:59:12 +0200

    ADDED sx-common: support method(s) for hex strings, digests

---
+++ libs/sx-common/src/main/java/sx/text/ByteArrays.kt
@@ -0,0 +1,31 @@
+package sx.text
+
+/**
+ *  Extension functions to format bytes as Hex values.
+ */
+
+/**
+ *  Set of chars for a half-byte.
+ */
+private val CHARS = arrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+
+/**
+ *  Returns the string of two characters representing the HEX value of the byte.
+ */
+fun Byte.toHexString(): String {
+    val i = this.toInt()
+    val char2 = CHARS[i and 0x0f]
+    val char1 = CHARS[i shr 4 and 0x0f]
+    return "$char1$char2"
+}
+
+/**
+ *  Returns the HEX representation of ByteArray data.
+ */
+fun ByteArray.toHexString(): String {
+    val builder = StringBuilder()
+    for (b in this) {
+        builder.append(b.toHexString())
+    }
+    return builder.toString()
+}
