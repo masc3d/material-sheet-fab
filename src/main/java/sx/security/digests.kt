f4b21a5d3bc004a26bc1b119e6dcd9c0cff5c035
masc <masc@disappear.de>
2017-04-27 12:59:12 +0200

    ADDED sx-common: support method(s) for hex strings, digests

---
+++ src/main/java/sx/security/digests.kt
@@ -0,0 +1,9 @@
+package sx.security
+
+/**
+ * Algorithm name constants for use with {@link java.security.MessageDigest}
+ * Created by n3 on 27.04.17.
+ */
+object Algorithms {
+    val SHA256 = "SHA256"
+}
