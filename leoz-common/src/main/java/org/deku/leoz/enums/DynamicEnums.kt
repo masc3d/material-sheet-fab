fed711ec101e6d2c13e9e28854cb46a976d87032
Helke <hw@dr-t.de>
2017-04-28 13:21:05 +0200

    Common enum classes

---
+++ leoz-common/src/main/java/org/deku/leoz/enums/DynamicEnums.kt
@@ -0,0 +1,8 @@
+package org.deku.leoz.enums
+
+/**
+ * Created by helke on 28.04.17.
+ * on changes code changes NOT necessary
+ */
+enum class DynamicEnums {
+}
---
+++ leoz-common/src/main/java/org/deku/leoz/enums/StaticEnums.kt
@@ -0,0 +1,9 @@
+package org.deku.leoz.enums
+
+/**
+ * Created by helke on 28.04.17.
+ * on changes code changes necessary
+ */
+enum class StaticEnums {
+
+}
