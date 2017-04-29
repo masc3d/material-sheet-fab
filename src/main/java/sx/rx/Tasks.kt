b8ad4833d9f25af19bab9922789cfa2a3d29e71b
masc <masc@disappear.de>
2017-04-29 15:35:09 +0200

    FIXED sx-common rx overloads

--- src/main/java/sx/rx/Tasks.kt
+++ src/main/java/sx/rx/Tasks.kt
@@ -1,9 +1,12 @@
 package sx.rx
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.rxkotlin.cast
+import io.reactivex.schedulers.Schedulers
 
 /**
- * RX Task
+ * Task based on rx
  * Created by masc on 16/11/2016.
  */
 fun <T> task(block: (observer: (onNext: T) -> Unit) -> Unit): Observable<T> {
