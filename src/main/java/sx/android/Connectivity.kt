0435f105d4c87f569271a2ddfdf0a7db48f2c571
masc <masc@disappear.de>
2017-05-03 15:48:01 +0200

    MOVED connectivity related funtionality from `Device` to `Connectivity` (there’s more connection related stuff to come)

---
+++ src/main/java/sx/android/Connectivity.kt
@@ -0,0 +1,35 @@
+package sx.android
+
+import android.content.Context
+
+/**
+ * Created by n3 on 03.05.17.
+ */
+class Connectivity(
+        private val context: Context) {
+
+    /**
+     * Android's connectivity manager
+     */
+    private val connectivityManager by lazy { this.context.getConnectivityManager() }
+
+    /**
+     * Indicates if there's connectivity
+     */
+    val isEstablished: Boolean
+        get() {
+            val connectivityManager = this.context.getConnectivityManager()
+
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+
+            if (activeNetworkInfo?.isConnectedOrConnecting ?: false) {
+                return true
+            }
+
+            // TODO: comment why it's not sufficient to check active network & deprecated property is used
+            if (connectivityManager.allNetworkInfo.firstOrNull { it.isConnectedOrConnecting } != null)
+                return true
+
+            return false
+        }
+}
