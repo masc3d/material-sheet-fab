65f8ef0f8c0dfb310a366edc3a1412d1bf6830fe
Helke <hw@dr-t.de>
2017-05-08 15:47:59 +0200

    ADD business objects

---
+++ leoz-common/src/main/java/org/deku/leoz/model/ScanObject.kt
@@ -0,0 +1,41 @@
+package org.deku.leoz.model
+
+/**
+ * Created by helke on 08.05.17.
+ */
+
+sealed class ScanSymbology {
+    class Code128 : ScanSymbology()
+    class DataMatrix : ScanSymbology()
+    class Interleaved2of5 : ScanSymbology()
+    class QRcode : ScanSymbology()
+    class unknown : ScanSymbology()
+}
+sealed class ScanObjectType{
+    class Seal(val sealType:SealType):ScanObjectType()
+    class ActioUser:ScanObjectType()
+    class PrinterId:ScanObjectType()
+    class LineScanId:ScanObjectType()
+    class UnitDeku:ScanObjectType()
+    class UnitGLS(val dekuNo:Double):ScanObjectType()
+    class BagID:ScanObjectType()
+    class BagUnitNo:ScanObjectType()
+    class BagBackUnitNo:ScanObjectType()
+}
+sealed class SealType{
+    class mainSeal:SealType()
+    class backSeal:SealType()
+    class reserveSeal:SealType()
+}
+data class ScanObject(val scanObjectType: ScanObjectType)
+
+fun parse(scanData:String, scanSymbology:ScanSymbology):ScanObject?{
+
+    when(scanSymbology){
+        is ScanSymbology.Interleaved2of5 -> {
+            //toDo checkdigit, gls-checkdigit, number ranges
+            //val so=ScanObject(ScanObjectType.UnitDeku)
+            //return so
+        }
+    }
+}
