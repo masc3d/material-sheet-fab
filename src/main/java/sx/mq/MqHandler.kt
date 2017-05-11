dc538f0e890552a6fb30bbb167ad557325842c4d
masc <masc@disappear.de>
2017-05-11 22:53:10 +0200

    [libs-sx-mq] UPDATED sx-mq: generalized Listener class hierarchy in preparation for mqtt listener

--- src/main/java/sx/mq/MqHandler.kt
+++ src/main/java/sx/mq/MqHandler.kt
@@ -5,6 +5,7 @@ package sx.mq
  * Created by masc on 28.06.15.
  */
 interface MqHandler<in T> {
+    // TODO: migrate to method annotation for `onMessage`. much better.
     /** Message types to handle. Must be overridden when multiple message types should be handled (Handler<Anx>) */
     val messageTypes: List<Class<*>> get() {
         return arrayListOf()
