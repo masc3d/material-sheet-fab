4064eebc5a8109c9869860258d494f79f8e3b249
masc <masc@disappear.de>
2017-05-18 01:24:59 +0200

    [libs-sx-android] UPDATED MqttClientPersistenceSQLite: improved interface conformance, should rethrow MqttPersistenceException on all occasions

--- src/main/java/sx/android/mqtt/MqttClientPersistenceSQLite.kt
+++ src/main/java/sx/android/mqtt/MqttClientPersistenceSQLite.kt
@@ -46,84 +46,111 @@ class MqttClientPersistenceSQLite constructor(
                         Pair(COL_DATA, BLOB)))
     }
 
-    override fun clear() {
-        this.db.delete(TABLE_NAME)
-    }
-
-    override fun put(key: String, persistable: MqttPersistable) {
-        val data = ByteArray(persistable.headerLength + persistable.payloadLength)
-
-        // Copy header to buffer
-        System.arraycopy(
-                persistable.headerBytes,
-                persistable.headerOffset,
-                data,
-                0,
-                persistable.headerLength)
-
-        // Copy payload to buffer
-        System.arraycopy(
-                persistable.payloadBytes,
-                persistable.payloadOffset,
-                data,
-                persistable.headerLength,
-                persistable.payloadLength)
-
-        db.insertOrThrow(
-                tableName = TABLE_NAME,
-                values = *arrayOf(Pair(COL_KEY, key), Pair(COL_DATA, data)))
-    }
-
-    override fun open(clientId: String, serverURI: String) {
-        // Creates/open connection to SQLite by retrieving lazy instance
-        this.dbInstance.get()
-    }
-
-    override fun remove(key: String) {
-        this.db.delete(
-                tableName = TABLE_NAME,
-                whereClause = "${COL_KEY} = {key}",
-                args = COL_KEY to key)
-    }
-
-    override fun get(key: String): MqttPersistable {
-        val data = this.db.select(
-                tableName = TABLE_NAME,
-                columns = COL_DATA
-        )
-                .whereSimple(
-                        select = "${COL_KEY} = ?",
-                        args = key
-                )
-                .parseOpt(BlobParser) ?: throw MqttPersistenceException()
-
-        return MqttPersistentData(key, data, 0, data.size, null, 0, 0)
-    }
-
-    override fun containsKey(key: String): Boolean {
-        return this.db.select(
-                tableName = TABLE_NAME,
-                columns = COL_KEY
-        )
-                .whereSimple(select = "${COL_KEY} = ?", args = key)
-                .parseOpt(StringParser) != null
-
-    }
-
-    override fun close() {
-        this.dbInstance.ifSet {
-            it.close()
-            this.dbInstance.reset()
+    private fun <T> rethrow(block: () -> T): T {
+        return try {
+            block()
+        } catch(e: Throwable) {
+            throw MqttPersistenceException(e)
         }
     }
 
+    override fun clear() {
+        log.trace("Clearing store")
+        rethrow {
+            this.db.delete(TABLE_NAME)
+        }
+    }
+
+    override fun put(key: String, persistable: MqttPersistable) {
+        log.trace("Storing message [${key}]")
+
+        rethrow {
+            val data = ByteArray(persistable.headerLength + persistable.payloadLength)
+
+            // Copy header to buffer
+            System.arraycopy(
+                    persistable.headerBytes,
+                    persistable.headerOffset,
+                    data,
+                    0,
+                    persistable.headerLength)
+
+            // Copy payload to buffer
+            System.arraycopy(
+                    persistable.payloadBytes,
+                    persistable.payloadOffset,
+                    data,
+                    persistable.headerLength,
+                    persistable.payloadLength)
+
+            db.insertOrThrow(
+                    tableName = TABLE_NAME,
+                    values = *arrayOf(Pair(COL_KEY, key), Pair(COL_DATA, data)))
+        }
+    }
+
+    override fun open(clientId: String, serverURI: String) {
+        log.trace("Opening store for [${clientId}]")
+
+        rethrow {
+            // Creates/open connection to SQLite by retrieving lazy instance
+            this.dbInstance.get()
+        }
+    }
+
+    override fun remove(key: String) {
+        log.trace("Removing key [${key}]")
+
+        rethrow {
+            this.db.delete(
+                    tableName = TABLE_NAME,
+                    whereClause = "${COL_KEY} = {key}",
+                    args = COL_KEY to key)
+        }
+    }
+
+    override fun get(key: String): MqttPersistable {
+        log.trace("Retrieving key [${key}]")
+
+        return rethrow {
+            val data = this.db.select(
+                    tableName = TABLE_NAME,
+                    columns = COL_DATA
+            )
+                    .whereSimple(
+                            select = "${COL_KEY} = ?",
+                            args = key
+                    )
+                    .parseOpt(BlobParser) ?: throw MqttPersistenceException()
+
+            MqttPersistentData(key, data, 0, data.size, null, 0, 0)
+        }
+    }
+
+    override fun containsKey(key: String): Boolean {
+        return rethrow {
+            this.db.select(
+                    tableName = TABLE_NAME,
+                    columns = COL_KEY
+            )
+                    .whereSimple(select = "${COL_KEY} = ?", args = key)
+                    .parseOpt(StringParser) != null
+        }
+    }
+
+    override fun close() {
+        // Never closing store, keep database open
+    }
+
     override fun keys(): Enumeration<*> {
-        return Vector(
-                this.db.select(
-                        tableName = TABLE_NAME,
-                        columns = COL_KEY)
-                        .parseList(StringParser)
-        )
-                .elements()
+        return rethrow {
+            Vector(
+                    this.db.select(
+                            tableName = TABLE_NAME,
+                            columns = COL_KEY)
+                            .parseList(StringParser)
+            )
+                    .elements()
+        }
     }
 }
