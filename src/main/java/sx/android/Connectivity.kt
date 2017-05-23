290abfe5e8b150eff77a66bcc3650b31662e57b5
masc <masc@disappear.de>
2017-05-23 22:00:02 +0200

    [libs-sx-android] FIXED Conectivity: should run on single threaded io scheduler to prevent threading issues (executorservice may offer various threads from a pool)

--- src/main/java/sx/android/Connectivity.kt
+++ src/main/java/sx/android/Connectivity.kt
@@ -5,6 +5,7 @@ import com.github.pwittchen.reactivenetwork.library.rx2.Connectivity
 import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
 import io.reactivex.Scheduler
 import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 import org.slf4j.LoggerFactory
 import sx.rx.ObservableRxProperty
 
@@ -13,8 +14,7 @@ import sx.rx.ObservableRxProperty
  * Created by n3 on 03.05.17.
  */
 class Connectivity(
-        private val context: Context,
-        private val scheduler: Scheduler) {
+        private val context: Context) {
 
     private val log = LoggerFactory.getLogger(this.javaClass)
 
@@ -30,7 +30,7 @@ class Connectivity(
 
     init {
         ReactiveNetwork.observeNetworkConnectivity(context)
-                .subscribeOn(scheduler)
+                .subscribeOn(Schedulers.io())
                 .subscribeBy(onNext = {
                     log.info("Network connectivity state change [${it}]")
                     this.network = it
