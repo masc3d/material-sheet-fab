e569469b8179a37a3a0d64f09c9f5183733afcef
masc <masc@disappear.de>
2017-05-23 20:30:35 +0200

    UPDATED sx.android.Connectivity: replaced custom implementation with ReactiveNetwork

--- libs/sx-android/src/main/java/sx/android/Connectivity.kt
+++ libs/sx-android/src/main/java/sx/android/Connectivity.kt
@@ -3,48 +3,33 @@ package sx.android
 import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
+import com.github.pwittchen.reactivenetwork.library.rx2.Connectivity
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
+import io.reactivex.Scheduler
+import io.reactivex.rxkotlin.subscribeBy
+import org.slf4j.LoggerFactory
+import sx.rx.ObservableRxProperty
 
 /**
+ * Connectivity singleton
  * Created by n3 on 03.05.17.
  */
 class Connectivity(
-        private val context: Context) {
+        private val context: Context,
+        private val scheduler: Scheduler) {
 
-    /**
-     * Android's connectivity manager
-     */
-    private val connectivityManager by lazy { this.context.getConnectivityManager() }
+    private val log = LoggerFactory.getLogger(this.javaClass)
 
-    /**
-     * Indicates if there's connectivity
-     */
-    val isEstablished: Boolean
-        get() {
-            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+    val stateProperty = ObservableRxProperty<Connectivity>(Connectivity.create())
+    var state by stateProperty
+        private set
 
-            if (activeNetworkInfo?.isConnectedOrConnecting ?: false) {
-                return true
-            }
-
-            /**
-             * If active network is not connected nor a connection is pending, check other networks,
-             * cause they could be taken from the ConnectionManager for failover. So they might are connecting right now.
-             *
-             * <getAllNetworks> is only available from API level 21. Project targets level 16
-             * See https://developer.android.com/reference/android/net/ConnectivityManager.html#pubmethods for more details
-             */
-            if (android.os.Build.VERSION.SDK_INT >= 21) {
-                if (connectivityManager.allNetworks.firstOrNull { connectivityManager.getNetworkInfo(it).isConnectedOrConnecting } != null) {
-                    return true
-                }
-            } else {
-                @Suppress("DEPRECATION")
-                if (connectivityManager.allNetworkInfo.firstOrNull { it.isConnectedOrConnecting } != null) {
-                    return true
-                }
-            }
-
-
-            return false
-        }
+    init {
+        ReactiveNetwork.observeNetworkConnectivity(context)
+                .subscribeOn(scheduler)
+                .subscribeBy(onNext = {
+                    log.info("Network connectivity state change [${it}]")
+                    state = it
+                })
+    }
 }
