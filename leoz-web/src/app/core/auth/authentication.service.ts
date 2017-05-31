2503b80aeb2e22287bb5b98b1bbae47725986003
Janussek <info@it-cobra.es>
2017-05-31 10:31:52 +0200

    minor cleanups

--- leoz-web/src/app/core/auth/authentication.service.ts
+++ leoz-web/src/app/core/auth/authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
-import { RequestOptions, Headers } from '@angular/http';
+import { RequestOptions } from '@angular/http';
 import { environment } from '../../../environments/environment';
 import { RoleGuard } from './role.guard';
 import { ApiKeyHeaderFactory } from '../api-key-header.factory';
