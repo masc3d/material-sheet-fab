1af5919762650b5cb36096110de36e73262eaf81
Janussek <info@it-cobra.es>
2017-06-01 12:33:47 +0200

    msgcomponent remove external html template file

--- leoz-web/src/app/shared/msg/msg-box.component.ts
+++ leoz-web/src/app/shared/msg/msg-box.component.ts
@@ -3,7 +3,9 @@ import { Msg } from './msg.model';
 
 @Component({
   selector: 'app-msg-box',
-  templateUrl: './msg-box.component.html'
+  template: `
+    <div *ngIf="msg.text.length > 0" class="alert" [ngClass]="msg.alertStyle">{{msg.text | translate}}</div>
+  `
 })
 export class MsgBoxComponent {
 
