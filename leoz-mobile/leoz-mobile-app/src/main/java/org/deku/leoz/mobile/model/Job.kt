f938b06088c8f9fb14c9ff35497a624aa9b36cbe
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-06-02 13:53:00 +0200

    Fixed: Wrong property names

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/model/Job.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/model/Job.kt
@@ -57,92 +57,122 @@ class Job {
     /**
      *
      */
-    fun queryDeliveryList(listId: String): Boolean {
+    fun queryDeliveryList(listId: String): List<Order> {
         try{
             val deliveryList: DeliveryListService.DeliveryList = deliveryListService.getById(id = listId)
 
             if (deliveryList.orders.isEmpty()) {
-                return false
+                return listOf()
             }
 
-            orderList.addAll(deliveryList.orders
-                    .map {
-                        Order(
-                                id =  it.orderID.toString(),
-                                state = Order.State.PENDING,
-                                classification =  it.orderClassification,
-                                parcel = it.Parcels.map {
-                                    Order.Parcel(
-                                            id = it.parcelID.toString(),
-                                            labelReference = it.parcelScanNumber,
-                                            status = null,
-                                            length = it.dimension?.length?.toFloat() ?: 0.0F,
-                                            height = it.dimension?.height?.toFloat() ?: 0.0F,
-                                            width = it.dimension?.width?.toFloat() ?: 0.0F,
-                                            weight = it.dimension?.weight?.toFloat() ?: 0.0F
-                                    )
-                                },
-                                addresses = mutableListOf(
-                                        Order.Address(
-                                                Order.Address.Classification.DELIVERY,
-                                                it.deliveryAddress.addressLine1,
-                                                it.deliveryAddress.addressLine2 ?: "",
-                                                it.deliveryAddress.street,
-                                                it.deliveryAddress.streetNo ?: "",
-                                                it.deliveryAddress.zipCode,
-                                                it.deliveryAddress.city,
-                                                Pair(it.deliveryAddress.geoLocation?.latitude ?: 0.0, it.deliveryAddress.geoLocation?.longitude ?: 0.0)
-                                        ),
-                                        Order.Address(
-                                                Order.Address.Classification.PICKUP,
-                                                it.pickupAddress.addressLine1,
-                                                it.pickupAddress.addressLine2 ?: "",
-                                                it.pickupAddress.street,
-                                                it.pickupAddress.streetNo ?: "",
-                                                it.pickupAddress.zipCode,
-                                                it.pickupAddress.city,
-                                                Pair(it.pickupAddress.geoLocation?.latitude ?: 0.0, it.pickupAddress.geoLocation?.longitude ?: 0.0)
-                                        )
-                                ),
-                                appointment = listOf(),
-                                carrier = it.carrier,
-                                service = listOf(
-                                        Order.Service(
-                                                classification = Order.Service.Classification.DELIVERY_SERVICE,
-                                                service = it.deliveryService.services ?: listOf(ParcelService.NoAdditionalService)
-                                        ),
-                                        Order.Service(
-                                                classification = Order.Service.Classification.PICKUP_SERVICE,
-                                                service = it.pickupService.services ?: listOf(ParcelService.NoAdditionalService)
-                                        )
-                                ),
-                                information = listOf(
-                                        Order.Information(
-                                                classification = Order.Information.Classification.DELIVERY_INFO,
-                                                additionalInformation = it.deliveryInformation!!.AdditionalInformation!!.map {
-                                                    Order.Information.AdditionalInformation(
-                                                            type = it.AdditionalInformationType!!,
-                                                            value = it.information ?: ""
-                                                    )
-                                                }
-                                        ),
-                                        Order.Information(
-                                                classification = Order.Information.Classification.PICKUP_INFO,
-                                                additionalInformation = it.pickupInformation!!.AdditionalInformation!!.map {
-                                                    Order.Information.AdditionalInformation(
-                                                            type = it.AdditionalInformationType!!,
-                                                            value = it.information ?: ""
-                                                    )
-                                                }
-                                        )
-                                ),
-                                sort = 0
-                        )
-                    })
+            val mobileOrder = mapServiceOrder(deliveryList.orders)
 
-            return true
+            orderList.addAll(mobileOrder)
+
+            return mobileOrder
         } catch (e: Exception) {
-            return false
+            log.error(e.message)
+            return listOf()
+        }
+    }
+
+    fun queryOrderServiceByLabel(ref: String): List<Order> {
+        try {
+            val order: List<OrderService.Order> = orderService.get(labelRef = ref)
+
+            when (order.size) {
+                0 -> {
+                    return listOf()
+                }
+                1 -> {
+                    orderList.addAll(mapServiceOrder(order))
+                    return mapServiceOrder(order)
+                }
+                else -> {
+                    return mapServiceOrder(order)
+                }
+            }
+        } catch (e: Exception) {
+            log.error(e.message)
+            return listOf()
+        }
+    }
+
+    fun mapServiceOrder(orderList: List<OrderService.Order>): List<Order> {
+        return orderList.map {
+            Order(
+                    id = it.orderID.toString(),
+                    state = Order.State.PENDING,
+                    classification = it.orderClassification,
+                    parcel = it.Parcels.map {
+                        Order.Parcel(
+                                id = it.parcelID.toString(),
+                                labelReference = it.parcelScanNumber,
+                                status = null,
+                                length = it.dimension?.length?.toFloat() ?: 0.0F,
+                                height = it.dimension?.height?.toFloat() ?: 0.0F,
+                                width = it.dimension?.width?.toFloat() ?: 0.0F,
+                                weight = it.dimension?.weight?.toFloat() ?: 0.0F
+                        )
+                    },
+                    addresses = mutableListOf(
+                            Order.Address(
+                                    Order.Address.Classification.DELIVERY,
+                                    it.deliveryAddress.addressLine1,
+                                    it.deliveryAddress.addressLine2 ?: "",
+                                    it.deliveryAddress.street,
+                                    it.deliveryAddress.streetNo ?: "",
+                                    it.deliveryAddress.zipCode,
+                                    it.deliveryAddress.city,
+                                    it.deliveryAddress.geoLocation?.latitude ?: 0.0,
+                                    it.deliveryAddress.geoLocation?.longitude ?: 0.0
+                            ),
+                            Order.Address(
+                                    Order.Address.Classification.PICKUP,
+                                    it.pickupAddress.addressLine1,
+                                    it.pickupAddress.addressLine2 ?: "",
+                                    it.pickupAddress.street,
+                                    it.pickupAddress.streetNo ?: "",
+                                    it.pickupAddress.zipCode,
+                                    it.pickupAddress.city,
+                                    it.pickupAddress.geoLocation?.latitude ?: 0.0,
+                                    it.pickupAddress.geoLocation?.longitude ?: 0.0
+                            )
+                    ),
+                    appointment = listOf(),
+                    carrier = it.carrier,
+                    service = listOf(
+                            Order.Service(
+                                    classification = Order.Service.Classification.DELIVERY_SERVICE,
+                                    service = it.deliveryService.services ?: listOf(ParcelService.NoAdditionalService)
+                            ),
+                            Order.Service(
+                                    classification = Order.Service.Classification.PICKUP_SERVICE,
+                                    service = it.pickupService.services ?: listOf(ParcelService.NoAdditionalService)
+                            )
+                    ),
+                    information = listOf(
+                            Order.Information(
+                                    classification = Order.Information.Classification.DELIVERY_INFO,
+                                    additionalInformation = it.deliveryInformation!!.AdditionalInformation!!.map {
+                                        Order.Information.AdditionalInformation(
+                                                type = it.AdditionalInformationType!!,
+                                                value = it.information ?: ""
+                                        )
+                                    }
+                            ),
+                            Order.Information(
+                                    classification = Order.Information.Classification.PICKUP_INFO,
+                                    additionalInformation = it.pickupInformation!!.AdditionalInformation!!.map {
+                                        Order.Information.AdditionalInformation(
+                                                type = it.AdditionalInformationType!!,
+                                                value = it.information ?: ""
+                                        )
+                                    }
+                            )
+                    ),
+                    sort = 0
+            )
         }
     }
 
