77cc6b37da9fcb705572003dc6909d4ee8cfd9de
masc <masc@disappear.de>
2017-06-06 14:37:38 +0200

    UPDATED User: enum, removed unused fields

--- leoz-common/src/main/java/org/deku/leoz/service/internal/entity/User.kt
+++ leoz-common/src/main/java/org/deku/leoz/service/internal/entity/User.kt
@@ -13,30 +13,21 @@ data class User(
         @get:ApiModelProperty(example = "12345678", required = false, value = "Allocation of User to debitor")
         var debitorId: Int? = null,
 
-        //        @get:ApiModelProperty(required = false, value = "Allocation of User to several station ids")
-//        var stations: List<String>? = null,
-
         @get:ApiModelProperty(example = "foo.bar", required = false, value = "Alias of the user")
         var alias: String? = null,
 
-        @get:ApiModelProperty(example = org.deku.leoz.service.internal.entity.User.Companion.ROLE_USER, required = true, value = "Role of the user", allowableValues = "${org.deku.leoz.service.internal.entity.User.Companion.ROLE_ADMINISTRATOR},${org.deku.leoz.service.internal.entity.User.Companion.ROLE_POWERUSER},${org.deku.leoz.service.internal.entity.User.Companion.ROLE_USER},${org.deku.leoz.service.internal.entity.User.Companion.ROLE_DRIVER},${org.deku.leoz.service.internal.entity.User.Companion.ROLE_CUSTOMER}")
+        @get:ApiModelProperty(example = "User", required = true, value = "Role of the user", allowableValues = "Admin, PowerUser, User, Driver, Custom")
         var role: String? = null,
 
         // TODO: should be hash instead of pw
         @get:ApiModelProperty(example = "MyS3cr3t", required = false, value = "Password")
         var password: String? = null,
 
-        //        @get:ApiModelProperty(example = "a1b2c3d4e5f6", required = true, value = "Salt")
-//        var salt: String? = null,
-
         @get:ApiModelProperty(example = "Foo", required = false, value = "First name")
         var firstName: String? = null,
 
         @get:ApiModelProperty(example = "Bar", required = false, value = "Last name")
         var lastName: String? = null,
-
-        //        @get:ApiModelProperty(example = "1a-2b-3c-4d-5e-6f", required = false, value = "API Key")
-//        var apiKey: String? = null,
 
         @get:ApiModelProperty(example = "true", required = false, value = "Active user")
         var active: Boolean? = null,
@@ -49,24 +40,14 @@ data class User(
 
         @get:ApiModelProperty(example = "2017-03-16T17:00:00.000Z", required = false, value = "Date this account is supposed to expire")
         var expiresOn: java.sql.Date? = null
+)
 
-        //var id: Int = 0
-) {
-    companion object {
-        const val ROLE_ADMINISTRATOR: String = "ADMIN"
-        const val ROLE_POWERUSER: String = "POWERUSER"
-        const val ROLE_USER: String = "USER"
-        const val ROLE_DRIVER: String = "DRIVER"
-        const val ROLE_CUSTOMER: String = "CUSTOMER"
-    }
-}
-
-enum class UserRole(val value: Int) {
-    ADMIN(10),
-    POWERUSER(7),
-    USER(6),
-    DRIVER(4),
-    CUSTOMER(2)
+enum class UserRole(val value: String) {
+    Admin("admin"),
+    PowerUser("power-user"),
+    User("user"),
+    Driver("driver"),
+    Customer("customer")
 }
 
 val User.isActive: Int
