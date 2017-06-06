794e672a0db36a9c3150def76e8509d55b8431fc
masc <masc@disappear.de>
2017-06-06 09:39:28 +0200

    UPDATED leoz-mobile-data migrations: cleaned up formatting, naming

--- leoz-mobile/leoz-mobile-data/src/main/resources/db/migration/V2__order.sql
+++ leoz-mobile/leoz-mobile-data/src/main/resources/db/migration/V2__order.sql
@@ -1,91 +1,77 @@
-create table address
+CREATE TABLE address
 (
-	id int
-		primary key,
-	classification varchar(30) null,
-	order_ref varchar(50) null,
-	addressLine1 varchar(50) null,
-	addressLine2 varchar(50) null,
-	street varchar(50) null,
-	streetNo varchar(50) null,
-	zipCode varchar(50) null,
-	city varchar(50) null,
-	latitude double null,
-	longitude double null
-)
-;
+    id INTEGER PRIMARY KEY,
+	classification VARCHAR NULL,
+	order_ref VARCHAR NULL,
+	address1 VARCHAR NULL,
+	address2 VARCHAR NULL,
+	street VARCHAR NULL,
+	street_no VARCHAR NULL,
+	zip_code VARCHAR NULL,
+	city VARCHAR NULL,
+	latitude DOUBLE NULL,
+	longitude DOUBLE NULL
+);
 
-create table appointment
+CREATE TABLE appointment
 (
-	id int 
-		primary key,
-	order_ref varchar(50) null,
-	classification varchar(30) null,
-	dateFrom timestamp null,
-	dateTo timestamp null
-)
-;
+	id INTEGER PRIMARY KEY,
+	order_ref VARCHAR NULL,
+	classification VARCHAR NULL,
+	date_from timestamp NULL,
+	date_to timestamp NULL
+);
 
-create table information
+CREATE TABLE information
 (
-	id int 
-		primary key,
-	order_ref varchar(50) null,
-	classification varchar(30) null,
-	type varchar(20) null,
-	value varchar(100) null
-)
-;
+	id INTEGER PRIMARY KEY,
+	order_ref VARCHAR NULL,
+	classification VARCHAR NULL,
+	type VARCHAR NULL,
+	value VARCHAR NULL
+);
 
-create table orders
+CREATE TABLE orders
 (
-	id varchar(50) not null
-		primary key,
-	state varchar(10) null,
-	classification varchar(20) null,
-	carrier varchar(20) null,
-	service double null,
-	sort int null
-)
-;
+	id VARCHAR NOT NULL PRIMARY KEY,
+	state VARCHAR NULL,
+	classification VARCHAR NULL,
+	carrier VARCHAR NULL,
+	service DOUBLE NULL,
+	sort int NULL
+);
 
-create table parcel
+CREATE TABLE parcel
 (
-	id varchar(50) not null
-		primary key,
-	labelReference varchar(30) null,
-	length double null,
-	height double null,
-	width double null,
-	weight double null
-)
-;
+	id VARCHAR NOT NULL PRIMARY KEY,
+	label_reference VARCHAR NULL,
+	`length` DOUBLE NULL,
+	height DOUBLE NULL,
+	width DOUBLE NULL,
+	weight DOUBLE NULL
+);
 
-create table status
+CREATE TABLE status
 (
-	id int 
-		primary key,
-	parcel varchar(50) null,
-	event integer null,
-	reason integer null,
-	date timestamp null,
-	latitude double null,
-	longitude double null,
-	recipient varchar(50) null,
-	information varchar(50) null
-)
-;
+	id INTEGER PRIMARY KEY,
+	parcel VARCHAR NULL,
+	event INTEGER NULL,
+	reason INTEGER NULL,
+	`timestamp` TIMESTAMP NULL,
+	latitude DOUBLE NULL,
+	longitude DOUBLE NULL,
+	recipient VARCHAR NULL,
+	information VARCHAR NULL
+);
 
-create table position
+CREATE TABLE gps
 (
-	id int 
-		primary key,
-	latitude double not null,
-	longitude double not null,
-	time timestamp default CURRENT_TIMESTAMP not null,
-	speed double null,
-	bearing double null,
-	altitude double null,
-	accuracy double null
-)
-;
+	id INTEGER PRIMARY KEY,
+	latitude DOUBLE NOT NULL,
+	longitude DOUBLE NOT NULL,
+	`timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	speed DOUBLE NULL,
+	bearing DOUBLE NULL,
+	altitude DOUBLE NULL,
+	accuracy DOUBLE NULL
+);
