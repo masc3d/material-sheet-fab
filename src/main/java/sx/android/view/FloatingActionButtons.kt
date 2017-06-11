e0d7ff8a89291b3de8a8ba6f7f2eea97b7334035
masc <masc@disappear.de>
2017-06-11 15:35:17 +0200

    [libs-sx-android] UPDATED sx.FloatingActionButton extensions: added @ColorRes annotations

--- src/main/java/sx/android/view/FloatingActionButtons.kt
+++ src/main/java/sx/android/view/FloatingActionButtons.kt
@@ -1,5 +1,6 @@
 package sx.android.view
 
+import android.support.annotation.ColorRes
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
 import android.support.v4.graphics.drawable.DrawableCompat
@@ -7,14 +8,14 @@ import android.support.v4.graphics.drawable.DrawableCompat
 /**
  * Set floating action button background tint
  */
-fun FloatingActionButton.setBackgroundTint(colorRes: Int) {
+fun FloatingActionButton.setBackgroundTint(@ColorRes colorRes: Int) {
     this.setBackgroundTintList(ContextCompat.getColorStateList(this.context, colorRes))
 }
 
 /**
  * Set floating action button icon tint
  */
-fun FloatingActionButton.setIconTint(colorRes: Int) {
+fun FloatingActionButton.setIconTint(@ColorRes colorRes: Int) {
     DrawableCompat.setTint(
             DrawableCompat.wrap(this.drawable),
             ContextCompat.getColor(this.context, colorRes))
@@ -25,7 +26,7 @@ fun FloatingActionButton.setIconTint(colorRes: Int) {
  * @param backgroundTint Color resource for background tint
  * @param iconTint Color resource for icon tint
  */
-fun FloatingActionButton.setColors(backgroundTint: Int, iconTint: Int) {
+fun FloatingActionButton.setColors(@ColorRes backgroundTint: Int, @ColorRes iconTint: Int) {
     this.setBackgroundTint(backgroundTint)
     this.setIconTint(iconTint)
 }
