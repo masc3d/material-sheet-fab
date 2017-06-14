8a898334dd7c08d6bc50a3dcb2d8056b716d4036
Jürgen Töpper <juergen.toepper@dr-t.de>
2017-06-14 16:22:58 +0200

    Status auch für trx

--- leoz-central-schema/db/migration/V16__tblstatus_erzeuger_line_4_stellen.sql
+++
@@ -1,5 +0,0 @@
-use dekuclient;
-
-ALTER TABLE `dekuclient`.`tblstatus`
-  CHANGE COLUMN `Erzeugerstation` `Erzeugerstation` CHAR(4) NOT NULL DEFAULT '' ,
-  CHANGE COLUMN `Exportstation` `Exportstation` CHAR(4) NOT NULL DEFAULT '' ;
