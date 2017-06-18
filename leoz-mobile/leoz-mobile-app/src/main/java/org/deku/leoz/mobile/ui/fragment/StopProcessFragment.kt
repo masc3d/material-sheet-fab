e84fe2a6e787bebc132481b395f491202ec52ea9
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-06-18 23:18:45 +0200

    VM: Updated Vagrantfile: Added possibility of using a local Vagrantfile leoz-mobile: Updated Delivery screen: Moved dynamic content to separate fragment

---
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/fragment/StopProcessFragment.kt
@@ -0,0 +1,130 @@
+package org.deku.leoz.mobile.ui.fragment
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.conf.global
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.lazy
+import com.jakewharton.rxbinding2.widget.RxTextView
+import com.trello.rxlifecycle2.android.FragmentEvent
+import com.trello.rxlifecycle2.kotlin.bindUntilEvent
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.fragment_stop_process.*
+
+import org.deku.leoz.mobile.R
+import org.deku.leoz.mobile.model.Order
+import org.deku.leoz.mobile.model.Stop
+import org.deku.leoz.mobile.ui.Fragment
+import org.deku.leoz.mobile.ui.ParcelListAdapter
+import org.slf4j.LoggerFactory
+import sx.android.aidc.AidcReader
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StopProcessFragment : Fragment() {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+    private val aidcReader: AidcReader by Kodein.global.lazy.instance()
+
+    private lateinit var stop: Stop
+    private var lastRef: String? = null
+    private var resultCount: Int = 0
+
+    companion object {
+        fun create(stop: Stop): StopProcessFragment {
+            val f = StopProcessFragment()
+            f.stop = stop
+            return f
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_stop_process, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        RxTextView.textChanges(this.uxLabelNo).subscribe {
+            this.uxLabelNo.error = null
+        }
+
+        val parcelList = mutableListOf<Order.Parcel>()
+
+        stop.orders.forEach {
+            parcelList.addAll(it.parcel)
+        }
+
+        val parcelRefList: List<String> = parcelList.map {
+            it.labelReference ?: ""
+        }
+
+        this.uxParcelList.adapter = ParcelListAdapter(context, parcelList)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        aidcReader.readEvent
+                .bindUntilEvent(this, FragmentEvent.PAUSE)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    log.info("Barcode scanned ${it.data}")
+                    processLabelRef(ref = it.data)
+                }
+    }
+
+    private fun processLabelRef(ref: String) {
+        val order: Order? = stop.orders.firstOrNull {
+            it.parcel.firstOrNull {
+                it.labelReference == ref
+            } != null
+        }
+
+        hideResultImages()
+
+        this.uxLabelNo.error = null
+
+        if (order != null) {
+            showResult(R.drawable.green)
+        } else {
+            //Parcel is not part of this stop
+            if (lastRef.isNullOrBlank() || lastRef != ref) {
+                //No (similar) reference scanned previously
+                this.uxLabelNo.error = "ID does not belong to active stop."
+                showResult(R.drawable.red)
+            } else {
+                this.uxLabelNo.error = "ID does not belong to active stop."
+                showResult(R.drawable.red)
+            }
+        }
+
+        lastRef = ref
+    }
+
+    private fun showResult(backgroundResource: Int) {
+        hideResultImages()
+
+        val view =  if (resultCount%2 == 0) this.uxResultLeft else this.uxResultRight
+
+        //view.setBackgroundResource(backgroundResource)
+        view.setImageDrawable(resources.getDrawable(backgroundResource))
+        view.visibility = View.VISIBLE
+
+        resultCount++
+    }
+
+    private fun hideResultImages() {
+        this.uxResultLeft.visibility = View.INVISIBLE
+        this.uxResultRight.visibility = View.INVISIBLE
+    }
+
+}
--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/screen/DeliveryProcessScreen.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/screen/DeliveryProcessScreen.kt
@@ -11,33 +11,28 @@ import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.conf.global
 import com.github.salomonbrys.kodein.instance
 import com.github.salomonbrys.kodein.lazy
-import com.jakewharton.rxbinding2.widget.RxTextView
 import com.trello.rxlifecycle2.android.FragmentEvent
 import com.trello.rxlifecycle2.kotlin.bindUntilEvent
-import io.reactivex.android.schedulers.AndroidSchedulers
-import kotlinx.android.synthetic.main.fragment_delivery_process.*
+import kotlinx.android.synthetic.main.screen_delivery_process.*
 import kotlinx.android.synthetic.main.item_stop_overview.*
 
 import org.deku.leoz.mobile.R
-import org.deku.leoz.mobile.model.Order
 import org.deku.leoz.mobile.model.Stop
-import org.deku.leoz.mobile.ui.ParcelListAdapter
 import org.deku.leoz.mobile.ui.ScreenFragment
 import org.deku.leoz.mobile.ui.activity.DeliveryActivity
+import org.deku.leoz.mobile.ui.fragment.StopProcessFragment
 import org.deku.leoz.mobile.ui.inflateMenu
 import org.deku.leoz.mobile.ui.view.ActionItem
 import org.slf4j.LoggerFactory
 import sx.android.aidc.AidcReader
+import sx.android.fragment.util.withTransaction
 import java.text.SimpleDateFormat
 
 
-class DeliveryProcessScreen() : ScreenFragment() {
+class DeliveryProcessScreen : ScreenFragment() {
 
     private val log = LoggerFactory.getLogger(this.javaClass)
     private val aidcReader: AidcReader by Kodein.global.lazy.instance()
-
-    private var lastRef: String? = null
-    private var resultCount: Int = 0
 
     private lateinit var stop: Stop
 
@@ -58,7 +53,7 @@ class DeliveryProcessScreen() : ScreenFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_delivery_process, container, false)
+        return inflater!!.inflate(R.layout.screen_delivery_process, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
@@ -73,24 +68,9 @@ class DeliveryProcessScreen() : ScreenFragment() {
         this.uxReceipient.text = stop.address.addressLine1
         this.uxAppointment.text = "${simpleDateFormat.format(stop.appointment.dateFrom)} - ${simpleDateFormat.format(stop.appointment.dateTo)}"
 
-        RxTextView.textChanges(this.uxLabelNo).subscribe {
-            this.uxLabelNo.error = null
-        }
-
-        //region TODO To be replaced by a custom adapter
-        val parcelList = mutableListOf<Order.Parcel>()
-
-        stop.orders.forEach {
-            parcelList.addAll(it.parcel)
-        }
-
-        val parcelRefList: List<String> = parcelList.map {
-            it.labelReference ?: ""
-        }
-        
         //val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(context, android.R.layout.simple_list_item_1, parcelRefList)
 
-        this.uxParcelList.adapter = ParcelListAdapter(context, parcelList)
+
         //endregion
 
         this.actionItems = listOf(
@@ -113,6 +93,13 @@ class DeliveryProcessScreen() : ScreenFragment() {
                         menu = this.activity.inflateMenu(R.menu.menu_deliver_fail)
                 )
         )
+        (this.activity as DeliveryActivity).showDeliverFabButtons()
+
+
+        childFragmentManager.withTransaction {
+            it.replace(this.uxContainer.id, StopProcessFragment())
+        }
+        //this.uxServiceList.adapter = ArrayAdapter(context, android.R.layout.simple_list_item_1, arrayOf("Nachnahme", "Tel. Empfangsbestätigung", "Security Return", "X-Change"))
     }
 
     override fun onResume() {
@@ -150,61 +137,10 @@ class DeliveryProcessScreen() : ScreenFragment() {
                     }
                 }
 
-        aidcReader.readEvent
-                .bindUntilEvent(this, FragmentEvent.PAUSE)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    log.info("Barcode scanned ${it.data}")
-                    processLabelRef(ref = it.data)
-                }
-
         /**
          * TODO: Action trigger on uxLabelNo TextView
          */
     }
 
-    private fun processLabelRef(ref: String) {
-        val order: Order? = stop.orders.firstOrNull {
-            it.parcel.firstOrNull {
-                it.labelReference == ref
-            } != null
-        }
 
-        hideResultImages()
-
-        this.uxLabelNo.error = null
-
-        if (order != null) {
-            showResult(R.drawable.green)
-        } else {
-            //Parcel is not part of this stop
-            if (lastRef.isNullOrBlank() || lastRef != ref) {
-                //No (similar) reference scanned previously
-                this.uxLabelNo.error = "ID does not belong to active stop."
-                showResult(R.drawable.red)
-            } else {
-                this.uxLabelNo.error = "ID does not belong to active stop."
-                showResult(R.drawable.red)
-            }
-        }
-
-        lastRef = ref
-    }
-
-    private fun showResult(backgroundResource: Int) {
-        hideResultImages()
-
-        val view =  if (resultCount%2 == 0) this.uxResultLeft else this.uxResultRight
-
-        //view.setBackgroundResource(backgroundResource)
-        view.setImageDrawable(resources.getDrawable(backgroundResource))
-        view.visibility = View.VISIBLE
-
-        resultCount++
-    }
-
-    private fun hideResultImages() {
-        this.uxResultLeft.visibility = View.INVISIBLE
-        this.uxResultRight.visibility = View.INVISIBLE
-    }
 }
