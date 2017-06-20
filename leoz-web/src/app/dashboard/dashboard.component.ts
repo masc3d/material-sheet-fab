fd1d739f2ccb66e687f4bbb9e23da19341716689
Janussek <info@it-cobra.es>
2017-06-20 11:59:46 +0200

    dashboard template inline

--- leoz-web/src/app/dashboard/dashboard.component.ts
+++ leoz-web/src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  template: `
+    <app-left-menu></app-left-menu>
+    <div id="main">
+      <router-outlet></router-outlet>
+    </div>`
 })
 export class DashboardComponent {
 }
