cd94ffd42ca3da8f0d069fa5b85f083a9de5b025
masc <masc@disappear.de>
2017-07-04 11:42:48 +0200

    UPDATED OrderJooqRepository: minor cleanup, added join example

--- leoz-central/src/main/java/org/deku/leoz/central/data/repository/OrderJooqRepository.kt
+++ leoz-central/src/main/java/org/deku/leoz/central/data/repository/OrderJooqRepository.kt
@@ -2,9 +2,11 @@ package org.deku.leoz.central.data.repository
 
 import org.deku.leoz.central.config.PersistenceConfiguration
 import org.deku.leoz.central.data.jooq.Tables
+import org.deku.leoz.central.data.jooq.tables.TrnVOrder
 import org.deku.leoz.central.data.jooq.tables.records.TrnVOrderParcelRecord
 import org.deku.leoz.central.data.jooq.tables.records.TrnVOrderRecord
 import org.jooq.DSLContext
+import org.jooq.RecordMapper
 import org.springframework.beans.factory.annotation.Qualifier
 import javax.inject.Inject
 import javax.inject.Named
@@ -20,10 +22,19 @@ open class OrderJooqRepository {
     @Qualifier(PersistenceConfiguration.QUALIFIER)
     private lateinit var dslContext: DSLContext
 
-    fun findByID(id: Long): TrnVOrderRecord? {
+    fun findById(id: Long): TrnVOrderRecord? {
         return dslContext.fetchOne(
                 Tables.TRN_V_ORDER,
                 Tables.TRN_V_ORDER.ID.eq(id.toDouble()))
+    }
+
+    fun findByIdJoined(id: Long): Map<TrnVOrderRecord, List<TrnVOrderParcelRecord>> {
+        return dslContext.select()
+                .from(Tables.TRN_V_ORDER)
+                .join(Tables.TRN_V_ORDER_PARCEL)
+                .on(Tables.TRN_V_ORDER_PARCEL.ORDER_ID.eq(Tables.TRN_V_ORDER.ID))
+                .where(Tables.TRN_V_ORDER.ID.eq(id.toDouble()))
+                .fetchGroups(Tables.TRN_V_ORDER, TrnVOrderParcelRecord::class.java)
     }
 
     fun findByScan(scanId: String): TrnVOrderRecord? {
@@ -31,7 +42,7 @@ open class OrderJooqRepository {
                 Tables.TRN_V_ORDER_PARCEL,
                 Tables.TRN_V_ORDER_PARCEL.SCAN_ID.eq(scanId.toDouble()))
 
-        return findByID(rParcel.orderId.toLong())
+        return findById(rParcel.orderId.toLong())
     }
 
     fun findParcelsByOrderId(id: Long): List<TrnVOrderParcelRecord> {
