341cfc827d9cfbb0d434df7291d99acc06c417e3
masc <masc@disappear.de>
2017-07-04 21:43:21 +0200

    UPDATED events: cleanup

--- leoz-common/src/main/java/org/deku/leoz/model/events.kt
+++ leoz-common/src/main/java/org/deku/leoz/model/events.kt
@@ -1,33 +1,41 @@
 package org.deku.leoz.model
 
-enum class EventType(val id: Int, val eventType: Class<*>) {
-    Delivered(1, EventDelivered::class.java),
-    NotDelivered(2, EventNotDelivered::class.java)
+/**
+ * Event types
+ */
+enum class EventType(val id: Int, reasonType: Class<*>) {
+    Delivered(1, reasonType = EventDeliveredReason::class.java),
+    NotDelivered(2, reasonType = EventNotDeliveredReason::class.java)
 }
 
+/**
+ * Event value, consists of an event type and referring reason enum value
+ */
 abstract class EventValue<R : Enum<R>>(
-        val type: EventType,
+        val event: EventType,
         val reason: R
 )
 
+// Event reasons
+
 enum class EventDeliveredReason(val id: Int) {
     Normal(1),
-    Neighbor(2),                // nur wenn SKZ 536870912 (Keine Alternativzustellung) nicht gesetzt
+    Neighbor(2), // nur wenn SKZ 536870912 (Keine Alternativzustellung) nicht gesetzt
     Postbox(3)                  // nur wenn SKZ 268435456 (Briefkastenzustellung möglich) gesetzt
 }
 
 enum class EventNotDeliveredReason(val id: Int) {
     Absend(500),
-    Refuse(503),                // Text : Wer? / Warum?
-    Vacation(504),              // Text : Wie lange?
-    AdressWrong(507),           // Text : Richtige Adresse?
-    Moved(509),                 // Text : Neue Adresse?
+    Refuse(503), // Text : Wer? / Warum?
+    Vacation(504), // Text : Wie lange?
+    AdressWrong(507), // Text : Richtige Adresse?
+    Moved(509), // Text : Neue Adresse?
     Unknown(510),
-    Damaged(513),               // Packstück bleibt in der Auswahl
+    Damaged(513), // Packstück bleibt in der Auswahl
     XC_CodeWrong(516),
     XC_ObjectDamaged(517),
     XC_ObjectWrong(518),
-    SignatureRefused(519),      // Text : Warum?
+    SignatureRefused(519), // Text : Warum?
     CouldWantNotPay(521),
     IdentDocNotPresent(524),
     XC_ObjectNotReady(529),
@@ -35,14 +43,22 @@ enum class EventNotDeliveredReason(val id: Int) {
     WaitTime(555)               // Text : Wartezeit (min)?   // Packstück bleibt in der Auswahl   // Unterschrift nach eingabe anfordern  // Kyboard auf Num stellen
 }
 
+// Events
+
+/**
+ * Delivered event
+ */
 class EventDelivered(reason: EventDeliveredReason)
     : EventValue<EventDeliveredReason>(
-        type = EventType.Delivered,
+        event = EventType.Delivered,
         reason = reason
 )
 
+/**
+ * Not delivered event
+ */
 class EventNotDelivered(reason: EventNotDeliveredReason
 ) : EventValue<EventNotDeliveredReason>(
-        type = EventType.NotDelivered,
+        event = EventType.NotDelivered,
         reason = reason
 )
