8beef84ed799d99e6fff35f04c6d25f022304c66
masc <masc@disappear.de>
2017-07-11 23:53:33 +0200

    [libs-sx-android] MOVED generic flexible adapter components to sx-android

---
+++ src/main/java/sx/android/ui/flexibleadapter/FlexibleVmItem.kt
@@ -0,0 +1,43 @@
+package sx.android.ui.flexibleadapter
+
+import android.view.View
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.items.AbstractFlexibleItem
+import eu.davidea.flexibleadapter.items.IFlexible
+
+/**
+ * Created by masc on 26.06.17.
+ */
+class FlexibleVmItem<VM>(
+        val viewRes: Int,
+        val variableId: Int,
+        val viewModel: VM
+)
+    : AbstractFlexibleItem<FlexibleVmHolder>() {
+
+    override fun equals(other: Any?): Boolean {
+        return this === other
+    }
+
+    override fun hashCode(): Int{
+        return super.hashCode()
+    }
+
+    override fun bindViewHolder(
+            adapter: FlexibleAdapter<out IFlexible<*>>,
+            viewHolder: FlexibleVmHolder,
+            position: Int,
+            payloads: MutableList<Any?>) {
+
+        viewHolder.binding.setVariable(this.variableId, this.viewModel)
+        viewHolder.binding.executePendingBindings()
+    }
+
+    override fun createViewHolder(view: View, adapter: FlexibleAdapter<out IFlexible<*>>): FlexibleVmHolder {
+        return FlexibleVmHolder(view, adapter)
+    }
+
+    override fun getLayoutRes(): Int {
+        return this.viewRes
+    }
+}
