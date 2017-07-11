76053d71bbc9d19561e7646eae0b618f6b4a6f51
masc <masc@disappear.de>
2017-07-11 18:04:31 +0200

    ADDED mobile databinding adapters

---
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/DatabindingAdapters.kt
@@ -0,0 +1,42 @@
+package org.deku.leoz.mobile.ui
+
+import android.databinding.BindingAdapter
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
+
+/**
+ * Created by masc on 11.07.17.
+ */
+
+/**
+ * Data binding adatpers
+ * Created by masc on 10.07.17.
+ */
+class DataBindingAdatpers {
+    companion object {
+        @BindingAdapter("android:src")
+        @JvmStatic fun setImageUri(view: ImageView, imageUri: String?) {
+            if (imageUri == null) {
+                view.setImageURI(null);
+            } else {
+                view.setImageURI(Uri.parse(imageUri));
+            }
+        }
+
+        @BindingAdapter("android:src")
+        @JvmStatic fun setImageUri(view: ImageView, imageUri: Uri) {
+            view.setImageURI(imageUri);
+        }
+
+        @BindingAdapter("android:src")
+        @JvmStatic fun setImageDrawable(view: ImageView, drawable: Drawable) {
+            view.setImageDrawable(drawable);
+        }
+
+        @BindingAdapter("android:src")
+        @JvmStatic fun setImageResource(imageView: ImageView, resource: Int){
+            imageView.setImageResource(resource);
+        }
+    }
+}
