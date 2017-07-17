95a2dd7f9333f1cd01fac8ef4dd1001a3458d99b
masc <masc@disappear.de>
2017-07-17 17:49:16 +0200

    ADDED DekuDeliveryListNumber

---
+++ leoz-common/src/main/java/org/deku/leoz/model/DeliveryListNumbers.kt
@@ -0,0 +1,13 @@
+package org.deku.leoz.model
+
+/**
+ * DEKU delivery list number
+ * Created by masc on 17.07.17.
+ */
+class DeliveryListNumber {
+    companion object {
+        fun parse(value: String) {
+
+        }
+    }
+}
