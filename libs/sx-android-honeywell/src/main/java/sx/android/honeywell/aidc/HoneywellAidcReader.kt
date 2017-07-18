ba44418ea6c8d7f1af6d00d989c6dd3d6f1c9f5c
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-07-18 15:35:56 +0200

    leoz-mobile: Updated/Extracted string resources sx-android-honeywell: Updated trigger control mode: Changed to Auto-Control.

--- libs/sx-android-honeywell/src/main/java/sx/android/honeywell/aidc/HoneywellAidcReader.kt
+++ libs/sx-android-honeywell/src/main/java/sx/android/honeywell/aidc/HoneywellAidcReader.kt
@@ -84,7 +84,7 @@ class HoneywellAidcReader private constructor(
         // Default settings
         bc.setProperties(mapOf<String, Any>(
                 Pair(BarcodeReader.PROPERTY_NOTIFICATION_BAD_READ_ENABLED, false),
-                Pair(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE, BarcodeReader.TRIGGER_CONTROL_MODE_CLIENT_CONTROL),
+                Pair(BarcodeReader.PROPERTY_TRIGGER_CONTROL_MODE, BarcodeReader.TRIGGER_CONTROL_MODE_AUTO_CONTROL),
                 Pair(BarcodeReader.PROPERTY_CENTER_DECODE, true),
                 // Disable all symbologies
                 Pair(BarcodeReader.PROPERTY_AZTEC_ENABLED, false),
