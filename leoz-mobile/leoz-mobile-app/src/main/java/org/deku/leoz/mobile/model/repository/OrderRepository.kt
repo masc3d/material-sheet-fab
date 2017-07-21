706b810c562817a585a99e7eb61033e7e1acc2a1
masc <masc@disappear.de>
2017-07-21 13:13:51 +0200

    UPDATED mobile OrderRepository: added self observable orders collection, improved .save implementation

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/model/repository/OrderRepository.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/model/repository/OrderRepository.kt
@@ -1,17 +1,14 @@
 package org.deku.leoz.mobile.model.repository
 
-import com.github.salomonbrys.kodein.Kodein
-import com.github.salomonbrys.kodein.conf.global
-import com.github.salomonbrys.kodein.erased.instance
-import com.github.salomonbrys.kodein.lazy
+import android.databinding.Observable
 import io.reactivex.Completable
-import io.reactivex.Single
+import io.reactivex.rxkotlin.subscribeBy
 import io.requery.Persistable
 import io.requery.reactivex.KotlinReactiveEntityStore
-import org.deku.leoz.mobile.Database
-import org.deku.leoz.mobile.model.entity.Order
-import org.deku.leoz.mobile.model.entity.OrderEntity
+import org.deku.leoz.mobile.model.entity.*
 import org.slf4j.LoggerFactory
+import sx.Stopwatch
+import sx.rx.ObservableRxProperty
 
 /**
  * Order repository
@@ -22,24 +19,73 @@ class OrderRepository(
 ) {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    val ordersProperty = ObservableRxProperty(listOf<OrderEntity>())
+    val orders by ordersProperty
+
+    /**
+     * Self observable order query
+     */
+    private val _orders = store.select(OrderEntity::class)
+            .get()
+            .observableResult()
+            .subscribeBy(
+                    onNext = {
+                        val orders = it.toList()
+                        log.trace("ORDERS CHANGED [${orders.count()}]")
+                        this.ordersProperty.set(orders)
+
+                        orders.forEach {
+                            it.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
+                                override fun onPropertyChanged(sender: Observable, propertyId: Int) {
+                                    log.trace("ORDER FIELD CHANGED [${propertyId}]")
+                                }
+                            })
+                        }
+                    },
+                    onError = {
+                        log.error(it.message, it)
+                    }
+            )
+
     /**
      * Save a batch of orders, replacing existing orders if applicable
      */
     fun save(orders: List<Order>): Completable {
+        val sw = Stopwatch.createStarted()
         return store.withTransaction {
-            orders.forEach { order ->
+            // Filter orders, remove duplicates eg.
+            val filteredOrders = orders.groupBy {
+                it.id
+            }.map {
+                if (it.value.count() > 1) {
+                    log.warn("Duplicate order id [${it.key}] parcel counts [${it.value.map { it.parcels.count() }.joinToString(", ")}]")
+                }
+                it.value.maxBy {
+                    it.parcels.count()
+                }
+            }.filterNotNull()
+
+            // Store orders
+            filteredOrders.forEach { order ->
                 val existingOrder = this.select(OrderEntity::class)
                         .where(OrderEntity.ID.eq(order.id))
                         .get().firstOrNull()
 
                 if (existingOrder != null) {
-                    log.warn("Duplicate order [${order.id}]")
                     delete(existingOrder)
                 }
 
                 insert(order)
             }
-        }.toCompletable()
+        }
+                .toCompletable()
+                .doOnComplete {
+                    val orderCount = store.count(OrderEntity::class).get().call()
+                    val taskCount = store.count(OrderTaskEntity::class).get().call()
+                    val addressCount = store.count(AddressEntity::class).get().call()
+                    val parcelCount = store.count(ParcelEntity::class).get().call()
+                    log.trace("Saved orders in $sw :: orders [${orderCount}] tasks [${taskCount}] addresses [${addressCount}] parcels [${parcelCount}]")
+                }
     }
 
     /**
