4028ea4488f94c984369c1527163c916912146e8
masc <masc@disappear.de>
2017-07-25 11:03:15 +0200

    Squashed 'leoz-central-schema/' changes from 45469643..47283725
    47283725 leoz -> 103 schnapp
    64edc43a ParcelService Event handling
    5dd6fa00 UPDATED to gradle-4.0.1
    92181a92 Merge commit '2733f097d925fa00c881a7ffa3b048ba6dc67871'
    fe6fe9af leo export improvements
    
    git-subtree-dir: leoz-central-schema
    git-subtree-split: 472837255a7b9c4142e194afefd214df619f6855

---
+++ db/migration/V24__parcel_signature_path.sql
@@ -0,0 +1,22 @@
+USE `dekuclient`;
+CREATE
+OR REPLACE ALGORITHM = UNDEFINED
+  DEFINER = `root`@`%`
+  SQL SECURITY DEFINER
+VIEW `tad_v_order_parcel` AS
+  SELECT
+    ((`tblauftragcollies`.`OrderID` * 100) + `tblauftragcollies`.`OrderPos`) AS `id`,
+    `tblauftragcollies`.`OrderID` AS `order_id`,
+    `tblauftragcollies`.`CollieBelegNr` AS `scan_id`,
+    `tblauftragcollies`.`RollkartennummerD` AS `last_delivery_list_id`,
+    `tblauftragcollies`.`VerpackungsArt` AS `parcel_type`,
+    `tblauftragcollies`.`Laenge` AS `dimension_length`,
+    `tblauftragcollies`.`Hoehe` AS `dimension_height`,
+    `tblauftragcollies`.`Breite` AS `dimension_width`,
+    `tblauftragcollies`.`GewichtReal` AS `dimention_weight`,
+    `tblauftragcollies`.`bmpFileName` AS `signature_path`
+  FROM
+    `tblauftragcollies`
+  WHERE
+    ((`tblauftragcollies`.`CollieBelegNr` > 0)
+     AND (`tblauftragcollies`.`OrderPos` > 0));
