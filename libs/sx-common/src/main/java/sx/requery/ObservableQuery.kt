288727d30eebdcd449622d039841c3bf19b16c24
masc <masc@disappear.de>
2017-07-26 19:14:20 +0200

    UPDATED sx-common: removed thread-safety from observable properties (consumer's responsibility), ObservableQuery should implement Disposable

--- libs/sx-common/src/main/java/sx/requery/ObservableQuery.kt
+++ libs/sx-common/src/main/java/sx/requery/ObservableQuery.kt
@@ -1,6 +1,6 @@
 package sx.requery
 
-import io.reactivex.Observable
+import io.reactivex.disposables.Disposable
 import io.requery.reactivex.ReactiveResult
 import org.slf4j.LoggerFactory
 import sx.rx.ObservableLazyRxProperty
@@ -10,19 +10,28 @@ import sx.rx.ObservableLazyRxProperty
  * Created by masc on 26.07.17.
  */
 class ObservableQuery<E>(
-        private val result: ReactiveResult<E>
-) {
+        private val reactiveResult: ReactiveResult<E>
+): Disposable {
     private val log = LoggerFactory.getLogger(this.javaClass)
+    private val subscription: Disposable
 
-    val records = ObservableLazyRxProperty({ listOf<E>() })
+    val result = ObservableLazyRxProperty({ listOf<E>() })
 
     init {
-        this.result
+        this.subscription = this.reactiveResult
                 .observableResult()
                 .subscribe {
                     val records = it.toList()
-                    log.trace("RECORDS CHANGED ${this.records.count()}")
-                    this.records.reset { records }
+                    log.trace("RECORDS CHANGED ${this.result.count()}")
+                    this.result.reset { records }
                 }
+    }
+
+    override fun isDisposed(): Boolean {
+        return this.subscription.isDisposed
+    }
+
+    override fun dispose() {
+        this.subscription.dispose()
     }
 }
