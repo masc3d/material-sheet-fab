4f5c75b0ae588ce4313c5172d1a39f01274c3bf5
masc <masc@disappear.de>
2017-07-28 14:53:43 +0200

    ADDED ParcelSectionsAdapter UPDATED VehicleLoadingScreen to use ParcelSectionsAdapter

---
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/vm/ParcelSectionsAdapter.kt
@@ -0,0 +1,126 @@
+package org.deku.leoz.mobile.ui.vm
+
+import android.support.v7.widget.RecyclerView
+import com.trello.rxlifecycle2.kotlin.bindToLifecycle
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.utils.Log
+import io.reactivex.Observable
+import org.deku.leoz.mobile.BR
+import org.deku.leoz.mobile.R
+import org.deku.leoz.mobile.model.entity.Parcel
+import org.deku.leoz.mobile.model.entity.ParcelEntity
+import org.slf4j.LoggerFactory
+import sx.android.rx.observeOnMainThread
+import sx.android.ui.flexibleadapter.FlexibleExpandableVmItem
+import sx.android.ui.flexibleadapter.FlexibleVmSectionableItem
+
+/**
+ * Created by masc on 28.07.17.
+ */
+class ParcelSectionsAdapter
+    :
+        FlexibleAdapter<
+                FlexibleExpandableVmItem<ParcelListHeaderViewModel, ParcelViewModel>
+                >(listOf(), null, true) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    private val _headerItems = mutableListOf<FlexibleExpandableVmItem<
+            ParcelListHeaderViewModel, ParcelViewModel>>()
+
+    init {
+        FlexibleAdapter.enableLogs(Log.Level.VERBOSE)
+
+        // TODO: unreliable. need to override flexibleadapter for proper reactive event
+        this.addListener(object : FlexibleAdapter.OnItemClickListener {
+            private var previousHeaderItem: Any? = null
+
+            override fun onItemClick(position: Int): Boolean {
+                val adapter = this@ParcelSectionsAdapter
+                val item: Any? = adapter.getItem(position)
+
+                if (item != null && _headerItems.contains(item)) {
+                    val changed = (item != this.previousHeaderItem)
+
+                    // Select & collapse
+                    adapter.toggleSelection(position)
+
+                    if (changed) {
+                        adapter.collapseAll()
+                    } else {
+                        if (adapter.isExpanded(position)) {
+                            adapter.collapse(position)
+                        } else {
+                            adapter.expand(position)
+                        }
+                    }
+
+                    this.previousHeaderItem = item
+                }
+
+                return true
+            }
+        })
+
+        this.mode = FlexibleAdapter.MODE_SINGLE
+
+        this.setStickyHeaders(true)
+        this.showAllHeaders()
+        this.setAutoCollapseOnExpand(true)
+        this.collapseAll()
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView?) {
+        super.onAttachedToRecyclerView(recyclerView)
+        log.trace("ADAPTER ATTACHED")
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView?) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        log.trace("ADAPTER DETACHED")
+    }
+
+    fun addParcelSection(
+            header: ParcelListHeaderViewModel,
+            items: Observable<List<ParcelEntity>>) {
+
+        val headerItem = FlexibleExpandableVmItem<ParcelListHeaderViewModel, ParcelViewModel>(
+                viewRes = R.layout.item_parcel_header,
+                variableId = BR.header,
+                viewModel = header,
+                isExpandableOnClick = false
+        )
+
+        items
+                .observeOnMainThread()
+                .subscribe {
+                    // Need to collapse on complete sublist update to prevent weird glitches
+                    this.collapseAll()
+
+                    headerItem.subItems = it.map {
+                        val item = FlexibleVmSectionableItem(
+                                viewRes = R.layout.item_parcel,
+                                variableId = BR.parcel,
+                                viewModel = ParcelViewModel(it)
+                        )
+
+                        item.isEnabled = true
+                        item.isDraggable = false
+                        item.isSwipeable = false
+                        item.isSelectable = false
+                        item.header = headerItem
+
+                        item
+                    }
+
+                    this.updateItem(headerItem)
+                }
+
+        headerItem.isSelectable = true
+
+        // Store expandable/header item in our own list, as expanding will
+        _headerItems.add(headerItem)
+
+        this.addItem(headerItem)
+    }
+}
