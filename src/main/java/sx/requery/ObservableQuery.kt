35d704c86853d5f783bf647f42c83a832197433d
masc <masc@disappear.de>
2017-07-31 22:07:42 +0200

    ADDED sx.requery: support for transforming query results

--- src/main/java/sx/requery/ObservableQuery.kt
+++ src/main/java/sx/requery/ObservableQuery.kt
@@ -1,29 +1,35 @@
 package sx.requery
 
 import io.reactivex.disposables.Disposable
+import io.requery.query.Tuple
 import io.requery.reactivex.ReactiveResult
 import org.slf4j.LoggerFactory
 import sx.rx.ObservableLazyRxProperty
 
 /**
- * Observable query wrapper
+ * Observable query
  * Created by masc on 26.07.17.
+ * @param query Reactive query
  */
-class ObservableQuery<E>(
-        private val reactiveResult: ReactiveResult<E>
+abstract class BaseObservableQuery<Q, T>(
+        private val query: ReactiveResult<Q>,
+        private val transform: (queryType: Q) -> T
 ): Disposable {
     private val log = LoggerFactory.getLogger(this.javaClass)
     private val subscription: Disposable
 
-    val result = ObservableLazyRxProperty({ listOf<E>() })
+    /**
+     * Observable lazy query result property
+     */
+    val result = ObservableLazyRxProperty({ listOf<T>() })
 
     init {
-        this.subscription = this.reactiveResult
+        this.subscription = this.query
                 .observableResult()
                 .subscribe {
                     val records = it.toList()
                     log.trace("RECORDS CHANGED ${this.result.count()}")
-                    this.result.reset { records }
+                    this.result.reset { records.map { transform(it) } }
                 }
     }
 
@@ -35,3 +41,28 @@ class ObservableQuery<E>(
         this.subscription.dispose()
     }
 }
+
+/**
+ * Observable query
+ * Created by masc on 26.07.17.
+ * @param query Reactive query
+ */
+class ObservableQuery<E>(
+        query: ReactiveResult<E>
+): BaseObservableQuery<E, E>(
+        query = query,
+        transform = { it }
+)
+
+/**
+ * Observable tuple query
+ * @param query Reactive query
+ * @param transform Lambda transforming the tuple into a specific type
+ */
+class ObservableTupleQuery<T>(
+        private val query: ReactiveResult<Tuple>,
+        private val transform: (tuple: Tuple) -> T
+): BaseObservableQuery<Tuple, T>(
+        query = query,
+        transform = transform
+)
