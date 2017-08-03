8f7464e3a8f89bfb3e0a36e3dd55953fbe66cd28
masc <masc@disappear.de>
2017-08-03 09:33:55 +0200

    UPDATED DekuDeliveryList Number: fixed .parse verification, renamed .location to .station, comments

--- leoz-common/src/main/java/org/deku/leoz/model/DeliveryListNumbers.kt
+++ leoz-common/src/main/java/org/deku/leoz/model/DeliveryListNumbers.kt
@@ -11,6 +11,10 @@ class DekuDeliveryListNumber private constructor(
         val value: String) {
 
     companion object {
+        /**
+         * Parse delivery list barcode/label content
+         * @param value Barcode/label content including check-digit
+         */
         fun parseLabel(value: String): Result<DekuDeliveryListNumber> {
             if (value.length != 10)
                 return Result(error = IllegalArgumentException("DEKU delivery list label [${value}] has invalid length"))
@@ -25,16 +29,25 @@ class DekuDeliveryListNumber private constructor(
             }
         }
 
+        /**
+         * Parse delivery list number
+         * @param value Delivery list number without check-digit
+         */
         fun parse(value: String): Result<DekuDeliveryListNumber> {
-            if (value.length < 7 || value.length > 9) {
+            if (value.length < 7 || value.length > 9)
                 return Result(error = IllegalArgumentException("DEKU delivery list number [${value}] has invalid length"))
-            }
+
+            if (!value.all { it.isDigit() })
+                return Result(error = IllegalArgumentException("DEKU delivery list number [${value}] must be numeric"))
 
             return Result(DekuDeliveryListNumber(value.padStart(9, '0')))
         }
     }
 
-    val location: Int by lazy {
+    /**
+     * The location/station number
+     */
+    val station: Int by lazy {
         this.value.substring(0, 3).toInt()
     }
 
