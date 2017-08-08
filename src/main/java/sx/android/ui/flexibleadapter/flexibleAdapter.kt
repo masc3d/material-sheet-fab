213bf196d0c7d3c4d4200ebe13c89ef3c8739fa1
masc <masc@disappear.de>
2017-08-08 12:29:44 +0200

    ADDED sx-android: FlexibleAdpater.customizeScrollBehavior

---
+++ src/main/java/sx/android/ui/flexibleadapter/flexibleAdapter.kt
@@ -0,0 +1,23 @@
+package sx.android.ui.flexibleadapter
+
+import android.support.v7.widget.RecyclerView
+import eu.davidea.flexibleadapter.FlexibleAdapter
+import eu.davidea.flexibleadapter.helpers.ItemTouchHelperCallback
+import eu.davidea.flexibleadapter.items.IFlexible
+
+/**
+ * Customize scroll behavior specifics of FlexibleAdapter
+ */
+fun <T : IFlexible<*>> FlexibleAdapter<T>.customizeScrollBehavior(
+        scrollSpeed: Float
+) {
+    // There's currently a bug in flexibleadapter-5.0rc2 which doesn't detach existing/default
+    // ItemTouchHelper, so customizing the callback (which internally attaches another touch helper)
+    // must be done early.
+    this.itemTouchHelperCallback = object : ItemTouchHelperCallback(this) {
+        override fun interpolateOutOfBoundsScroll(recyclerView: RecyclerView?, viewSize: Int, viewSizeOutOfBounds: Int, totalSize: Int, msSinceStartScroll: Long): Int {
+            val direction = java.lang.Math.signum(viewSizeOutOfBounds.toDouble()).toInt()
+            return (scrollSpeed * direction).toInt()
+        }
+    }
+}
