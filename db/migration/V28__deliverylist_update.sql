b9d195cace82d3f4264cb5f6f85da69ee4abdf1c
Jürgen Töpper <juergen.toepper@dr-t.de>
2017-08-10 01:18:39 +0200

    fix deliverylist sort, multiparcel in orders, address nulls, stopservices=null

---
+++ db/migration/V28__deliverylist_update.sql
@@ -0,0 +1,14 @@
+USE `dekuclient`;
+CREATE
+OR REPLACE ALGORITHM = UNDEFINED
+  DEFINER = `root`@`%`
+  SQL SECURITY DEFINER
+VIEW `tad_v_deliverylist_details` AS
+  SELECT DISTINCT
+    `rkdetails`.`rollkartennummer` AS `id`,
+    `rkdetails`.`position`         AS `order_position`,
+    `rkdetails`.`OrderID`          AS `order_id`,
+    'DELIVERY'                     AS `stopType`
+  FROM
+    `rkdetails`
+  ORDER BY `rkdetails`.`position`;
