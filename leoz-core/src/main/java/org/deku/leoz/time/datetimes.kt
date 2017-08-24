bcdd0a8cd89bfb5da95edfa9477fc99088c69628
Helke <hw@dr-t.de>
2017-08-24 15:26:42 +0200

    UPDATE ParcelService ADD FileNames

--- leoz-core/src/main/java/org/deku/leoz/time/datetimes.kt
+++ leoz-core/src/main/java/org/deku/leoz/time/datetimes.kt
@@ -1,6 +1,8 @@
 package org.deku.leoz.time
 
 import org.deku.leoz.service.entity.ShortTime
+import sx.time.toLocalDate
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -14,4 +16,16 @@ fun java.sql.Time.toShortTime(): ShortTime {
 
 fun Date.toShortTime(): ShortTime {
     return ShortTime(this)
+}
+
+fun Date.toString_ddMMyyyy_PointSeparated(): String {
+    return SimpleDateFormat("dd.MM.yyyy").format(this)
+}
+
+fun Date.toDateWithoutTime(): Date {
+    return SimpleDateFormat("yyyy-MM-dd HH:mm").parse(this.toLocalDate().toString() + (" 00:00"))
+}
+
+fun Date.toDateOnlyTime(): Date {
+    return SimpleDateFormat("yyyy-MM-dd HH:mm").parse("1899-12-30 " + (this.toShortTime().toString()))
 }
