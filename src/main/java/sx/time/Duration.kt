b85ea4d30e58c5ebfd39924f172411846e2ab029
masc <masc@disappear.de>
2017-08-31 17:43:49 +0200

    UPDATED sx.Duration: added `ofHours`

--- src/main/java/sx/time/Duration.kt
+++ src/main/java/sx/time/Duration.kt
@@ -100,6 +100,7 @@ class Duration(val length: Long, val unit: TimeUnit) {
 
         fun ofSeconds(seconds: Long): Duration = Duration(seconds, TimeUnit.SECONDS)
         fun ofMinutes(minutes: Long): Duration = Duration(minutes, TimeUnit.MINUTES)
+        fun ofHours(hours: Long):Duration = Duration(hours, TimeUnit.HOURS)
         fun ofDays(days: Long): Duration = Duration(days, TimeUnit.DAYS)
     }
 }
