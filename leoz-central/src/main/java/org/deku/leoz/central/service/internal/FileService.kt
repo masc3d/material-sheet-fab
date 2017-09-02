dcebb832e4e9dd324163a9da531f590cb1440cf0
masc <masc@disappear.de>
2017-09-02 18:37:32 +0200

    Revert "revert FileService to be25ad899c9a71463b8ad4e2fa89d4798e56b98a"
    This reverts commit 812de37443b7cbb66fb6181aa3d643de8ec04c2a.

--- leoz-central/src/main/java/org/deku/leoz/central/service/internal/FileService.kt
+++ leoz-central/src/main/java/org/deku/leoz/central/service/internal/FileService.kt
@@ -1,17 +1,23 @@
 package org.deku.leoz.central.service.internal
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.net.MediaType
-import org.deku.leoz.central.config.PersistenceConfiguration
+import kotlinx.support.jdk7.use
 import org.deku.leoz.node.Storage
-import org.deku.leoz.service.internal.ParcelServiceV1
 import org.deku.leoz.service.internal.FileServiceV1
-import org.deku.leoz.service.internal.isLastFragment
 import org.slf4j.LoggerFactory
+import sx.io.serialization.JacksonSerializer
+import sx.io.serialization.Serializable
+import sx.io.serialization.Serializer
 import sx.mq.MqChannel
 import sx.mq.MqHandler
 import sx.rs.auth.ApiKey
 import java.io.File
-import java.io.FileOutputStream
+import java.io.RandomAccessFile
+import java.nio.ByteBuffer
+import java.nio.channels.Channels
+import java.nio.channels.FileChannel
+import java.nio.file.StandardOpenOption
 import javax.inject.Inject
 import javax.inject.Named
 import javax.ws.rs.Path
@@ -29,8 +35,20 @@ class FileServiceV1 :
 
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    @Serializable(0x5880838e3ce330)
+    private data class FileMeta(
+            var total: Int = 0,
+            var parts: Array<Int> = arrayOf()
+    )
+
+    init {
+        Serializer.types.register(FileMeta::class.java)
+    }
+
     @Inject
     private lateinit var storage: Storage
+
+    private val objectMapper by lazy { ObjectMapper() }
 
     override fun onMessage(message: FileServiceV1.FileFragmentMessage, replyChannel: MqChannel?) {
         log.trace("Received ${message}")
@@ -56,22 +74,80 @@ class FileServiceV1 :
         val total = message.total ?:
                 throw IllegalArgumentException("File UID must not be null")
 
-        // File related vars
+        val chunkSize = message.chunkSize ?:
+                throw IllegalArgumentException("Chunk size must not be null")
+
+        val totalSize = message.totalSize ?:
+                throw IllegalArgumentException("Total size must not be null")
+
+        // File names/extensions
 
         val extension = mimeType.subtype()
         val filename = "${fileUid}.${extension}"
+        val metaFilename = "${fileUid}.${extension}.meta"
         val tempFilename = "${fileUid}.${extension}.temp"
 
+        // Files
+
         val file = File(storage.workTmpDataDirectory, filename)
+        val metaFile = File(storage.workTmpDataDirectory, metaFilename)
         val tempFile = File(storage.workTmpDataDirectory, tempFilename)
 
-        FileOutputStream(tempFile, true).use {
-            it.write(payload)
+        var isFileComplete = false
+
+        FileChannel.open(
+                metaFile.toPath(),
+                StandardOpenOption.READ,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.CREATE).use { metaFileChannel ->
+
+            // Lock against meta file to synchronize access
+            metaFileChannel.lock().use {
+                val serializer = JacksonSerializer(this.objectMapper)
+
+                val meta = if (metaFileChannel.size() > 0) {
+                    // Deserialize meta data
+                    metaFile.inputStream().use { stream ->
+                        serializer.deserialize(stream) as FileMeta
+                    }
+                } else {
+                    // Create new meta structure
+                    FileMeta(total = total)
+                }
+
+                val isNewFile = tempFile.exists()
+
+                // Use random access file, allowing creation of fixed size file and writing to specific positions
+                RandomAccessFile(tempFile, "rw").use { tempRaFile ->
+                    if (isNewFile) {
+                        // Create file with fixed total size initially
+                        tempRaFile.setLength(totalSize.toLong())
+                    }
+
+                    // Write chunk to file
+                    tempRaFile.channel.use { tempFileChannel ->
+                        tempFileChannel.position(index.toLong() * chunkSize)
+                        tempFileChannel.write(ByteBuffer.wrap(payload))
+                    }
+
+                    // Update meta
+                    meta.parts = meta.parts.plus(index)
+
+                    isFileComplete = meta.total == meta.parts.size
+
+                    // Serialize meta back to file (if necessary)
+                    if (!isFileComplete) {
+                        metaFile.outputStream().use { stream ->
+                            serializer.serialize(stream, meta)
+                        }
+                    }
+                }
+            }
         }
 
-        if (message.isLastFragment) {
-            // Finalize file
+        if (isFileComplete) {
             tempFile.renameTo(file)
+            metaFile.delete()
         }
     }
 }
