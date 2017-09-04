3850b402fa323d90011f7449be79a05d1b47dfdc
masc <masc@disappear.de>
2017-09-04 22:54:14 +0200

    UPDATED sx-android: imageview extensions

--- src/main/java/sx/android/view/imageView.kt
+++ src/main/java/sx/android/view/imageView.kt
@@ -1,5 +1,6 @@
 package sx.android.view
 
+import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.content.ContextCompat
@@ -8,16 +9,27 @@ import android.widget.ImageView
 
 /**
  * Set background tint
+ * @param colorRes Color resource
  */
-fun ImageView.setBackgroundTint(@ColorRes colorRes: Int) {
+fun ImageView.setBackgroundTintRes(@ColorRes colorRes: Int) {
     this.setBackgroundTintList(ContextCompat.getColorStateList(this.context, colorRes))
 }
 
 /**
  * Set icon tint
+ * @param colorRes Color resource
  */
-fun ImageView.setIconTint(@ColorRes colorRes: Int) {
+fun ImageView.setIconTintRes(@ColorRes colorRes: Int) {
+    this.setIconTint(
+            ContextCompat.getColor(this.context, colorRes))
+}
+
+/**
+ * Set icon tint
+ * @param color Color
+ */
+fun ImageView.setIconTint(@ColorInt color: Int) {
     DrawableCompat.setTint(
             DrawableCompat.wrap(this.drawable.mutate()),
-            ContextCompat.getColor(this.context, colorRes))
+            color)
 }
