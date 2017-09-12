a1739a3a2b258dbd449de891a50738aedd488dce
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-09-12 17:00:20 +0200

    leoz-mobile: Updated structure and name of xml file for providing App-Data to the MDM

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/device/DeviceManagement.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/device/DeviceManagement.kt
@@ -18,9 +18,12 @@ class DeviceManagement(
 ) {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
-    private val identityFile = File(path, "identity.xml")
+    private val identityFile = File(path, "data.xml")
 
     fun saveIdentityFile() {
+        val TAG_ROOT = "mobilex"
+        val TAG_IDENTITY = "identity"
+        val TAG_DEVICE_ID_SHORT = "device-id-short"
         val TAG_DEVICE_ID = "device-id"
 
         log.trace("Writing identity file [${identityFile}]")
@@ -29,9 +32,16 @@ class DeviceManagement(
             Xml.newSerializer().also {
                 it.setOutput(writer)
                 it.startDocument(Charsets.UTF_8.name(), null)
-                it.startTag(null, TAG_DEVICE_ID)
+                it.startTag(null, TAG_ROOT)
+                it.startTag(null, TAG_IDENTITY)
+                it.startTag(null, TAG_DEVICE_ID_SHORT)
                 it.text(identity.shortUid)
+                it.endTag(null, TAG_DEVICE_ID_SHORT)
+                it.startTag(null, TAG_DEVICE_ID)
+                it.text(identity.uid.value)
                 it.endTag(null, TAG_DEVICE_ID)
+                it.endTag(null, TAG_IDENTITY)
+                it.endTag(null, TAG_ROOT)
                 it.endDocument()
             }
         }
