fd90962110ad5ff6a31a8455e3bbd4486ff6b865
masc <masc@disappear.de>
2017-09-13 13:58:44 +0200

    [libs-sx-android] UPDATED sx-android: added more rx scheduler extensions

--- src/main/java/sx/android/rx/Schedulers.kt
+++ src/main/java/sx/android/rx/Schedulers.kt
@@ -1,17 +1,22 @@
 package sx.android.rx
 
 import io.reactivex.Completable
+import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 
-/**
- * Observe on android main thread
- * Created by masc on 25.07.17.
- */
-fun <T> Observable<T>.observeOnMainThread(): Observable<T> {
-    return this.observeOn(AndroidSchedulers.mainThread())
-}
 
-fun Completable.observeOnMainThread(): Completable {
-    return this.observeOn(AndroidSchedulers.mainThread())
-}
+// Android scheduler helpers
+
+fun <T> Observable<T>.observeOnMainThread(): Observable<T> =
+        this.observeOn(AndroidSchedulers.mainThread())
+
+fun Completable.observeOnMainThread(): Completable
+        = this.observeOn(AndroidSchedulers.mainThread())
+
+fun <T> Maybe<T>.observeOnMainThread(): Maybe<T> =
+        this.observeOn(AndroidSchedulers.mainThread())
+
+fun <T> Single<T>.observeOnMainThread(): Single<T> =
+        this.observeOn(AndroidSchedulers.mainThread())
