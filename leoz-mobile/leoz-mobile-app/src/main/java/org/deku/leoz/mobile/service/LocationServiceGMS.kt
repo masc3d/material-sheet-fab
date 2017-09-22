82f86a3bcd3b37e0ee615f7c7c3984f3f0d68bf1
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-09-22 21:15:52 +0200

    leoz-mobile: Updated GooglePlay LocationServices

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/service/LocationServiceGMS.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/service/LocationServiceGMS.kt
@@ -12,18 +12,37 @@ import com.google.android.gms.location.LocationListener
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import org.threeten.bp.Duration
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import android.content.IntentSender
+import org.deku.leoz.mobile.ui.activity.MainActivity
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.common.api.ApiException
+
+
+
+
 
 /**
  * Created by 27694066 on 22.09.2017.
  */
 class LocationServiceGMS(
         var period: Duration = Duration.ofSeconds(30),
-        var minDistance: Float = 250F,
         var enabled: Boolean = true
 ):
         BaseLocationService(),
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
+
+    val locationRequest = LocationRequest().also {
+        it.interval = this.period.toMillis()
+        it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        it.smallestDisplacement = 200F
+    }
+
+    var builder: LocationSettingsRequest.Builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
 
     val googleApiClient: GoogleApiClient by lazy {
         GoogleApiClient.Builder(applicationContext)
@@ -31,6 +50,35 @@ class LocationServiceGMS(
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build()
+    }
+
+    val settingsClient = LocationServices.getSettingsClient(this)
+    val task = settingsClient.checkLocationSettings(builder.build()).also {
+        it.addOnSuccessListener {
+
+        }
+
+        it.addOnFailureListener {
+            val statusCode = (it as ApiException).statusCode
+            when (statusCode) {
+                CommonStatusCodes.RESOLUTION_REQUIRED ->
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        val resolvable = it as ResolvableApiException
+                        resolvable.startResolutionForResult(this@Activity,
+                                REQUEST_CHECK_SETTINGS)
+                    } catch (sendEx: IntentSender.SendIntentException) {
+                        // Ignore the error.
+                    }
+
+                LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                }
+            }// Location settings are not satisfied. However, we have no way
+            // to fix the settings so we won't show the dialog.
+        }
     }
 
     override fun onBind(intent: Intent?) = null
@@ -46,11 +94,6 @@ class LocationServiceGMS(
     }
 
     override fun onConnected(p0: Bundle?) {
-        val locationRequest = LocationRequest().also {
-            it.interval = this.period.toMillis()
-            it.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            it.smallestDisplacement = 500F
-        }
         if(ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
             LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, locationListener)
     }
