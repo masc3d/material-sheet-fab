09b80d7d6439b50dbb2b05a0a97c336ea6d31f46
Jürgen Töpper <juergen.toepper@dr-t.de>
2017-10-04 12:18:47 +0200

    Abgrenzung active_from / to für Stations am debitor

---
+++ leoz-central-schema/db/migration/V42__debitor_station_user.sql
@@ -0,0 +1,95 @@
+USE dekuclient;
+
+ALTER TABLE tblauftrag
+  ADD COLUMN longitude DOUBLE NOT NULL DEFAULT 0,
+  ADD COLUMN latitude DOUBLE NOT NULL DEFAULT 0;
+
+ALTER TABLE tblauftragtmp
+  ADD COLUMN longitude DOUBLE NOT NULL DEFAULT 0,
+  ADD COLUMN latitude DOUBLE NOT NULL DEFAULT 0;
+
+ALTER TABLE tbldepotliste
+  DROP COLUMN debitor_id,
+  DROP INDEX debitorid;
+
+ALTER TABLE tbldepotliste
+  ADD COLUMN ValOk_without_bag INT NOT NULL DEFAULT 0;
+
+ALTER TABLE mst_user
+  ADD config JSON DEFAULT NULL  NULL;
+ALTER TABLE mst_user
+  ADD preferences JSON DEFAULT NULL  NULL;
+ALTER TABLE mst_user
+  DROP allowed_stations;
+
+ALTER TABLE mst_debitor
+  ADD parent_id INT DEFAULT 0 NOT NULL;
+
+CREATE TABLE mst_debitor_station (
+  id         INT(11)   NOT NULL  AUTO_INCREMENT,
+  debitor_id INT(11)   NULL,
+  station_id INT(11)   NULL,
+  ts_created TIMESTAMP NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  ts_updated TIMESTAMP NOT NULL  DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  activ_from DATE      NOT NULL  DEFAULT '0000-00-00',
+  activ_to   DATE      NOT NULL  DEFAULT '0000-00-00',
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE mst_station_user (
+  id         INT(11)   NOT NULL  AUTO_INCREMENT,
+  user_id    INT(11)   NULL,
+  station_id INT(11)   NULL,
+  ts_created TIMESTAMP NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+  ts_updated TIMESTAMP NOT NULL  DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO `sys_sync` (`table_name`) VALUES ('mst_debitor_station');
+
+DELIMITER $$
+ALTER TABLE `mst_debitor_station`
+  ADD COLUMN `sync_id` BIGINT NOT NULL DEFAULT 0,
+  ADD INDEX `ix_sync_id` (`sync_id`) $$
+
+CREATE DEFINER = CURRENT_USER TRIGGER `mst_debitor_station_sync_insert`
+BEFORE INSERT ON `mst_debitor_station`
+FOR EACH ROW
+  BEGIN
+    SET NEW.sync_id = f_sync_increment('mst_debitor_station');
+  END $$
+
+CREATE DEFINER = CURRENT_USER TRIGGER `mst_debitor_station_sync_update`
+BEFORE UPDATE ON `mst_debitor_station`
+FOR EACH ROW
+  BEGIN
+    SET NEW.sync_id = f_sync_increment('mst_debitor_station');
+  END $$
+
+DELIMITER ;
+
+INSERT INTO `sys_sync` (`table_name`) VALUES ('mst_station_user');
+
+DELIMITER $$
+ALTER TABLE `mst_station_user`
+  ADD COLUMN `sync_id` BIGINT NOT NULL DEFAULT 0,
+  ADD INDEX `ix_sync_id` (`sync_id`) $$
+
+CREATE DEFINER = CURRENT_USER TRIGGER `mst_station_user_sync_insert`
+BEFORE INSERT ON `mst_station_user`
+FOR EACH ROW
+  BEGIN
+    SET NEW.sync_id = f_sync_increment('mst_station_user');
+  END $$
+
+CREATE DEFINER = CURRENT_USER TRIGGER `mst_station_user_sync_update`
+BEFORE UPDATE ON `mst_station_user`
+FOR EACH ROW
+  BEGIN
+    SET NEW.sync_id = f_sync_increment('mst_station_user');
+  END $$
+
+
+
+
+
