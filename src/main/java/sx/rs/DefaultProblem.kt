2e727faf2d925dc2b52637235a1b7174955f4f1b
masc <masc@disappear.de>
2017-10-06 01:16:46 +0200

    MOVED DefaultProblem ot sx.rs

---
+++ src/main/java/sx/rs/DefaultProblem.kt
@@ -0,0 +1,58 @@
+package sx.rs
+
+import org.zalando.problem.Exceptional
+import org.zalando.problem.Status
+import org.zalando.problem.StatusType
+import org.zalando.problem.ThrowableProblem
+import java.net.URI
+import javax.ws.rs.core.Response
+
+/**
+ * Default problem kt implementation
+ * Created by masc on 28/03/2017.
+ */
+class DefaultProblem(
+        type: URI = URI.create("about:blank"),
+        title: String? = null,
+        status: Status = Status.BAD_REQUEST,
+        detail: String? = null,
+        instance: URI? = null,
+        cause: ThrowableProblem? = null,
+        parameters: Map<String, Any>? = null
+) :
+        org.zalando.problem.AbstractThrowableProblem(
+                type,
+                title,
+                status,
+                detail,
+                instance,
+                cause,
+                parameters
+        ) {
+
+    /**
+     * Constructor for use with JAX-RS (Response.Status)
+     */
+    constructor(
+            type: URI = URI.create("about:blank"),
+            title: String? = null,
+            status: Response.StatusType,
+            detail: String? = null,
+            instance: URI? = null,
+            cause: ThrowableProblem? = null,
+            parameters: Map<String, Any>? = null
+    ) :
+            this(
+                    type = type,
+                    title = title,
+                    status = Status.values().first { it.statusCode == status.statusCode },
+                    detail = detail,
+                    instance = instance,
+                    cause = cause,
+                    parameters = parameters
+            )
+
+    override fun getCause(): Exceptional? {
+        return super.cause
+    }
+}
