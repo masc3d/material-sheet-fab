9387ff0b8171ba42d0bf5c0eb09a36e4a62e91ec
masc <masc@disappear.de>
2017-10-10 21:05:05 +0200

    ADDED mobile support for automatic logout, resolving & closing #220

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/config/ApplicationConfiguration.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/config/ApplicationConfiguration.kt
@@ -6,10 +6,12 @@ import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.erased.bind
 import com.github.salomonbrys.kodein.erased.eagerSingleton
 import com.github.salomonbrys.kodein.erased.instance
+import org.deku.leoz.mobile.model.process.Login
 import org.slf4j.LoggerFactory
 import org.threeten.bp.Duration
 import sx.android.ApplicationStateMonitor
 import sx.android.IdleTimer
+import sx.android.rx.observeOnMainThread
 import sx.android.ui.Timer
 import sx.time.TimeSpan
 import java.util.concurrent.ScheduledExecutorService
@@ -35,9 +37,25 @@ class ApplicationConfiguration {
             bind<IdleTimer>() with eagerSingleton {
                 IdleTimer(
                         context = instance<Context>(),
-                        executor = instance<ScheduledExecutorService>()
+                        executor = instance<ScheduledExecutorService>(),
+                        checkPeriod = Duration.ofSeconds(1)
                 ).also {
-                    it.notifyIdleDuration = Duration.ofMinutes(1)
+                    it.notifyIdleDuration = Duration.ofSeconds(15)
+
+                    // Auto log-off
+                    it.isIdleProperty
+                            .distinctUntilChanged()
+                            .filter { it.value == true }
+                            .observeOnMainThread()
+                            .subscribe {
+                                val login = instance<Login>()
+
+                                login.authenticatedUser?.also {
+                                    log.trace("Auto logging off user [$it]")
+                                    login.logout()
+                                }
+                            }
+
                 }
             }
         }
