9ce108679789eeb6b32f8a460452cad704f97721
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-10-15 14:41:00 +0200

    Squashed 'leoz-central-schema/' changes from ced172460..2a3503f5d
    2a3503f5d ADD index auftragtmp
    9f4cfe777 ADD id autoincrement for tblauftragcollies migrate schema to 48
    
    git-subtree-dir: leoz-central-schema
    git-subtree-split: 2a3503f5d517eddb7b3722639829e964093de0c5

--- db/migration/V46__tblauftragcollies_add_ID.sql
+++ db/migration/V46__tblauftragcollies_add_ID.sql
@@ -5,15 +5,9 @@ Apply this migration to live only on sunday, before evening
 
 use dekuclient;
 
-START TRANSACTION;
 ALTER TABLE tblauftragcollies
-  MODIFY OrderPos SMALLINT(6) NOT NULL;
-ALTER TABLE tblauftragcollies
-  DROP PRIMARY KEY;
-ALTER TABLE tblauftragcollies
-  ADD CONSTRAINT unique_position_in_order UNIQUE (OrderID, OrderPos);
-ALTER TABLE tblauftragcollies
-  ADD id INT NOT NULL PRIMARY KEY AUTO_INCREMENT;
-ALTER TABLE tblauftragcollies
-  MODIFY COLUMN id INT AUTO_INCREMENT FIRST;
-COMMIT;
+  AUTO_INCREMENT = 15000000 ,
+  CHANGE COLUMN OrderPos OrderPos SMALLINT(6) NOT NULL ,
+  ADD COLUMN parcel_id INT(11) NOT NULL AUTO_INCREMENT FIRST ,
+  DROP PRIMARY KEY,
+  ADD PRIMARY KEY (parcel_id);
