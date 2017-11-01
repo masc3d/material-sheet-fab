1a4f768e653970181261b05e56b30a22c1fa3afa
masc <masc@disappear.de>
2017-11-01 11:15:05 +0100

    UPDATED sx.android.flexibleadapter: class names

--- src/main/java/sx/android/ui/flexibleadapter/SimpleVmItem.kt
+++ src/main/java/sx/android/ui/flexibleadapter/SimpleVmItem.kt
@@ -5,13 +5,13 @@ import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 
 /**
- * Flexible view model item
+ * Simple view model item without subitems
+ *
  * Created by masc on 26.06.17.
- * @property view The view layout resource ID
- * @property variable View model binding ID
- * @property viewModel The view model
- * @property dragHandleViewId The view id of the drag handle (wheb moving items is supported)
- * @property beginDelayedTransition Begins a transition everytime the binding is rebound (eg. bound fields change)
+ * @param view The view layout resource ID
+ * @param variable View model binding ID
+ * @param viewModel The view model
+ * @param dragHandleViewId View id of the drag handle
  */
 open class SimpleVmItem<VM>(
         @LayoutRes view: Int,
