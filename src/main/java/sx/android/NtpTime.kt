764eec347fdea18bb7b8dc43d63979a1c39c84c9
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2017-11-07 09:53:17 +0100

    [libs-sx-android] leoz-mobile: Moved "TimeConfiguration.Time" class into sx-android, Updated Time/NtpTime: Removed dependencies to leoz-mobile-app specific classes/objects

---
+++ src/main/java/sx/android/NtpTime.kt
@@ -0,0 +1,71 @@
+package sx.android
+
+import android.content.Context
+import com.instacart.library.truetime.TrueTimeRx
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
+import org.slf4j.LoggerFactory
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created by prangenberg on 07.11.17.
+ */
+open class NtpTime(
+        private val context: Context,
+        private val ntpHost: String,
+        private val maxRetryCount: Int = 5,
+        private val trueTimeInternalLoggingEnabled: Boolean = true
+) {
+
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    /**
+     * This observable emits every 5 minutes the current Date/Time offset.
+     */
+    val offsetObservable = Observable.create<Float?> {
+        Schedulers.newThread().schedulePeriodicallyDirect(
+                {
+                    val offset = getOffset()
+                    if (offset == null) {
+                        log.warn("TrueTimeRx not yet initialized")
+                    }
+                    else {
+                        it.onNext(offset)
+                    }
+                },
+                0,
+                5,
+                TimeUnit.MINUTES)
+    }
+
+    init {
+        TrueTimeRx.build()
+                .withSharedPreferences(this.context)
+                .withLoggingEnabled(this.trueTimeInternalLoggingEnabled)
+                .withRetryCount(this.maxRetryCount)
+                .initializeRx(this.ntpHost)
+                .ignoreElements()
+                .subscribeOn(Schedulers.io())
+                .subscribeBy(
+                        onComplete = {
+                            log.trace("Initializing TrueTime succeeded")
+                        },
+
+                        onError = {
+                            log.warn("Initializing TrueTime failed with error.", it)
+                        }
+                )
+    }
+
+    fun currentNtpDateTime(): Date? {
+        if (!TrueTimeRx.isInitialized()) {
+            return null
+        }
+
+        return TrueTimeRx.now()
+    }
+
+    private fun getOffset(): Float? = if (!TrueTimeRx.isInitialized()) null else (this.currentNtpDateTime()!!.time - Date().time) / 1000F
+}
