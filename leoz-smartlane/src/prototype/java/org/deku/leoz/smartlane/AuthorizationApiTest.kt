cb252269806d3787c4127c74b4d196d7c3b1f93a
masc <masc@disappear.de>
2017-11-15 21:44:16 +0100

    ADDED smartlane: first draft of functional REST client test, fixed openapi spec

---
+++ leoz-smartlane/src/prototype/java/org/deku/leoz/smartlane/AuthorizationApiTest.kt
@@ -0,0 +1,27 @@
+package org.deku.leoz.smartlane
+
+import org.deku.leoz.smartlane.api.AuthorizationApi
+import org.junit.Test
+import org.slf4j.LoggerFactory
+import sx.rs.client.RestEasyClient
+import java.net.URI
+
+/**
+ * Created by masc on 15.11.17.
+ */
+class AuthorizationApiTest {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    private val restClient by lazy {
+        RestEasyClient(
+            baseUri = URI.create("https://dispatch.smartlane.io/der-kurier-test/api/")
+        )
+    }
+
+    @Test
+    fun testRefreshToken() {
+        restClient.proxy(AuthorizationApi::class.java).also {
+            log.trace("Token refreshed ${it.refreshToken}")
+        }
+    }
+}
