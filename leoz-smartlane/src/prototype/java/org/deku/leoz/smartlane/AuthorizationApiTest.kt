bb77e4d1a22aabdb2d9a01a0160c4ff228d972c8
masc <masc@disappear.de>
2017-11-17 00:42:01 +0100

    ADDED smartlane address api tests

--- leoz-smartlane/src/prototype/java/org/deku/leoz/smartlane/AuthorizationApiTest.kt
+++ leoz-smartlane/src/prototype/java/org/deku/leoz/smartlane/AuthorizationApiTest.kt
@@ -1,10 +1,15 @@
 package org.deku.leoz.smartlane
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationConfig
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import org.deku.leoz.smartlane.api.AddressApi
 import org.deku.leoz.smartlane.api.AuthApi
 import org.deku.leoz.smartlane.api.AuthorizationApi
+import org.deku.leoz.smartlane.model.Address
 import org.junit.Test
 import org.junit.experimental.categories.Category
 import org.slf4j.LoggerFactory
@@ -23,9 +28,32 @@ class AuthorizationApiTest {
         RestEasyClient(
                 baseUri = URI.create("https://dispatch.smartlane.io/der-kurier-test/"),
                 objectMapper = ObjectMapper().also {
+                    // Don't epxlicitly serialize nulls with json (breaks swagger-ui too when having all those nulls in swagger.json)
+                    it.setSerializationInclusion(JsonInclude.Include.NON_NULL)
                     it.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+                    it.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
                 }
         )
+    }
+
+    private fun auth() {
+        this.restClient.jwtToken = restClient
+                .proxy(AuthApi::class.java)
+                .auth(AuthApi.Request(
+                        email = "juergen.toepper@derkurier.de",
+                        password = "PanicLane"
+                ))
+                .accessToken
+    }
+
+    private val jwtToken by lazy {
+        restClient
+                .proxy(AuthApi::class.java)
+                .auth(AuthApi.Request(
+                        email = "juergen.toepper@derkurier.de",
+                        password = "PanicLane"
+                ))
+                .accessToken
     }
 
     @Test
@@ -43,6 +71,36 @@ class AuthorizationApiTest {
                             email = "juergen.toepper@derkurier.de",
                             password = "PanicLane"
                     ))
+            )
+        }
+    }
+
+    @Test
+    fun testAddressPost() {
+        this.auth()
+
+        restClient.proxy(AddressApi::class.java).also {
+            log.trace(
+                    it.postAddress(
+                            Address().also {
+                                it.contactcompany = "Test"
+                                it.street = "Waldhof"
+                                it.housenumber = "1"
+                                it.city = "Schaafheim"
+                                it.postalcode = "64850"
+                            }
+                    )
+            )
+        }
+    }
+
+    @Test
+    fun testAddressGet() {
+        this.auth()
+
+        restClient.proxy(AddressApi::class.java).also {
+            log.trace(
+                    it.address
             )
         }
     }
