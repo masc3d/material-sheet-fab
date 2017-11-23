d675182a6126d143a3aea6e97df641317615f126
masc <masc@disappear.de>
2017-11-23 16:12:07 +0100

    UPDATED BundleRepositoryTest: added prototype test for concurrent bundle donwloads via ssh/rsync

--- leoz-core/src/prototype/java/org/deku/leoz/bundle/BundleRepositoryTest.kt
+++ leoz-core/src/prototype/java/org/deku/leoz/bundle/BundleRepositoryTest.kt
@@ -3,15 +3,20 @@ package org.deku.leoz.bundle
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.conf.global
 import com.github.salomonbrys.kodein.erased.instance
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.merge
+import io.reactivex.schedulers.Schedulers
 import org.apache.commons.lang3.SystemUtils
 import org.deku.leoz.config.BundleConfiguration
 import org.deku.leoz.config.RsyncTestConfiguration
 import org.junit.Test
+import org.slf4j.LoggerFactory
 import sx.packager.Bundle
 import sx.platform.CpuArch
 import sx.platform.OperatingSystem
 import sx.platform.PlatformId
 import sx.rsync.Rsync
+import sx.rx.limit
 import java.io.File
 
 /**
@@ -24,24 +29,50 @@ class BundleRepositoryTest {
         }
     }
 
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
     @Test
-    fun testDownload() {
-        val BUNDLE_NAME = "leoz-central"
-        val path = File("build/download", BUNDLE_NAME)
+    fun testConcurrentDownloads() {
+        val BUNDLE_NAME = BundleType.LEOZ_BOOT.value
+        val HOST = "10.211.55.7"
+        val DOWNLOADS = 100
+        val DOWNLOADS_CONCURRENCY = 8
 
-        path.mkdirs()
+        val repository = BundleConfiguration.createRepository(HOST)
 
-        val repository = BundleConfiguration.stagingRepository
+        val rootPath = File("build")
+                .resolve("download")
+
+        val scheduler = Schedulers.io().limit(DOWNLOADS_CONCURRENCY)
 
         try {
-            repository.download(
-                    bundleName = BUNDLE_NAME,
-                    version = repository.listVersions("leoz-central").first(),
-                    platformId = PlatformId(OperatingSystem.WINDOWS, CpuArch.X64),
-                    destPath = path)
+            (0..DOWNLOADS).map { i ->
+                Observable.fromCallable {
+                    val path = rootPath.resolve("${BUNDLE_NAME}-${i}")
+
+                    try {
+                        log.trace("Start ${path}")
+                        path.mkdirs()
+
+                        repository.download(
+                                bundleName = BUNDLE_NAME,
+                                version = repository.listVersions(BUNDLE_NAME).first(),
+                                platformId = PlatformId(OperatingSystem.WINDOWS, CpuArch.X64),
+                                destPath = path
+                        )
+                    } finally {
+                        log.trace("Removing ${path}")
+                        path.deleteRecursively()
+                    }
+                }
+                        .subscribeOn(scheduler)
+            }
+                    .merge()
+                    .blockingIterable()
+                    .toList()
         } finally {
-            println("Cleaning up")
-            path.deleteRecursively()
+            println("Removing ${rootPath}")
+            rootPath.deleteRecursively()
         }
     }
 }
