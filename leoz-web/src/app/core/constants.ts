190af231d45172245af339f6fd7a129346bde730
Janussek <info@it-cobra.es>
2017-11-30 13:00:44 +0100

    extract nowMinusXHours into constants

--- leoz-web/src/app/core/constants.ts
+++ leoz-web/src/app/core/constants.ts
@@ -1,1 +1,2 @@
 export const TYPE_VALUABLE = 91;
+export const NOW_MINUS_X_HOURS = 5; // workingdate = now - (<=== this value)
--- leoz-web/src/app/core/workingdate.service.ts
+++ leoz-web/src/app/core/workingdate.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 
 import * as moment from 'moment';
 
-import { environment } from '../../environments/environment';
+import { NOW_MINUS_X_HOURS } from './constants';
 
 @Injectable()
 export class WorkingdateService {
 
   public workingDate() {
-    const nowMinusXHours = moment().subtract( environment.nowMinusXHours, 'hours' );
+    const nowMinusXHours = moment().subtract( NOW_MINUS_X_HOURS, 'hours' );
     return nowMinusXHours.format( 'YYYY/MM/DD' );
   }
 }
