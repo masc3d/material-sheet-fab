a18c5024ef30ee95478995ac3bb768582625fb98
masc <masc@disappear.de>
2017-12-13 12:54:09 +0100

    FIXED swagge-3.x doesn't support/render x-example by applying workaround for pending PR https://github.com/swagger-api/swagger-ui/pull/3538

--- leoz-node/src/main/java/org/deku/leoz/node/rest/swagger/SwaggerListingResourceBase.kt
+++ leoz-node/src/main/java/org/deku/leoz/node/rest/swagger/SwaggerListingResourceBase.kt
@@ -2,8 +2,11 @@ package org.deku.leoz.node.rest.swagger
 
 import io.swagger.annotations.ApiOperation
 import io.swagger.jaxrs.listing.BaseApiListingResource
+import io.swagger.models.Swagger
+import io.swagger.models.parameters.AbstractSerializableParameter
 import org.apache.commons.lang3.StringUtils
 import org.jboss.resteasy.spi.ResteasyUriInfo
+import org.slf4j.LoggerFactory
 import java.net.URI
 import java.nio.file.Paths
 import javax.inject.Inject
@@ -21,6 +24,8 @@ import javax.ws.rs.core.*
  * Based on {@link io.swagger.jaxrs.listing.BaseApiListingResource}
  */
 abstract class SwaggerListingResourceBase : BaseApiListingResource() {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
     @Inject
     private lateinit var context: ServletContext
 
@@ -48,6 +53,27 @@ abstract class SwaggerListingResourceBase : BaseApiListingResource() {
             return getListingYamlResponse(app, context, sc, headers, newUriInfo)
         } else {
             return getListingJsonResponse(app, context, sc, headers, newUriInfo)
+        }
+    }
+
+    override fun process(app: Application?, servletContext: ServletContext?, sc: ServletConfig?, headers: HttpHeaders?, uriInfo: UriInfo?): Swagger {
+        return super.process(app, servletContext, sc, headers, uriInfo).also { swagger ->
+            swagger.paths.values.forEach {
+                it.operations?.forEach {
+                    it.parameters?.forEach {
+                        if (it is AbstractSerializableParameter<*>) {
+                            val example = it.getExample()
+                            val default = it.getDefaultValue()
+
+                            // Set default values from example when default is not explicitly supplied.
+                            // TODO: This is a workaround for pending swagger pull request https://github.com/swagger-api/swagger-ui/pull/3538
+                            if (example != null && default == null) {
+                                it.setDefaultValue(example.toString())
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
