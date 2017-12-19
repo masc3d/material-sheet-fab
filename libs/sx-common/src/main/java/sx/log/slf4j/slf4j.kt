0dba5f7bc865adeb65d1143ac2130d3843b98839
masc <masc@disappear.de>
2017-12-20 00:31:06 +0100

    ADDED sx.log.slf4j: extensions for lazy message evaluation

--- libs/sx-common/src/main/java/sx/log/slf4j/slf4j.kt
+++ libs/sx-common/src/main/java/sx/log/slf4j/slf4j.kt
@@ -1,6 +1,7 @@
 package sx.log.slf4j
 
 import org.slf4j.Logger
+import org.slf4j.Marker
 import org.slf4j.bridge.SLF4JBridgeHandler
 import java.util.logging.Level
 import java.util.logging.LogManager
@@ -28,6 +29,88 @@ fun Logger.error(it: Any?) {
 fun Logger.trace(it: Any?) {
     this.trace(it.toString())
 }
+
+//region Lazy message evaluation
+inline fun Logger.error(it: () -> Any?) {
+    if (isErrorEnabled) error(it().toString())
+}
+
+inline fun Logger.warn(it: () -> Any?) {
+    if (isWarnEnabled) warn(it().toString())
+}
+
+inline fun Logger.info(it: () -> Any?) {
+    if (isInfoEnabled) info(it().toString())
+}
+
+inline fun Logger.debug(it: () -> Any?) {
+    if (isDebugEnabled) debug(it().toString())
+}
+
+inline fun Logger.trace(it: () -> Any?) {
+    if (isTraceEnabled) trace(it().toString())
+}
+
+inline fun Logger.error(marker: Marker?, it: () -> Any?) {
+    if (isErrorEnabled(marker)) error(marker, it().toString())
+}
+
+inline fun Logger.warn(marker: Marker?, it: () -> Any?) {
+    if (isWarnEnabled(marker)) warn(marker, it().toString())
+}
+
+inline fun Logger.info(marker: Marker?, it: () -> Any?) {
+    if (isInfoEnabled(marker)) info(marker, it().toString())
+}
+
+inline fun Logger.debug(marker: Marker?, it: () -> Any?) {
+    if (isDebugEnabled(marker)) debug(marker, it().toString())
+}
+
+inline fun Logger.trace(marker: Marker?, it: () -> Any?) {
+    if (isTraceEnabled(marker)) trace(marker, it().toString())
+}
+
+inline fun Logger.error(throwable: Throwable?, it: () -> Any?) {
+    if (isErrorEnabled) error(it().toString(), throwable)
+}
+
+inline fun Logger.warn(throwable: Throwable?, it: () -> Any?) {
+    if (isWarnEnabled) warn(it().toString(), throwable)
+}
+
+inline fun Logger.info(throwable: Throwable?, it: () -> Any?) {
+    if (isInfoEnabled) info(it().toString(), throwable)
+}
+
+inline fun Logger.debug(throwable: Throwable?, it: () -> Any?) {
+    if (isDebugEnabled) debug(it().toString(), throwable)
+}
+
+inline fun Logger.trace(throwable: Throwable?, it: () -> Any?) {
+    if (isTraceEnabled) trace(it().toString(), throwable)
+}
+
+inline fun Logger.error(marker: Marker?, throwable: Throwable?, it: () -> Any?) {
+    if (isErrorEnabled(marker)) error(marker, it().toString(), throwable)
+}
+
+inline fun Logger.warn(marker: Marker?, throwable: Throwable?, it: () -> Any?) {
+    if (isWarnEnabled(marker)) warn(marker, it().toString(), throwable)
+}
+
+inline fun Logger.info(marker: Marker?, throwable: Throwable?, it: () -> Any?) {
+    if (isInfoEnabled(marker)) info(marker, it().toString(), throwable)
+}
+
+inline fun Logger.debug(marker: Marker?, throwable: Throwable?, it: () -> Any?) {
+    if (isDebugEnabled(marker)) debug(marker, it().toString(), throwable)
+}
+
+inline fun Logger.trace(marker: Marker?, throwable: Throwable?, it: () -> Any?) {
+    if (isTraceEnabled(marker)) trace(marker, it().toString(), throwable)
+}
+//endregion
 
 /**
  * Helper for installing the JUL (java.util.logging) bridge,
