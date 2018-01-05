42587950168fd28dfb3e84f2c3b2f135c7395c74
masc <masc@disappear.de>
2018-01-06 00:35:22 +0100

    Squashed 'libs/sx-android/' changes from da3d559e9..794db93f1
    794db93f1 Merge commit '50689838a48cdb917b21b34bcf8baf4ca467f7c1'
    ebfcbbe13 UPDATED to flyway-5.0.4, rxjava-2.1.8, anko-0.10.4, reactivenetwork-0.12.3
    0bcd6ac3d UPDATED to gradle-4.4.1, android-build-tools-27.0.3
    52b221f18 UPDATED MqttSqlitePersistence: added support for adaptive batch sizes. this will imrpove throughput for small payloads and respectively optimize memory consumption for large payloads
    9d5a8d880 UPDATED to flyway-5.0.3, jackson-2.9.3, android-gms-11.8.0, roboelectric-3.6.1, javassist-3.22.0-GA
    737ddec5b FIXED MqttSqlitePersistence: sqlite doesn't properly support record modifications during cursor iteration of large results
    34d96eb2a UPDATED to flexible-adapter-5.0.0-rc4
    d85ed3a7b UPDATED sx-android: minor cleanup
    71f94e7de UPDATED sx-android: added sqlite backup extensions
    c6f6e5de9 UPDATED to paho-1.2.0, camerakit-0.13.1, sqlite-android-3.21.0
    21359d947 UPDATED to kt-1.2.10
    f1e806cc5 UPDATED sx-android to kt-1.2
    a253d9567 electron printing
    
    git-subtree-dir: libs/sx-android
    git-subtree-split: 794db93f10b8d044eaafaad8d295ddbc5f6c38af

--- src/main/java/sx/android/mqtt/MqttSqlitePersistence.kt
+++ src/main/java/sx/android/mqtt/MqttSqlitePersistence.kt
@@ -5,10 +5,14 @@ import io.reactivex.Observable
 import org.eclipse.paho.client.mqttv3.MqttMessage
 import org.jetbrains.anko.db.*
 import org.slf4j.LoggerFactory
+import sx.Stopwatch
 import sx.android.anko.sqlite.getBoolean
 import sx.android.anko.sqlite.getByteArray
 import sx.android.anko.sqlite.getInt
 import sx.android.anko.sqlite.getString
+import sx.android.database.sqlite.backupTo
+import sx.log.slf4j.info
+import sx.log.slf4j.trace
 import sx.mq.mqtt.IMqttPersistence
 import sx.mq.mqtt.MqttPersistentMessage
 import sx.mq.mqtt.toPersistentMessage
@@ -17,23 +21,27 @@ import java.io.File
 /**
  * MQTT dispatcher persistence implementation for SQLite on android
  * @param databaseFile Database file to use
+ * @param maxInMemoryBatchSize The maximum (in-memory) size of a single batch (in bytes)
  * Created by masc on 16.05.17.
  * TODO: (not yet) thread-safe. currently not suited for sharing among multiple dispatchers (or a dispatcher implementing multi-threaded dequee)
  */
 class MqttSqlitePersistence constructor(
-        private val databaseFile: File
+        private val databaseFile: File,
+        private val maxInMemoryBatchSize: Int = MAX_BATCH_SIZE_BYTES
 ) : IMqttPersistence {
-
     private val log = LoggerFactory.getLogger(this.javaClass)
 
-    companion object {
-        private val TABLE_NAME = "mqtt"
-        private val COL_ID = "id"
-        private val COL_TOPIC = "topic"
-        private val COL_QOS = "qos"
-        private val COL_RETAINED = "retained"
-        private val COL_MESSAGEID = "message_id"
-        private val COL_PAYLOAD = "payload"
+    private companion object {
+        const val TABLE_NAME = "mqtt"
+        const val COL_ID = "id"
+        const val COL_TOPIC = "topic"
+        const val COL_QOS = "qos"
+        const val COL_RETAINED = "retained"
+        const val COL_MESSAGEID = "message_id"
+        const val COL_PAYLOAD = "payload"
+
+        const val MAX_BATCH_SIZE = 4096
+        const val MAX_BATCH_SIZE_BYTES = 5 * 1024 * 1024
     }
 
     private val db by lazy {
@@ -58,6 +66,14 @@ class MqttSqlitePersistence constructor(
                         Pair(COL_PAYLOAD, BLOB)))
 
         this.db.execSQL("CREATE INDEX IF NOT EXISTS ix_topic ON ${TABLE_NAME} ( ${COL_TOPIC} )")
+    }
+
+    /**
+     * Backup persistence database
+     * @param destinationFile Destination file
+     */
+    fun backup(destinationFile: File) {
+        this.db.backupTo(destinationFile)
     }
 
     override fun add(topicName: String, message: MqttMessage) {
@@ -93,21 +109,75 @@ class MqttSqlitePersistence constructor(
     }
 
     override fun get(topicName: String?): Observable<MqttPersistentMessage> {
-        return Observable.create<MqttPersistentMessage> { onSubscribe ->
-            this.db.select(
-                    tableName = TABLE_NAME
-            ).exec {
+        return Observable.create<MqttPersistentMessage> { emitter ->
+            try {
                 val parser = MessageRowParser()
 
-                try {
-                    asMapSequence().forEach {
-                        onSubscribe.onNext(parser.parseRow(it))
+                // Group selects into batches, so it's safe for consumers
+                // to remove records even on large queues
+                var lastId = -1
+
+                loop@ while (!emitter.isDisposed) {
+                    var batchSize: Int = 0
+                    var batchSizeBytes: Int = 0
+
+                    //region Determine batch size
+                    // Peek payload sizes
+                    val sizes = Stopwatch.createStarted(this, "SELECT BATCH SIZES", { _, _ ->
+                        val COL_SIZE = "LENGTH(${COL_PAYLOAD})"
+                        this.db.select(TABLE_NAME, COL_SIZE)
+                                .orderBy(COL_ID)
+                                .limit(MAX_BATCH_SIZE)
+                                .exec {
+                                    asSequence()
+                                            .map { IntParser.parseRow(it) }
+                                            .toList()
+                                }
+
+                    })
+
+                    log.info { "Maximum batch size ${sizes.count()} -> ${sizes.sum()} bytes" }
+
+                    for (size in sizes) {
+                        batchSizeBytes += size
+                        if (batchSizeBytes > this.maxInMemoryBatchSize)
+                            break
+                        batchSize++
                     }
-                } catch (e: Throwable) {
-                    onSubscribe.onError(e)
-                    return@exec
+                    batchSize = if (batchSize > 0) batchSize else 1
+                    //endregion
+
+                    log.info { "Processing batch size ${batchSize} -> ${batchSizeBytes} bytes" }
+                    val batch = Stopwatch.createStarted(this, "SELECT BATCH", { _, _ ->
+                        this.db.select(TABLE_NAME)
+                                .whereArgs(
+                                        select = "${COL_ID} > {${COL_ID}}",
+                                        args = *arrayOf(COL_ID to lastId)
+                                )
+                                .orderBy(COL_ID)
+                                .limit(batchSize)
+                                .exec {
+                                    asMapSequence()
+                                            .map { parser.parseRow(it) }
+                                            .toList()
+                                }
+
+                    })
+
+                    // No results -> done
+                    if (batch.size == 0)
+                        break@loop
+
+                    // Store last id for next batch/iteration
+                    lastId = batch.last().persistentId
+
+                    // Emit message batch
+                    batch.forEach { emitter.onNext(it) }
                 }
-                onSubscribe.onComplete()
+
+                emitter.onComplete()
+            } catch (e: Throwable) {
+                emitter.onError(e)
             }
         }
     }
@@ -116,7 +186,7 @@ class MqttSqlitePersistence constructor(
         this.db.delete(
                 tableName = TABLE_NAME,
                 whereClause = "${COL_ID} = {${COL_ID}}",
-                args = COL_ID to message.persistentId
+                args = *arrayOf(COL_ID to message.persistentId)
         )
     }
 
@@ -144,5 +214,4 @@ class MqttSqlitePersistence constructor(
                             .toMap()
                 }
     }
-
 }
