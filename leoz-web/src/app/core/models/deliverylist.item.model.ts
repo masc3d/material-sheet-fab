5b444dc544a94cb6a3a2afa6104fe8039eb346c0
Janussek <info@it-cobra.es>
2018-01-29 21:56:35 +0100

    loading existing tourlists

--- leoz-web/src/app/core/models/deliverylist.item.model.ts
+++ leoz-web/src/app/core/models/deliverylist.item.model.ts
@@ -1,6 +1,8 @@
 import { Parcel } from './parcel.model';
+import { Address } from './address.model';
 
 export interface DeliverylistItem {
   id: number;
+  deliveryAddress?: Address;
   parcels: Parcel[];
 }
