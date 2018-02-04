c1446d0c06a14d333262aea554bfc0658b4dae00
masc <masc@disappear.de>
2018-02-04 15:17:26 +0100

    UPDATED address: swagger doc

--- leoz-common/src/main/java/org/deku/leoz/service/internal/entity/Address.kt
+++ leoz-common/src/main/java/org/deku/leoz/service/internal/entity/Address.kt
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModel
 import io.swagger.annotations.ApiModelProperty
 
 
-@ApiModel(value = "Address", description = "universal address")
+@ApiModel(value = "Address", description = "Universal address")
 data class Address(
         @ApiModelProperty(dataType = "string", example = "Hans Mustermann", position = 10, required = false, value = "addressLine1")
         var line1: String? = null,
@@ -28,7 +28,7 @@ data class Address(
         var geoLocation: GeoLocation? = null
 )
 
-@ApiModel(value = "geoLocation", description = "geoLocation")
+@ApiModel(value = "GeoLocation", description = "GeoLocation")
 data class GeoLocation(
         @get:ApiModelProperty(dataType = "Double", example = "52.76866", position = 10, required = true, value = "latitude")
         var latitude: Double = 0.0,
