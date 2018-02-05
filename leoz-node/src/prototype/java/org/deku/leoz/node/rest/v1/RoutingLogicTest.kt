c65042853e26d28a366d24d73a06a926d72b7f9f
masc <masc@disappear.de>
2018-02-05 16:14:24 +0100

    UPDATED RoutingLogicTest category

--- leoz-node/src/prototype/java/org/deku/leoz/node/rest/v1/RoutingLogicTest.kt
+++ leoz-node/src/prototype/java/org/deku/leoz/node/rest/v1/RoutingLogicTest.kt
@@ -12,13 +12,14 @@ import org.junit.experimental.categories.Category
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import sx.junit.PrototypeTest
 import sx.junit.StandardTest
 import javax.inject.Inject
 
 /**
  * Created by JT on 15.05.15.
  */
-@Category(StandardTest::class)
+@Category(PrototypeTest::class)
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = arrayOf(
         DataTestConfiguration::class,
