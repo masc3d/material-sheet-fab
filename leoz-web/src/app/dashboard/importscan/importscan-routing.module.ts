40d29ad88424bfd288ad81a10f779fe93f6c959f
Janussek <info@it-cobra.es>
2018-02-07 12:55:36 +0100

    remove obsolete lists

--- leoz-web/src/app/dashboard/importscan/importscan-routing.module.ts
+++ leoz-web/src/app/dashboard/importscan/importscan-routing.module.ts
@@ -10,14 +10,6 @@ const routes: Routes = [
       {
         path: 'importscanquick',
         loadChildren: 'app/dashboard/importscan/importscanquick/importscanquick.module#ImportscanquickModule'
-      },
-      {
-        path: 'importscanlist',
-        loadChildren: 'app/dashboard/importscan/importscanlist/importscanlist.module#ImportscanlistModule'
-      },
-      {
-        path: 'importscancheck',
-        loadChildren: 'app/dashboard/importscan/importscancheck/importscancheck.module#ImportscancheckModule'
       }
     ]
   }
