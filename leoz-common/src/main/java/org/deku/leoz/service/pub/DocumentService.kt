a9cefce0f14de1e5dadb6ddba86c668a942fadf1
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2018-02-08 11:53:26 +0100

    leoz-common: Updated DocumentService interface

--- leoz-common/src/main/java/org/deku/leoz/service/pub/DocumentService.kt
+++ leoz-common/src/main/java/org/deku/leoz/service/pub/DocumentService.kt
@@ -5,9 +5,10 @@ import org.deku.leoz.service.internal.OrderService
 import sx.rs.auth.ApiKey
 import javax.ws.rs.*
 import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
 
 @Path("v1/document")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_OCTET_STREAM)
 @Consumes(MediaType.APPLICATION_JSON)
 @Api(value = "Document service")
 @ApiKey(false)
@@ -17,39 +18,49 @@ interface DocumentService {
         const val PARCELNO = "parcel-no"
     }
 
-    @ApiModel(value = "LabelRequest", description = "Request for label print")
+    @ApiModel(value = "LabelRequest", description = "Request for label print", subTypes = [(LabelRequest.LabelParticipant::class)])
     class LabelRequest {
 
         @ApiModel(value = "LabelParticipant", description = "Request participant. Consignor or consignee")
         class LabelParticipant {
-            @ApiModelProperty(position = 10, required = true, value = "Station Number")
-            var stationNo: String? = null
-            @ApiModelProperty(position = 20, required = true, value = "Name 1")
-            var name1: String? = null
-            @ApiModelProperty(position = 30, required = false, value = "Name 2")
+            @ApiModelProperty(position = 10, required = true, value = "Station Number", example = "100")
+            var stationNo: String = "100"
+            @ApiModelProperty(position = 20, required = true, value = "Name 1", example = "Max Mustermann")
+            var name1: String = "Max Mustermann"
+            @ApiModelProperty(position = 30, required = false, value = "Name 2", example = "Name2")
             var name2: String? = null
-            @ApiModelProperty(position = 40, required = true, value = "Street")
-            var street: String? = null
-            @ApiModelProperty(position = 50, required = true, value = "City")
-            var city: String? = null
-            @ApiModelProperty(position = 60, required = true, value = "Zip code")
-            var zipCode: String? = null
-            @ApiModelProperty(position = 70, required = false, value = "Phone")
+            @ApiModelProperty(position = 40, required = false, value = "Name 3", example = "Name3")
+            var name3: String? = null
+            @ApiModelProperty(position = 50, required = true, value = "Street", example = "Dörrwiese")
+            var street: String = "Doerrwiese"
+            @ApiModelProperty(position = 60, required = true, value = "Street-No", example = "2")
+            var streetNo: String = "2"
+            @ApiModelProperty(position = 70, required = true, value = "City", example = "Neuenstein")
+            var city: String = "Neuenstein"
+            @ApiModelProperty(position = 80, required = true, value = "Zip code", example = "36286")
+            var zipCode: String = "36286"
+            @ApiModelProperty(position = 90, required = false, value = "Phone", example = "+49 6677 950")
             var phone: String? = null
-            @ApiModelProperty(position = 80, required = false, value = "Notice")
+            @ApiModelProperty(position = 100, required = false, value = "Notice", example = "Hinten klingeln")
             var notice: String? = null
         }
 
-        @ApiModelProperty(position = 10, required = true, value = "Parcel ID")
-        var parcelId: Long? = null
-        @ApiModelProperty(position = 20, required = true, value = "Order ID")
-        var orderId: Long? = null
-        @ApiModelProperty(position = 30, required = true, value = "Consignor")
-        var consignor: LabelParticipant? = null
-        @ApiModelProperty(position = 40, required = true, value = "Consignee")
-        var consignee: LabelParticipant? = null
-        @ApiModelProperty(position = 50, required = true, value = "Appointment")
+        @ApiModelProperty(position = 10, required = true, value = "Parcel Number", notes = "Without check digit", example = "10010000002")
+        var parcelNumber: String = "10010000001"
+        @ApiModelProperty(position = 20, required = true, value = "Order Number", notes = "Without check digit", example = "10010000001")
+        var orderNumber: String = "10010000001"
+        @ApiModelProperty(position = 30, required = true, value = "Client Station Number", example = "100")
+        var clientStationNo: String = "999"
+        @ApiModelProperty(position = 40, required = true, value = "Consignor")
+        var consignor: LabelParticipant = LabelParticipant()
+        @ApiModelProperty(position = 50, required = true, value = "Consignee")
+        var consignee: LabelParticipant = LabelParticipant()
+        @ApiModelProperty(position = 60, required = true, value = "Appointment")
         var appointment: OrderService.Order.Appointment = OrderService.Order.Appointment()
+        @ApiModelProperty(position = 70, required = false, value = "Total parcel amount", example = "2") @DefaultValue("1")
+        var parcelAmount: Int? = null
+        @ApiModelProperty(position = 80, required = false, value = "Parcel position", example = "2") @DefaultValue("1")
+        var parcelPosition: Int? = null
     }
 
     @GET
@@ -60,5 +71,5 @@ interface DocumentService {
             @ApiParam(value = "Label request", required = false) labelRequest: LabelRequest? = null,
             @QueryParam(PARCELID) @ApiParam(value = "Parcel ID", required = false) parcelId: Long? = null,
             @QueryParam(PARCELNO) @ApiParam(value = "Parcel Number", required = false) parcelNo: String? = null
-    )
+    ): Response
 }
