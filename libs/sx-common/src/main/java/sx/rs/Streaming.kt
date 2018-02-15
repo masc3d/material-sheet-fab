5f451e950182065737de869a11618749f6540567
masc <masc@disappear.de>
2018-02-15 19:36:45 +0100

    FIXED sx.rs streaming: write errors must terminate stream without rethrow

--- libs/sx-common/src/main/java/sx/rs/Streaming.kt
+++ libs/sx-common/src/main/java/sx/rs/Streaming.kt
@@ -1,6 +1,11 @@
 package sx.rs
 
+import io.reactivex.Completable
 import io.reactivex.Observable
+import io.reactivex.ObservableOperator
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
+import java.io.IOException
 import java.io.OutputStream
 import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.StreamingOutput
@@ -19,11 +24,37 @@ fun Observable<String>.toStreamingOutput(): StreamingOutput {
                 val writer = output.buffered().writer()
 
                 // There's no non-blocking in jax/rs :/
-                this@toStreamingOutput
-                        .doOnNext { writer.write(it) }
-                        .doFinally { writer.flush() }
-                        .blockingSubscribe()
 
+                this@toStreamingOutput
+                        .lift<String> {
+                            object : Observer<String> {
+                                var d: Disposable? = null
+
+                                // Pass-through
+                                override fun onComplete() { it.onComplete() }
+                                override fun onSubscribe(d: Disposable) { this.d = d; it.onSubscribe(d) }
+                                override fun onError(e: Throwable) { it.onError(e) }
+
+                                override fun onNext(t: String) {
+                                    try {
+                                        writer.write(t)
+                                        it.onNext(t)
+                                    } catch (e: Throwable) {
+                                        // Ignore errors on writing, immediately dispose upstream and complete
+                                        d?.dispose()
+                                        it.onComplete()
+                                    }
+                                }
+                            }
+                        }
+                        .doFinally {
+                            try {
+                                writer.flush()
+                            } catch (e: Throwable) {
+                                // Ignore errors on flushing
+                            }
+                        }
+                        .blockingSubscribe()
             } catch (e: Throwable) {
                 throw WebApplicationException(e)
             }
