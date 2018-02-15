353c9060d94cb549d508cd56a8634bd2c4917f11
masc <masc@disappear.de>
2018-02-15 02:04:06 +0100

    ADDED support for @RestrictRoles on service methods and classes

--- leoz-central/src/main/java/org/deku/leoz/central/rest/ApiKeyRequestFilter.kt
+++ leoz-central/src/main/java/org/deku/leoz/central/rest/ApiKeyRequestFilter.kt
@@ -4,10 +4,16 @@ import org.deku.leoz.central.data.repository.JooqKeyRepository
 import org.deku.leoz.central.data.repository.JooqNodeRepository
 import org.deku.leoz.central.data.repository.JooqUserRepository
 import org.deku.leoz.config.Rest
+import org.deku.leoz.model.UserRole
+import org.deku.leoz.rest.RestrictRoles
+import sx.annotationOfType
+import sx.reflect.allInterfaces
+import sx.rs.auth.ApiKey
 import sx.rs.auth.ApiKeyRequestFilterBase
 
 import javax.inject.Inject
 import javax.inject.Named
+import javax.ws.rs.container.ResourceInfo
 import javax.ws.rs.ext.Provider
 
 /**
@@ -25,12 +31,30 @@ class ApiKeyRequestFilter : ApiKeyRequestFilterBase(
     @Inject
     private lateinit var userJooqRepository: JooqUserRepository
 
-    @Inject
-    private  lateinit  var keyJooqRepository: JooqKeyRepository
+    /**
+     * Returns either method or class level annotation
+     * @param a Annotation type
+     */
+    private fun <A : Annotation> ResourceInfo.annotationOfType(a: Class<A>): A? {
+        return this.resourceMethod.getAnnotation(a)
+                ?: this.resourceClass.allInterfaces.map { it.getAnnotation(a) }.firstOrNull()
+    }
 
-    override fun verify(apiKey: String): Boolean {
-        return nodeJooqRepository.hasAuthorizedKey(apiKey)
-                || keyJooqRepository.findValidByKey(apiKey)
-        // || userJooqRepository.hasAuthorizedKey(apiKey)
+    override fun verify(resourceInfo: ResourceInfo, apiKey: String): Boolean {
+        val user = this.userJooqRepository.findByKey(apiKey)
+
+        // Check role restrictions
+        resourceInfo.annotationOfType(RestrictRoles::class.java)
+                ?.also {
+                    if (user == null || !it.role.contains(UserRole.valueOf(user.role)))
+                        return false
+                }
+
+        // Check for node key
+        if (user == null)
+            return nodeJooqRepository.hasAuthorizedKey(apiKey)
+
+        // All checks passed, permission granted
+        return true
     }
 }
---
+++ leoz-common/src/main/java/org/deku/leoz/rest/Annotations.kt
@@ -0,0 +1,16 @@
+package org.deku.leoz.rest
+
+import org.deku.leoz.model.UserRole
+
+/**
+ * Created by masc on 15.02.18.
+ */
+
+/**
+ * Restrict access by role
+ * @param role User role
+ */
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+annotation class RestrictRoles(
+        vararg val role: UserRole
+)
