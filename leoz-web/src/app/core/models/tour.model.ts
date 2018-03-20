10ac782de8bff024219e94e4f41774bd6d5a13c5
Janussek <info@it-cobra.es>
2018-03-20 10:45:30 +0100

    eta in stoplist

--- leoz-web/src/app/core/models/tour.model.ts
+++ leoz-web/src/app/core/models/tour.model.ts
@@ -2,8 +2,23 @@ import { TourListItem } from './tour-list-item.model';
 import { DeliverylistItem } from './deliverylist.item.model';
 import { Address } from './address.model';
 
+interface Interval {
+  from?: string;
+  to?: string;
+}
+
+interface TourStopRouteMeta {
+  eta?: Interval;
+}
+
+interface Task {
+  orderId?: number;
+}
+
 interface Stop {
   address?: Address;
+  tasks?: Task[];
+  route?: TourStopRouteMeta;
 }
 
 export interface Tour extends TourListItem {
