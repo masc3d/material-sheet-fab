1df0f1d5292d5b081f2a69d664f87e0a59217ec5
masc <masc@disappear.de>
2018-03-26 21:15:50 +0200

    ADDED uuid most significant bit collision test

--- libs/sx-common/src/test/java/sx/util/UUIDTest.kt
+++ libs/sx-common/src/test/java/sx/util/UUIDTest.kt
@@ -1,11 +1,17 @@
 package sx.util
 
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers
+import org.junit.Ignore
 import org.junit.Test
 import org.slf4j.LoggerFactory
+import org.threeten.bp.Duration
 import sx.log.slf4j.debug
 import sx.log.slf4j.trace
 import sx.text.toHexString
 import java.util.*
+import java.util.concurrent.ConcurrentSkipListSet
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by masc on 24.03.18.
@@ -20,5 +26,38 @@ class UUIDTest {
         log.debug {
             "${uid} ${uid.mostSignificantBits.shr(8*4).toInt().toHexString()}"
         }
+    }
+
+    @Ignore
+    @Test
+    fun testMostSignificantBitsCollisions() {
+        val TEST_TIME = Duration.ofMinutes(1)
+
+        val bits = mutableSetOf<Long>()
+
+        Observable.generate<UUID> {
+            it.onNext(UUID.randomUUID())
+        }
+                .buffer(10000)
+                .flatMap {
+                    Observable
+                            .just(it)
+                            .map {
+                                synchronized(bits) {
+                                    it.forEach {
+                                        if (bits.contains(it.mostSignificantBits))
+                                            throw IllegalStateException("collision detected")
+
+                                        bits.add(it.mostSignificantBits)
+                                    }
+                                }
+                            }
+                            .subscribeOn(Schedulers.computation())
+                }
+                .ignoreElements()
+                .subscribeOn(Schedulers.computation())
+                .blockingAwait(TEST_TIME.toMillis(), TimeUnit.MILLISECONDS)
+
+        log.trace { "Checked ${bits.count()} most significant bits of uuids for collisions"}
     }
 }
