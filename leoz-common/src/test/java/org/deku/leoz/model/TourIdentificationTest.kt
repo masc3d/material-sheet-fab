30cbe55b0fa0096457775bcb36ecdbac52483184
masc <masc@disappear.de>
2018-04-09 11:46:28 +0200

    UPDATED tour identification spec

--- leoz-common/src/test/java/org/deku/leoz/model/TourIdentificationTest.kt
+++ leoz-common/src/test/java/org/deku/leoz/model/TourIdentificationTest.kt
@@ -1,5 +1,6 @@
 package org.deku.leoz.model
 
+import org.junit.Assert
 import org.junit.Test
 import org.junit.experimental.categories.Category
 import org.slf4j.LoggerFactory
@@ -13,10 +14,18 @@ import sx.log.slf4j.trace
 class TourIdentificationTest {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
+    private val LABELS = listOf(
+        "DK;TR;310;6BA54459-0504-49D9-A536-1194BA6E4EB4"
+    )
+
     @Test
     fun testParseLabel() {
-        log.trace {
-            TourIdentification.parseLabel("<deku-tour id=\"701\" uid=\"6ba54459-0504-49d9-a536-1194ba6e4eb4\"/>")
+        LABELS.forEach { label ->
+            log.trace {
+                TourIdentification.parseLabel(label).also {
+                    Assert.assertEquals(it.value.label, label)
+                }
+            }
         }
     }
 }
