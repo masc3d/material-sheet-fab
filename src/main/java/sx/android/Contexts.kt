95163c8a786b53f908e98b46cb4dd16d9d255001
masc <masc@disappear.de>
2018-04-19 17:57:39 +0200

    [libs-sx-android] ADDED more observable extensions, cleanup

--- src/main/java/sx/android/Contexts.kt
+++ src/main/java/sx/android/Contexts.kt
@@ -1,7 +1,10 @@
 package sx.android
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.Drawable
+import android.support.annotation.ColorInt
+import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
 import android.support.v4.content.ContextCompat
 
@@ -10,3 +13,8 @@ import android.support.v4.content.ContextCompat
 fun Context.getDrawableCompat(@DrawableRes id: Int): Drawable =
         ContextCompat.getDrawable(this, id)
                 ?: throw IllegalArgumentException("Invalid drawable id [${id}]")
+
+@ColorInt
+fun Context.getColorCompat(@ColorRes id: Int): Int =
+        ContextCompat.getColor(this, id)
+
