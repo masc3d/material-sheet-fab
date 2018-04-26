37056a21e7c8a6dc24caf190d58b6978ce3705e3
Helke <hw@dr-t.de>
2018-04-26 09:15:23 +0200

    ADDED node Orderservice

--- leoz-node/src/main/java/org/deku/leoz/node/service/internal/OrderService.kt
+++ leoz-node/src/main/java/org/deku/leoz/node/service/internal/OrderService.kt
@@ -1,15 +1,13 @@
-package org.deku.leoz.central.service.internal
+package org.deku.leoz.node.service.internal
 
-import org.deku.leoz.central.Application
-import org.deku.leoz.central.data.jooq.dekuclient.tables.records.TadVOrderParcelRecord
-import org.deku.leoz.central.data.jooq.dekuclient.tables.records.TadVOrderRecord
-import org.deku.leoz.central.data.repository.JooqOrderRepository
-import org.deku.leoz.central.data.repository.JooqStationRepository
 import org.deku.leoz.model.*
+import org.deku.leoz.node.Application
+import org.deku.leoz.node.data.repository.DebitorStationRepository
+import org.deku.leoz.node.data.repository.OrderParcelRepository
+import org.deku.leoz.node.data.repository.OrderRepository
+import org.deku.leoz.node.data.repository.StationRepository
 import org.deku.leoz.node.rest.restrictByDebitor
 import org.deku.leoz.service.internal.OrderService
-import org.deku.leoz.service.internal.OrderService.Order
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import sx.rs.RestProblem
@@ -19,40 +17,49 @@ import javax.ws.rs.Path
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 import org.deku.leoz.service.internal.OrderService.Type
+import org.deku.leoz.service.internal.OrderService.Order
+import org.deku.leoz.node.data.jpa.TadOrderParcel
+import org.deku.leoz.node.data.jpa.TadOrder
 
-/**
- * Order service implementation
- * Created by JT on 30.06.17.
- */
 @Component
 @Path("internal/v1/order")
-@Profile(Application.PROFILE_CENTRAL)
-class OrderService
-    :
-        org.deku.leoz.service.internal.OrderService {
-
-    private val log = LoggerFactory.getLogger(this.javaClass)
-
-    @Inject
-    private lateinit var orderRepository: JooqOrderRepository
-
-    @Inject
-    private lateinit var stationRepository: JooqStationRepository
+@Profile(Application.PROFILE_CLIENT_NODE)
+class OrderService : org.deku.leoz.service.internal.OrderService {
 
     @Context
     private lateinit var httpRequest: HttpServletRequest
 
+    @Inject
+    private lateinit var orderRepository: OrderRepository
+
+    @Inject
+    private lateinit var orderParcelRepository: OrderParcelRepository
+
+    @Inject
+    private lateinit var debitorStationRepository: DebitorStationRepository
+
+    @Inject
+    private lateinit var stationRepository: StationRepository
+
     override fun get(ids: List<Long>?, labelRef: String?, custRef: String?, parcelScan: String?): List<OrderService.Order> {
-        val orders: List<Order>
+
+        val orders: List<OrderService.Order>
 
         when {
             parcelScan != null -> {
                 // Query by parcel scan
-                val order = this.orderRepository.findByScan(parcelScan)
-                        ?.toOrder()
-                        ?: throw RestProblem(
-                                title = "Order not found",
-                                status = Response.Status.NOT_FOUND)
+                val parcelScanId = parcelScan.toLongOrNull()
+                parcelScanId ?: throw RestProblem(
+                        title = "ScanId not numeric - not implemented",
+                        status = Response.Status.BAD_REQUEST
+                )
+                val orderRecord: TadOrder? = this.orderParcelRepository.findByScanId(parcelScanId)?.let { this.orderRepository.findByOrderId(it.orderid) }
+                orderRecord ?: throw RestProblem(
+                        title = "Order not found",
+                        status = Response.Status.NOT_FOUND)
+
+
+                val order = orderRecord.toOrder()
 
                 httpRequest.restrictByDebitor { order.findOrderDebitorId(Type.DELIVERY) }
 
@@ -65,11 +72,16 @@ class OrderService
                 TODO("Lookup by cust ref not implemented")
             }
             ids != null -> {
-                val rOrders = this.orderRepository.findByIds(ids)
+                val rOrders = this.orderRepository.findByOrderIds(ids)
 
-                val rParcelsByOrderId = this.orderRepository
-                        .findParcelsByOrderIds(rOrders.map { it.id.toLong() })
-                        .groupBy { it.orderId.toLong() }
+                if(rOrders.count()==0)
+                    throw RestProblem(
+                            title = "Order not found",
+                            status = Response.Status.NOT_FOUND)
+
+                val rParcelsByOrderId = this.orderParcelRepository
+                        .findByOrderIds(rOrders.map { it.orderid })
+                        .groupBy { it.orderid }
 
                 // TODO: debitor restricition validation, but only if it was http request. check must be skipped for internal consumers
 
@@ -84,9 +96,9 @@ class OrderService
             else -> TODO("Not supported")
         }
 
-        orders.forEach { order ->
-            order.parcels = this.orderRepository
-                    .findParcelsByOrderId(order.id)
+        orders.forEach { x ->
+            x.parcels = this.orderParcelRepository
+                    .findByOrderId(x.id)
                     .map { it.toParcel() }
         }
 
@@ -94,8 +106,7 @@ class OrderService
     }
 
     override fun getById(id: Long): OrderService.Order {
-
-        val order = this.orderRepository.findById(id)
+        val order = this.orderRepository.findByOrderId(id)
                 ?.toOrder()
                 ?: throw RestProblem(
                         title = "Order not found",
@@ -103,34 +114,28 @@ class OrderService
 
         httpRequest.restrictByDebitor { order.findOrderDebitorId(Type.DELIVERY) }
 
-        order.parcels = this.orderRepository
-                .findParcelsByOrderId(order.id)
+        order.parcels = this.orderParcelRepository
+                .findByOrderId(order.id)
                 .map { it.toParcel() }
 
         return order
     }
 
     //todo include send_date
-    fun Order.findOrderDebitorId(pickupDeliver: Type): Int? {
+    fun OrderService.Order.findOrderDebitorId(pickupDeliver: Type): Int? {
         val stationNo: Int
 
         when (pickupDeliver) {
             Type.PICKUP -> stationNo = this.pickupStation
             Type.DELIVERY -> stationNo = this.deliveryStation
         }
-        return stationRepository.findByStationNo(stationNo)?.debitorId
+        return stationRepository.findByStation(stationNo)?.let { debitorStationRepository.findByStationId(it.stationId)?.debitorId }
     }
 
-
-
-    /**
-     * Order record conversion extension
-     */
-    fun TadVOrderRecord.toOrder(): Order {
+    fun TadOrder.toOrder(): OrderService.Order {
         val o = Order()
         val r = this
-
-        o.id = r.id.toLong()
+        o.id = this.orderid
         o.referenceIDToExchangeOrderID = r.referenceIdToExchangeId.toLong()
         o.carrier = Carrier.DER_KURIER
         if (r.pickupStation == r.deliveryStation && r.customerStation == r.deliveryStation)
@@ -164,10 +169,10 @@ class OrderService
 
         o.pickupAppointment.dateStart = r.appointmentPickupStart
         o.pickupAppointment.dateEnd = r.appointmentPickupEnd
-        o.pickupAppointment.notBeforeStart = r.appointmentPickupNotBeforeStart == 1
+        o.pickupAppointment.notBeforeStart = r.appointmentPickupNotBeforeStart
         o.pickupAppointment.dateStart = r.appointmentPickupStart
         o.pickupAppointment.dateEnd = r.appointmentPickupEnd
-        o.pickupAppointment.notBeforeStart = r.appointmentPickupNotBeforeStart == 1
+        o.pickupAppointment.notBeforeStart = r.appointmentPickupNotBeforeStart
 
         o.deliveryStation = r.deliveryStation.toInt()
         o.deliveryAddress.line1 = r.deliveryAddressLine1
@@ -202,23 +207,20 @@ class OrderService
 
         o.deliveryAppointment.dateStart = r.appointmentDeliveryStart
         o.deliveryAppointment.dateEnd = r.appointmentDeliveryEnd
-        o.deliveryAppointment.notBeforeStart = r.appointmentDeliveryNotBeforeStart == 1
+        o.deliveryAppointment.notBeforeStart = r.appointmentDeliveryNotBeforeStart
         o.deliveryAppointment.dateStart = r.appointmentDeliveryStart
         o.deliveryAppointment.dateEnd = r.appointmentDeliveryEnd
-        o.deliveryAppointment.notBeforeStart = r.appointmentDeliveryNotBeforeStart == 1
+        o.deliveryAppointment.notBeforeStart = r.appointmentDeliveryNotBeforeStart
 
         return o
-    }
 
-    /**
-     * Order parcel record conversion extension
-     */
-    fun TadVOrderParcelRecord.toParcel(): Order.Parcel {
+    }
+    fun TadOrderParcel.toParcel():OrderService.Order.Parcel{
         val r = this
         val p = Order.Parcel()
-        p.id = r.id.toLong()
+        p.id = r.parcelId
         p.number = toUnitNo(r.scanId)
-        p.parcelType = ParcelType.valueMap.getValue(r.parcelType)
+        p.parcelType = ParcelType.valueMap.getValue(r.parcelType.toInt())
         p.lastDeliveryListId = r.lastDeliveryListId?.toInt()
         if (DekuUnitNumber.parse(
                         toUnitNo(r.scanId)).value.type == UnitNumber.Type.Bag
@@ -229,17 +231,16 @@ class OrderService
             p.isDelivered = r.deliveredStatus.toInt() == 4
         }
         p.isMissing = r.deliveredStatus.toInt() == 8 && r.lastDeliveredEventReason.toInt() == 30
-        p.isDamaged = r.isDamaged.toInt() == -1  // is set form leo statusExport 1
-        p.dimension.weight = r.dimentionWeight
-        p.dimension.height = r.dimensionHeight.toInt()
-        p.dimension.length = r.dimensionLength.toInt()
-        p.dimension.width = r.dimensionWidth.toInt()
+        p.isDamaged = r.isDamaged // is set form leo statusExport 1
+        p.dimension.weight = r.dimensionWeightReal
+        p.dimension.height = r.dimensionHeight
+        p.dimension.length = r.dimensionLength
+        p.dimension.width = r.dimensionWidth
 
         return p
     }
-
-    fun toUnitNo(id: Double): String {
-        return id.toLong().toString().padStart(11, padChar = '0')
+    fun toUnitNo(id: Long): String {
+        return id.toString().padStart(11, padChar = '0')
     }
-}
 
+}
