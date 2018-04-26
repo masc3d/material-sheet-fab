a34cf904efc23e9ad2e94d1f702b550198ab8c8c
masc <masc@disappear.de>
2018-04-26 17:19:03 +0200

    UPDATED sx threetenbp extensions

--- libs/sx-common/src/main/java/sx/time/threeten/utils.kt
+++ libs/sx-common/src/main/java/sx/time/threeten/utils.kt
@@ -8,48 +8,37 @@ import java.util.*
  * Created by masc on 10.10.17.
  */
 
-fun Date.toInstantBp(): Instant
-        = DateTimeUtils.toInstant(this)
+fun Date.toInstantBp(): Instant = DateTimeUtils.toInstant(this)
 
-fun Instant.toDate(): Date
-        = DateTimeUtils.toDate(this)
+fun Instant.toDate(): Date = DateTimeUtils.toDate(this)
 
-fun Calendar.toInstantBp(): Instant
-        = DateTimeUtils.toInstant(this)
+fun Calendar.toInstantBp(): Instant = DateTimeUtils.toInstant(this)
 
-fun Calendar.toZonedDateTime(): ZonedDateTime
-        = DateTimeUtils.toZonedDateTime(this)
+fun Calendar.toZonedDateTime(): ZonedDateTime = DateTimeUtils.toZonedDateTime(this)
 
-fun ZonedDateTime.toCalendar(): GregorianCalendar
-        = DateTimeUtils.toGregorianCalendar(this)
+fun ZonedDateTime.toCalendar(): GregorianCalendar = DateTimeUtils.toGregorianCalendar(this)
 
-fun TimeZone.toZoneIdBp(): ZoneId
-        = DateTimeUtils.toZoneId(this)
+fun TimeZone.toZoneIdBp(): ZoneId = DateTimeUtils.toZoneId(this)
 
-fun ZoneId.toTimeZone(): TimeZone
-        = DateTimeUtils.toTimeZone(this)
+fun ZoneId.toTimeZone(): TimeZone = DateTimeUtils.toTimeZone(this)
 
-fun java.sql.Date.toLocalDateBp(): LocalDate
-        = DateTimeUtils.toLocalDate(this)
+fun java.sql.Date.toLocalDateBp(): LocalDate = DateTimeUtils.toLocalDate(this)
 
-fun LocalDate.toSqlDate(): java.sql.Date
-        = DateTimeUtils.toSqlDate(this)
+fun LocalDate.toSqlDate(): java.sql.Date = DateTimeUtils.toSqlDate(this)
 
-fun java.sql.Time.toLocalTimeBp(): LocalTime
-        = DateTimeUtils.toLocalTime(this)
+fun LocalDate.toDate(): java.util.Date = DateTimeUtils.toSqlDate(this)
 
-fun LocalTime.toSqlTime(): java.sql.Time
-        = DateTimeUtils.toSqlTime(this)
+fun java.sql.Time.toLocalTimeBp(): LocalTime = DateTimeUtils.toLocalTime(this)
 
-fun java.sql.Timestamp.toLocalDateTimeBp(): LocalDateTime
-        = DateTimeUtils.toLocalDateTime(this)
+fun LocalTime.toSqlTime(): java.sql.Time = DateTimeUtils.toSqlTime(this)
 
-fun LocalDateTime.toSqlTimetamp(): java.sql.Timestamp
-        = DateTimeUtils.toSqlTimestamp(this)
+fun LocalTime.toDate(): java.util.Date = DateTimeUtils.toSqlTime(this)
 
-fun java.sql.Timestamp.toInstantBp(): Instant
-        = DateTimeUtils.toInstant(this)
+fun java.sql.Timestamp.toLocalDateTimeBp(): LocalDateTime = DateTimeUtils.toLocalDateTime(this)
 
-fun Instant.toSqlTimestamp(): java.sql.Timestamp
-        = DateTimeUtils.toSqlTimestamp(this)
+fun LocalDateTime.toSqlTimetamp(): java.sql.Timestamp = DateTimeUtils.toSqlTimestamp(this)
+
+fun java.sql.Timestamp.toInstantBp(): Instant = DateTimeUtils.toInstant(this)
+
+fun Instant.toSqlTimestamp(): java.sql.Timestamp = DateTimeUtils.toSqlTimestamp(this)
 
