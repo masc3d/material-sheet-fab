eeec168a86751bd66fb2706a4ce33b05c178fcfe
masc <masc@disappear.de>
2018-04-29 15:06:40 +0200

    FIXED threetenbp conversion extensions

--- libs/sx-common/src/main/java/sx/time/threeten/utils.kt
+++ libs/sx-common/src/main/java/sx/time/threeten/utils.kt
@@ -12,6 +12,8 @@ fun Date.toInstantBp(): Instant = DateTimeUtils.toInstant(this)
 
 fun Instant.toDate(): Date = DateTimeUtils.toDate(this)
 
+fun Instant.toSqlTimestamp(): java.sql.Timestamp = DateTimeUtils.toSqlTimestamp(this)
+
 fun Calendar.toInstantBp(): Instant = DateTimeUtils.toInstant(this)
 
 fun Calendar.toZonedDateTime(): ZonedDateTime = DateTimeUtils.toZonedDateTime(this)
@@ -22,23 +24,25 @@ fun TimeZone.toZoneIdBp(): ZoneId = DateTimeUtils.toZoneId(this)
 
 fun ZoneId.toTimeZone(): TimeZone = DateTimeUtils.toTimeZone(this)
 
-fun java.sql.Date.toLocalDateBp(): LocalDate = DateTimeUtils.toLocalDate(this)
-
 fun LocalDate.toSqlDate(): java.sql.Date = DateTimeUtils.toSqlDate(this)
 
 fun LocalDate.toDate(): java.util.Date = DateTimeUtils.toSqlDate(this)
-
-fun java.sql.Time.toLocalTimeBp(): LocalTime = DateTimeUtils.toLocalTime(this)
 
 fun LocalTime.toSqlTime(): java.sql.Time = DateTimeUtils.toSqlTime(this)
 
 fun LocalTime.toDate(): java.util.Date = DateTimeUtils.toSqlTime(this)
 
-fun java.sql.Timestamp.toLocalDateTimeBp(): LocalDateTime = DateTimeUtils.toLocalDateTime(this)
-
 fun LocalDateTime.toSqlTimetamp(): java.sql.Timestamp = DateTimeUtils.toSqlTimestamp(this)
+
+fun LocalDateTime.toDate(): Date = Date(this.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli())
+
+fun java.sql.Date.toLocalDateBp(): LocalDate = DateTimeUtils.toLocalDate(this)
+
+fun java.sql.Time.toLocalTimeBp(): LocalTime = DateTimeUtils.toLocalTime(this)
+
+fun java.sql.Timestamp.toLocalDateTimeBp(): LocalDateTime = DateTimeUtils.toLocalDateTime(this)
 
 fun java.sql.Timestamp.toInstantBp(): Instant = DateTimeUtils.toInstant(this)
 
-fun Instant.toSqlTimestamp(): java.sql.Timestamp = DateTimeUtils.toSqlTimestamp(this)
+fun java.util.Date.toLocalDateTime(): LocalDateTime = this.toInstantBp().atZone(ZoneId.systemDefault()).toLocalDateTime()
 
