597bfe4e14f5710748e530ee290000c093d833d1
masc <masc@disappear.de>
2018-05-16 12:53:31 +0200

    FIXED tour service location push causes excessive error logging for legacy consumers

--- leoz-central/src/main/java/org/deku/leoz/central/service/internal/TourService.kt
+++ leoz-central/src/main/java/org/deku/leoz/central/service/internal/TourService.kt
@@ -2,6 +2,7 @@ package org.deku.leoz.central.service.internal
 
 import io.reactivex.rxkotlin.subscribeBy
 import org.deku.leoz.node.data.repository.StationContractRepository
+import org.deku.leoz.node.data.repository.TadTourRepository
 import org.deku.leoz.node.service.smartlane.SmartlaneBridge
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -30,6 +31,9 @@ class TourServiceV1
     private lateinit var locationService: LocationServiceV2
 
     @Inject
+    private lateinit var tourRepo: TadTourRepository
+
+    @Inject
     private lateinit var smartlane: SmartlaneBridge
 
     // TODO: move to node as soon as location service has been migrated
@@ -42,6 +46,14 @@ class TourServiceV1
                     try {
                         val userId = gpsMessage.userId ?: run {
                             // Skip location updates without user id
+                            return@subscribe
+                        }
+
+                        // Most recent tour
+                        val tour = tourRepo.findMostRecentByUserId(userId.toLong())
+
+                        if (tour == null || tour.stationNo == null) {
+                            // No recent tour or legacy tour without station no -> silently ignoring
                             return@subscribe
                         }
 
