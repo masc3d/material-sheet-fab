bd3a2fc4e5f040ca9104f19f5601e8b606fc5664
masc <masc@disappear.de>
2018-05-18 11:44:48 +0200

    UPDATED sx reflection extensions

--- libs/sx-common/src/main/java/sx/reflect/extensions.kt
+++ libs/sx-common/src/main/java/sx/reflect/extensions.kt
@@ -1,25 +1,33 @@
 package sx.reflect
 
 import org.apache.commons.lang3.ClassUtils
+import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
+
+data class GenericInterface(
+        val type: Type,
+        val implementingType: Class<*>
+) {
+    val parameterizedType = type as? ParameterizedType
+}
 
 /**
  * All generic interfaces of a class
  * Created by masc on 12.05.17.
  */
-val Class<*>.allGenericInterfaces: List<Type>
+val Class<*>.allGenericInterfaces: List<GenericInterface>
     get() {
-        val interfaces = mutableListOf<Type>()
+        val types = mutableListOf<GenericInterface>()
 
-        fun recurse(c: Class<*>, types: MutableList<Type>) {
-            types.addAll(c.genericInterfaces.toList())
+        fun recurse(c: Class<*>, types: MutableList<GenericInterface>) {
+            types.addAll(c.genericInterfaces.map { GenericInterface(it, c) }.toList())
             if (c.superclass != null && c.superclass != Any::class.java)
                 recurse(c.superclass, types)
         }
 
-        recurse(this, interfaces)
+        recurse(this, types)
 
-        return interfaces
+        return types
     }
 
 /**
