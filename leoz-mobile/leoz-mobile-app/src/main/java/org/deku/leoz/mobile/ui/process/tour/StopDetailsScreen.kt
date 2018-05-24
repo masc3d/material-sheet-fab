39c836951922b302f09b3d38c13b32e05c4ef977
Philipp Prangenberg <philipp.prangenberg@derkurier.de>
2018-05-24 13:39:17 +0200

    leoz-mobile: Removed unnecessary visible flag in ActionItem

--- leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/process/tour/StopDetailsScreen.kt
+++ leoz-mobile/leoz-mobile-app/src/main/java/org/deku/leoz/mobile/ui/process/tour/StopDetailsScreen.kt
@@ -282,8 +282,7 @@ class StopDetailsScreen
                         iconRes = R.drawable.ic_comment_alert,
                         iconTintRes = android.R.color.white,
                         alignEnd = false,
-                        menu = stopNoteMenu,
-                        visible = isReportDelayActionAvailable || isAddStopNoteActionAvailable
+                        menu = stopNoteMenu
                 )
         )
     }
