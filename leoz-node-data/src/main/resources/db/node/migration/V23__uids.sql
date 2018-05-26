06dcbd93d648307a5c2d03d6b729a4b45a753b7a
masc <masc@disappear.de>
2018-05-26 23:14:07 +0200

    FIXED h2 mst_user: key_uid reference should not be unique

--- leoz-node-data/src/main/resources/db/node/migration/V23__uids.sql
+++ leoz-node-data/src/main/resources/db/node/migration/V23__uids.sql
@@ -8,7 +8,7 @@ CREATE UNIQUE INDEX ix_mst_user_sync_id ON mst_user(sync_id);
 
 ALTER TABLE mst_user
     ADD COLUMN key_uid UUID AFTER key_id;
-CREATE UNIQUE INDEX ix_mst_user_key_uid ON mst_user(key_uid);
+CREATE INDEX ix_mst_user_key_uid ON mst_user(key_uid);
 
 ALTER TABLE mst_key
     ADD COLUMN uid UUID DEFAULT RANDOM_UUID() AFTER id;
